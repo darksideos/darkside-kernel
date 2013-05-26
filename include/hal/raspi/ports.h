a960becada630713da05a26278f920c21b2d75f4
Noah Singer <singerng@gmail.com>
2013-05-26 18:02:10 -0400

    Moved port functions in raspi to mmio

--- include/hal/raspi/ports.h
+++
@@ -1,4 +0,0 @@
-void outportl(unsigned int address, unsigned int value);
-unsigned int inportl(unsigned int address);
-void outportw(unsigned int address, unsigned short value);
-unsigned short inportw(unsigned int address);
