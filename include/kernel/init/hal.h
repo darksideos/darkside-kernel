a5d2b6a6ee0d20826b3f9c99168e1f59aff542c3
George Klees <gksharkboy@gmail.com>
2013-05-27 20:23:46 -0400

    Changed some functions in the HAL struct

--- include/kernel/init/hal.h
+++ include/kernel/init/hal.h
@@ -6,7 +6,7 @@ typedef struct
 	/* HAL main functions */
 	void (*hal_main)(struct multiboot *mboot_ptr);
 
-	/* Port I/O */
+	/* Port I/O*/
 	unsigned char (*inportb)(unsigned int port);
 	void (*outportb)(unsigned int port, unsigned char data);
 	unsigned char (*inportw)(unsigned int port);
@@ -14,30 +14,34 @@ typedef struct
 	unsigned char (*inportl)(unsigned int port);
 	void (*outportl)(unsigned int port, unsigned long data);
 
+	/* MMIO */
+	unsigned char (*inmemb)(unsigned int address);
+	void (*outmemb)(unsigned int address, unsigned char data);
+	unsigned char (*inmemw)(unsigned int address);
+	void (*outmemw)(unsigned int address, unsigned short data);
+	unsigned char (*inmeml)(unsigned int address);
+	void (*outmeml)(unsigned int address, unsigned long data);
+
 	/* Interrupts */
-	void (*irq_install)();
 	void (*irq_install_handler)(int irq, void *handler);
 	void (*irq_uninstall_handler)(int irq);
 	void (*hal_cli)();
 	void (*hal_sti)();
 
 	/* Timer */
-	void (*timer_install)(int hz);
 	void (*sleep)(int sec);
 
 	/* Physical memory manager */
-	void (*init_pmm)(unsigned int size);
 	unsigned int (*pmm_alloc_page)();
 	void (*pmm_free_page)(unsigned int address);
 
 	/* Virtual memory manager */
-	void  (*init_vmm)();
 	void  (*map_page)(void *dir, unsigned int virtual_address, unsigned int physical_address, unsigned int flags);
 	void  (*unmap_page)(void *dir, unsigned int virtual_address);
 	void  (*map_kernel)(void *dir);
 	void* (*clone_address_space)(void *src);
-	void  (*switch_address_space)(void *dir);
 	void* (*create_address_space)();
+	void  (*switch_address_space)(void *dir);
 
 	/* Multitasking */
 	void  (*task_switch_stub)(void *context);
