f7b942c0aad7faf82643fc3c007114a23053ebbf
George Klees <george.t.klees@gmail.com>
2013-06-14 16:55:11 -0400

    Got rid of that stupid bit.c file

--- include/lib/libc/bit.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BIT_H
-#define __BIT_H
-
-unsigned int bit_set(unsigned int val, unsigned char bit);
-unsigned int bit_clear(unsigned int val, unsigned char bit);
-unsigned int bit_test(unsigned int val, unsigned char bit);
-unsigned int bit_toggle(unsigned int val, unsigned char bit);
-
-#endif
