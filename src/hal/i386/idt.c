df923552d2eb1329c19ea278c7c4efc9b7dc47d3
George Klees <gksharkboy@gmail.com>
2013-06-14 16:55:11 -0400

    Got rid of that stupid bit.c file

--- src/hal/i386/idt.c
+++ src/hal/i386/idt.c
@@ -17,12 +17,7 @@ struct idt_ptr
     unsigned int base;
 } __attribute__((packed));
 
-/* Declare an IDT of 256 entries. Although we will only use the
-*  first 48 entries in this tutorial, the rest exists as a bit
-*  of a trap. If any undefined IDT entry is hit, it normally
-*  will cause an "Unhandled Interrupt" exception. Any descriptor
-*  for which the 'presence' bit is cleared (0) will generate an
-*  "Unhandled Interrupt" exception */
+/* IDT and IDT Pointer */
 struct idt_entry idt[256];
 struct idt_ptr idtp;
 
