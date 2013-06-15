2a016e34dd28c58a857f3770467bdd52d1981d83
George Klees <george.t.klees@gmail.com>
2013-06-15 10:03:01 -0400

    Edited OS design

--- include/lib/libc/math.h
+++ include/lib/libc/math.h
@@ -1,21 +1,6 @@
 #ifndef __MATH_H
 #define __MATH_H
 
-typedef struct
-{
-	float x;
-	float y;
-} Vector2;
-
-
-typedef struct
-{
-	float x;
-	float y;
-	float z;
-} Vector3;
-
-
 int pow(int num, int exp);
 
 int ceil(int num, int div);
