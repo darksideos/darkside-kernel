c384835d85ce282f8879166e46bbf81811c9f19c
George Klees <george.t.klees@gmail.com>
2013-06-15 22:03:00 -0400

    Wrote skeleton version of lookup_chunk()

--- include/kernel/mm/heap/heap.h
+++ include/kernel/mm/heap/heap.h
@@ -2,19 +2,33 @@
 #define __HEAP_H
 
 #include <lib/libgcc/stdbool.h>
-#include <lib/libgeneric.h>
 
 #define KHEAP_START        0xE0000000
 #define KHEAP_INITIAL_SIZE 0x100000
 #define KHEAP_MIN_SIZE	   0x70000
 #define KHEAP_MAX_SIZE	   0xFFFF000
-#define KHEAP_INDEX_SIZE   0x60000
 
 #define HEAP_MAGIC		   0x123890AB
 
+/* Heaader, footer, and heap structures */
+typedef struct header
+{
+   unsigned int magic;			// Magic number
+   unsigned char type;			// 0 - Hole, 1 - Block, 2 - Hole/Block
+   unsigned int size;			// Size of the block
+
+   struct header *left, *right;	// Left and right pointers
+} header_t;
+
+typedef struct footer
+{
+	unsigned int magic;			// Magic number
+	header_t *header;			// Pointer to the header
+} footer_t;
+
 typedef struct heap
 {
-	binary_tree_t index;		// Root of the heap index
+	header_t *index;			// Root of the heap index
 
 	unsigned int start_address; // The start address of the heap
 	unsigned int end_address;   // The end address of the heap
@@ -23,20 +37,6 @@ typedef struct heap
 
 	bool user;					// Accessible in user mode?
 } heap_t;
-
-/* Heaader, footer, and heap structures */
-typedef struct header
-{
-   unsigned int magic;			// Magic number
-   unsigned char type;			// 0 - Hole, 1 - Block
-   unsigned int size;			// Size of the block
-} header_t;
-
-typedef struct footer
-{
-	unsigned int magic;			// Magic number
-	header_t *header;			// Pointer to the header
-} footer_t;
 
 /* Allocate memory on the kernel heap */
 void *kmalloc(unsigned int size);
@@ -54,7 +54,7 @@ void *krealloc_p(void *ptr, unsigned int size, unsigned int *phys);
 void *krealloc_ap(void *ptr, unsigned int size, unsigned int *phys);
 
 /* Create a heap */
-heap_t *create_heap(unsigned int start_address, unsigned int end_address, unsigned int min_address, unsigned int max_address, unsigned int index_size, bool user);
+heap_t *create_heap(unsigned int start_address, unsigned int end_address, unsigned int min_address, unsigned int max_address, bool user);
 
 /* Allocate, free memory, and resize memory allocated on a heap */
 void *heap_malloc(heap_t *heap, unsigned int size, bool align);
