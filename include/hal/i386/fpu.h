ff7460645dd583d58517f4d078b1862573495719
George Klees <gksharkboy@gmail.com>
2013-06-18 18:48:56 -0400

    Added constants to FPU code

--- include/hal/i386/fpu.h
+++ include/hal/i386/fpu.h
@@ -1,6 +1,15 @@
 #ifndef __FPU_H
 #define __FPU_H
 
+/* FPU control register flags */
+#define FPU_CR0_TASK_ENABLE		0x08
+#define FPU_CR0_387_ENABLE		0x10
+#define	FPU_CR0_NE_ENABLE		0x20
+
+#define FPU_CR4_SSE_ENABLE		0x200
+#define FPU_CR4_XF_ENABLE		0x400
+#define FPU_CR4_FXSAVE_ENABLE	0x40000
+
 /* Initialize the FPU */
 void init_fpu();
 
