7961445a1fe12f569772ede28ae323facea5bcef
George Klees <gksharkboy@gmail.com>
2013-06-18 09:23:16 -0400

    Added a file for BIOS memory map

---
+++ src/hal/i386/bios.c
@@ -0,0 +1,14 @@
+#include <lib/libgcc/stdbool.h>
+#include <hal/i386/bios.h>
+
+/* Check if a page in the BIOS memory map is usable */
+bool mem_map_page_ok(unsigned int address)
+{
+	/* Temporary! */
+	return true;
+}
+
+/* Initialize the BIOS memory map */
+void init_bios()
+{
+}
