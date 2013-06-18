f6039d5f6b6978b3bc5a6811752f07762cdca58e
George Klees <gksharkboy@gmail.com>
2013-06-18 18:24:33 -0400

    Modified the FPU code a bit

--- src/hal/i386/fpu.c
+++ src/hal/i386/fpu.c
@@ -6,18 +6,16 @@ void set_fpu_cw(const unsigned short cw)
 	asm volatile("fldcw %0" :: "m"(cw));
 }
 
-/* Enable the FPU */
-void enable_fpu()
+/* Initialize the FPU */
+void init_fpu()
 {
+	/* Enable SSE */
 	unsigned int cr4;
 	asm volatile("mov %%cr4, %0" : "=r" (cr4));
 	cr4 |= 0x200;
 	asm volatile("mov %0, %%cr4" :: "r" (cr4));
-	set_fpu_cw(0x37F);
-}
 
-/* Initialize the FPU by enabling it */
-void init_fpu()
-{
-	enable_fpu();
+	set_fpu_cw(0x37F);
+	set_fpu_cw(0x37E);
+	set_fpu_cw(0x37A);
 }
