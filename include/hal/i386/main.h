132ee05ea4c3cffa99ef6148851d18544f9f9b04
George Klees <gksharkboy@gmail.com>
2013-06-19 16:20:17 -0400

    Nearly all warnings fixed

--- include/hal/i386/main.h
+++ include/hal/i386/main.h
@@ -1,6 +1,8 @@
 #ifndef __i386_MAIN_H
 #define __i386_MAIN_H
 
+#include <kernel/modules/multiboot.h>
+
 /* Initialize the CPU */
 void hal_main(struct multiboot *mboot_ptr);
 
--- src/hal/i386/idt.c
+++ src/hal/i386/idt.c
@@ -1,22 +1,6 @@
 #include <lib/libc/string.h>
 #include <hal/i386/idt.h>
 
-/* Defines an IDT entry */
-struct idt_entry
-{
-    unsigned short base_lo;
-    unsigned short sel;
-    unsigned char always0;
-    unsigned char flags;
-    unsigned short base_hi;
-} __attribute__((packed));
-
-struct idt_ptr
-{
-    unsigned short limit;
-    unsigned int base;
-} __attribute__((packed));
-
 /* IDT and IDT Pointer */
 struct idt_entry idt[256];
 struct idt_ptr idtp;
@@ -42,7 +26,7 @@ void idt_install()
 {
     /* Sets the special IDT pointer up, just like in 'gdt.c' */
     idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
-    idtp.base = &idt;
+    idtp.base = (unsigned int) &idt;
 
     /* Clear out the entire IDT, initializing it to zeros */
     memset(&idt, 0, sizeof(struct idt_entry) * 256);
