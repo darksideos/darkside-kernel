e0724ef60219d5001970bffae24c72b9c1a3bf7b
George Klees <gksharkboy@gmail.com>
2013-06-19 12:52:26 -0400

    Finished VMM rewrite (Noah, please look over it)

--- include/kernel/mm/heap/heap.h
+++ include/kernel/mm/heap/heap.h
@@ -33,20 +33,10 @@ typedef struct heap
 	bool user;					// Accessible in user mode?
 } heap_t;
 
-/* Allocate memory on the kernel heap */
+/* Allocate, free, and resize memory on the kernel heap */
 void *kmalloc(unsigned int size);
-void *kmalloc_a(unsigned int size);
-void *kmalloc_p(unsigned int size, unsigned int *phys);
-void *kmalloc_ap(unsigned int size, unsigned int *phys);
-
-/* Free memory on the kernel heap */
 void kfree(void *ptr);
-
-/* Resize memory allocated on the kernel heap */
 void *krealloc(void *ptr, unsigned int size);
-void *krealloc_a(void *ptr, unsigned int size);
-void *krealloc_p(void *ptr, unsigned int size, unsigned int *phys);
-void *krealloc_ap(void *ptr, unsigned int size, unsigned int *phys);
 
 /* Create a heap */
 heap_t *create_heap(unsigned int start_address, unsigned int end_address, unsigned int min_address, unsigned int max_address, bool user);
