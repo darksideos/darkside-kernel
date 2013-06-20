365a01ab6bfae2a8b3844ad02f0b750bff845efd
Noah Singer <singerng@gmail.com>
2013-06-20 15:23:26 -0400

    Removed a bunch of HAL calls in generic code

--- include/kernel/debug/bochs.h
+++ include/kernel/debug/bochs.h
@@ -1,7 +1,7 @@
 #ifndef __BOCHS_H
 #define __BOCHS_H
 
-#include <hal/i386/ports.h>
+#include <kernel/init/hal.h>
 
 #define bochs_putch(c) outportb(0xe9, c)
 #define bochs_break_e9() outportw(0x8A00,0x8A00); outportw(0x8A00,0x08AE0)
