82ae72d48f87842f8c472fe9b6484b302544f949
Noah Singer <singerng@gmail.com>
2013-06-20 14:59:30 -0400

    Removed HAL struct (now it's just HAL funcs)

--- include/kernel/init/hal.h
+++ include/kernel/init/hal.h
@@ -3,60 +3,51 @@
 
 #include <kernel/modules/multiboot.h>
 
-typedef struct
-{
-	/* HAL main functions */
-	void (*hal_main)(struct multiboot *mboot_ptr);
+/* HAL main functions */
+void hal_main(struct multiboot *mboot_ptr);
 
-	/* Port I/O*/
-	unsigned char (*inportb)(unsigned int port);
-	void (*outportb)(unsigned int port, unsigned char data);
-	unsigned char (*inportw)(unsigned int port);
-	void (*outportw)(unsigned int port, unsigned short data);
-	unsigned char (*inportl)(unsigned int port);
-	void (*outportl)(unsigned int port, unsigned long data);
+/* Port I/O */
+unsigned char inportb(unsigned int port);
+void outportb(unsigned int port, unsigned char data);
+unsigned char inportw(unsigned int port);
+void outportw(unsigned int port, unsigned short data);
+unsigned char inportl(unsigned int port);
+void outportl(unsigned int port, unsigned long data);
 
-	/* MMIO */
-	unsigned char (*inmemb)(unsigned int address);
-	void (*outmemb)(unsigned int address, unsigned char data);
-	unsigned char (*inmemw)(unsigned int address);
-	void (*outmemw)(unsigned int address, unsigned short data);
-	unsigned char (*inmeml)(unsigned int address);
-	void (*outmeml)(unsigned int address, unsigned long data);
+/* MMIO */
+unsigned char inmemb(unsigned int address);
+void outmemb(unsigned int address, unsigned char data);
+unsigned char inmemw(unsigned int address);
+void outmemw(unsigned int address, unsigned short data);
+unsigned char inmem(unsigned int address);
+void outmeml(unsigned int address, unsigned long data);
 
-	/* Interrupts */
-	void (*irq_install_handler)(int irq, void *handler);
-	void (*irq_uninstall_handler)(int irq);
-	void (*hal_cli)();
-	void (*hal_sti)();
+/* Interrupts */
+void irq_install_handler(int irq, void *handler);
+void irq_uninstall_handler(int irq);
+void hal_cli();
+void hal_sti();
 
-	/* Timer */
-	void (*sleep)(int sec);
+/* Timer */
+void sleep(int sec);
 
-	/* Physical memory manager */
-	unsigned int (*pmm_alloc_page)();
-	void (*pmm_free_page)(unsigned int address);
+/* Physical memory manager */
+unsigned int pmm_alloc_page();
+void pmm_free_page(unsigned int address);
 
-	/* Virtual memory manager */
-	void  (*map_page)(void *dir, unsigned int virtual_address, unsigned int physical_address, unsigned int flags);
-	void  (*unmap_page)(void *dir, unsigned int virtual_address);
-	void  (*map_kernel)(void *dir);
-	void* (*clone_address_space)(void *src);
-	void* (*create_address_space)();
-	void  (*switch_address_space)(void *dir);
+/* Virtual memory manager */
+void map_page(void *dir, unsigned int virtual_address, unsigned int physical_address, unsigned int flags);
+void unmap_page(void *dir, unsigned int virtual_address);
+void map_kernel(void *dir);
+void* clone_address_space(void *src);
+void* create_address_space();
+void switch_address_space(void *dir);
 
-	/* Multitasking */
-	void  (*task_switch_stub)(void *context);
+/* Multitasking */
+void task_switch_stub(void *context);
 
-	/* Syscalls */
-	void (*hal_init_syscalls)();
-	void (*syscall_install_handler)(int syscall, void *handler);
-} hal_t;
-
-hal_t *get_hal();
-void set_hal(hal_t *hal);
-
-/* NOTE: this is only temporary */
-void load_hal();
+/* Syscalls */
+void hal_init_syscalls();
+void syscall_install_handler(int syscall, void *handler);
 
 #endif
