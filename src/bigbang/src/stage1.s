8870beebeb5c1bf68aa25c0fc02c4ad247b757ca
Noah Singer <singerng@gmail.com>
2013-06-21 12:01:52 -0400

    Did a bit of VMM debugging and worked on bigbang

--- src/bigbang/src/stage1.s
+++ src/bigbang/src/stage1.s
@@ -2,7 +2,9 @@
 [ORG 0x7C00]
 [BITS 16]
 
-mov	ax, 0x4F00		; destination segment
+cli
+
+mov	ax, 0x4000		; destination segment
 mov	es, ax			; is stored in es
 mov	ah, 0x02		; function = 02h to read
 mov al, 1			; read one sector
@@ -14,22 +16,24 @@ inc	cl				; make it one-based
 mov	dh,	0			; head number 0
 mov	dl, 0x80		; goes in dh
 
-mov	bx, 0		; offset goes in bx
+mov	bx, 0x0000			; offset goes in bx
 ; stage2 will be stored at 0x4444:0x0000 (es:bx)
 
 ; Call the BIOS to read the disk
 int	0x13
 
 ; If the carry flag was set, then there was an error.
-; jc eternal
+jc eternal
 
 ; Otherwise, jump to our code
-; jmp 0x4F00:0x18
-mov ax, 0xDEAD
-mov bx, 0xBEEF
+jmp 0x4000:0x0000
 
 ; Eternal loop
 eternal:
+	mov ax, 0xDEAD
+	mov bx, 0xDEAD
+	mov cx, 0xDEAD
+	mov dx, 0xDEAD
 	jmp eternal
 
 ; Fill up the rest of the MBR boot sector with zeroes
