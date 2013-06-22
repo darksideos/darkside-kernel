a323e3ad03d1bcba4a8ff38a62811937faa16c27
George Klees <george.t.klees@gmail.com>
2013-06-22 16:06:14 -0400

    Added the OS info struct to stage3

--- bigbang/src/stage3/loader.s
+++ bigbang/src/stage3/loader.s
@@ -5,6 +5,8 @@ section .text
 global start
 start:
 	mov esp, stack	; Set up the stack
+	
+	push ebx		; Push the OS info structure
 	extern main		; Tell the linker that main is in another file
 	call main		; Call our main function
 
