85f0cac9de0013362a0cb409e8fa012ee57c9190
Noah Singer <singerng@gmail.com>
2013-06-24 21:17:08 -0400

    Working on stage3

--- bigbang/src/stage3/main.c
+++ bigbang/src/stage3/main.c
@@ -1,6 +1,40 @@
+#include <placement.h>
+#include <vga.h>
+
 void main(unsigned int *os_info)
 {
-	*((unsigned int*) 0xB8000) = 0xFFFF;
+	init_text_mode(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
+	
+	/* Doesn't work */
+	unsigned char *str = "hello, world!";
+	while((*str) != '\0')
+	{
+		putch(*str);
+		putch('Q');
+		++str;
+	}
+	
+	putch('\n');
+	
+	/* Doesn't work */
+	str = "hello, world!";
+	unsigned int index = 0;
+	while(str[index] != '\0')
+	{
+		putch(str[index]);
+		putch('Q');
+		index++;
+	}
+	
+	putch('\n');
+	
+	/* Doesn't work */
+	str = "hello, world!";
+	for(index = 0; index < 13; index++)
+	{
+		putch(str[index]);
+		putch('A' + index);
+	}
 	
 	while(1);
 }
---
+++ bigbang/src/stage3/placement.c
@@ -0,0 +1,10 @@
+#include <placement.h>
+
+unsigned int placement_address = 0xA000;
+
+unsigned int kmalloc(unsigned int size)
+{
+	unsigned int temp = placement_address;
+	placement_address += size;
+	return temp;
+}
