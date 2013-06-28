040509c3cfe274bca2f8d24b9b51bf95b5d25fdf
Noah Singer <singerng@gmail.com>
2013-06-28 15:59:40 -0400

    IDE/ATA works in bigbang

---
+++ bigbang/stage3/include/storage/ata.h
@@ -0,0 +1,10 @@
+#ifndef __ATA_H
+#define __ATA_H
+
+/* Read and write sectors from and to an ATA drive in PIO mode */
+unsigned char *lba28_sector_read_pio(unsigned char drive, unsigned int addr, unsigned int numsectors);
+void lba28_sector_write_pio(unsigned char drive, unsigned int addr, unsigned int numsectors, unsigned char *buffer);
+unsigned char *lba48_sector_read_pio(unsigned char drive, unsigned int addr, unsigned short numsectors);
+void lba48_sector_write_pio(unsigned char drive, unsigned int addr, unsigned short numsectors, unsigned char *buffer);
+
+#endif
