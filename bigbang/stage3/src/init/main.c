6d1f748e88427449767b74bd2bd38608f643ef9e
Noah Singer <singerng@gmail.com>
2013-06-29 19:57:28 -0400

    Printing names of files on the hard drive

--- bigbang/stage3/src/init/main.c
+++ bigbang/stage3/src/init/main.c
@@ -12,12 +12,19 @@ void main(unsigned int *os_info)
 	partition_t *part = get_mbr_partition(0, get_active_mbr_entry(0));
 	fat_BPB_t *bpb = read_BPB(part);
 	
+	kprintf("Calculating root cluster.\n");
 	unsigned char *root = read_root_cluster(bpb, part);
 	
+	kprintf("Starting dumping the stuff.\n");
 	int index;
 	for(index = 0; index < 10; index++)
 	{
-		kprintf("%c", ext->volume_label[index]);
+		int index2;
+		for(index2 = 0; index2 < 11; index2++)
+		{
+			kprintf("%c", root[index * 32 + index2]);
+		}
+		kprintf("\n");
 	}
 	
 	while(1);
