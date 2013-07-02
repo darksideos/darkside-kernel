26df81ffb6a6856b7bae973a8bc28eb93678f010
Noah Singer <singerng@gmail.com>
2013-07-02 15:09:40 -0400

    Started on ext2

--- bigbang/stage3/include/storage/ata.h
+++ bigbang/stage3/include/storage/ata.h
@@ -1,6 +1,8 @@
 #ifndef __ATA_H
 #define __ATA_H
 
+#define BYTES_PER_SECTOR		512
+
 /* Read and write sectors from and to an ATA drive in PIO mode */
 unsigned char *lba28_sector_read_pio(unsigned char drive, unsigned int addr, unsigned int numsectors);
 void lba28_sector_write_pio(unsigned char drive, unsigned int addr, unsigned int numsectors, unsigned char *buffer);
