8fa684c718abb050bef078ee17273ca81fbe02e8
Noah Singer <singerng@gmail.com>
2013-07-03 13:30:46 -0400

    Ext2 reading works completely

--- bigbang/stage3/src/init/main.c
+++ bigbang/stage3/src/init/main.c
@@ -16,14 +16,16 @@ void main(unsigned int *os_info)
 	unsigned int test = ext2_finddir(part, superblock, root_inode, "test.txt");
 	inode_t *test_inode = read_inode(part, superblock, test);
 	unsigned char *data = test_inode;
-	unsigned char *test_data = kmalloc(test_inode->low_size);
-	kprintf("Size: %d\n", test_inode->low_size);
-	ext2_read(part, superblock, test_inode, test_data, test_inode->low_size);
+	
+	unsigned int size = 1024 * 12 + 1;
+	
+	unsigned char *test_data = kmalloc(size);
+	ext2_read(part, superblock, test_inode, test_data, size);
 	
 	int index;
-	for(index = 0; index < test_inode->low_size; index++)
+	for(index = 0; index < size; index++)
 	{
-		kprintf("%c", test_data[index]);
+		//kprintf("%c", test_data[index]);
 	}
 	
 	while(1);
