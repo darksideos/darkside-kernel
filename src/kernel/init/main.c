be1e34d00ad0db74857d5344cd87f6169b2cb223
George Klees <george.t.klees@gmail.com>
2013-07-05 15:14:39 -0400

    Fixed pointer math problems in bigbang's ELF code

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -2,6 +2,7 @@
 #include <kernel/mm/heap.h>
 #include <drivers/graphics/vga.h>
 #include <lib/libc/stdint.h>
+#include <lib/libadt/list.h>
 
 void kernel_main(struct multiboot *mboot_ptr)
 {
@@ -13,6 +14,20 @@ void kernel_main(struct multiboot *mboot_ptr)
 
 	/* Initialize the kernel heap */
 	init_kheap();
+
+	/* List test code */
+	list_t list = create_list();
+
+	append_list(list, 1);
+	append_list(list, 2);
+	append_list(list, 3);
+
+	uint32_t zero, one, two;
+	zero = get_list(list, 0);
+	one = get_list(list, 1);
+	two = get_list(list, 2);
+
+	kprintf("0 = %d, 1 = %d, 2 = %d\n", zero, one, two);
 
 	while(1);
 }
