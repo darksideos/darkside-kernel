9cfd22cc0306486d11488f33c6dd12b843b89311
Noah Singer <singerng@gmail.com>
2013-07-06 12:29:30 -0400

    Added a cygwin clause to updateimage.sh

--- img/updateimage.sh
+++ img/updateimage.sh
@@ -7,20 +7,25 @@
 # 
 # fi
 
-if [ "$(uname -s)" = "Darwin" ]
+if [ "$(uname -s)" == "Darwin" ]
 	then
 		sudo fuse-ext2 -o rw,force ext2.img /mnt/hdd
 		sudo cp ../build-i386/kernel-i386.elf /mnt/hdd/boot
 		sudo hdiutil eject /mnt/hdd
 fi
 
-if [ "$(uname -s)" = "Linux" ]
+if [ "$(uname -s)" == "Linux" ]
 	then
 		sudo losetup /dev/loop0 ext2.img
 		sudo mount /dev/loop0 /mnt/hdd
 		sudo cp ../build-i386/kernel-i386.elf /mnt/hdd/boot
 		sudo umount /dev/loop0
 		sudo losetup -d /dev/loop0
+fi
+
+if [ "$(uname -s)" == "CYGWIN*" ]
+	then
+		pause "Waiting for kernel-i386.elf to be copied into ext2.img"
 fi
 
 dd if=ext2.img of=os.img bs=512 seek=2048 conv=notrunc
