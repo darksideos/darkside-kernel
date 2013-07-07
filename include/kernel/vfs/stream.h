89a3899500638da749347c7f4bae5dbaa046b893
George Klees <george.t.klees@gmail.com>
2013-07-06 21:11:18 -0400

    More console edits

--- include/kernel/vfs/stream.h
+++ include/kernel/vfs/stream.h
@@ -20,8 +20,18 @@ typedef struct outstream
 	int32_t (*seek)(struct instream *stream, uint32_t offset, int32_t origin);
 } outstream_t;
 
-/* Create input and output streams */
+/* Create, open, close, read from, and seek an input stream */
 instream_t *instream_create();
+int32_t instream_open(instream_t *stream, fs_node_t *file);
+int32_t instream_close(instream_t *stream);
+uint32_t instream_read(instream_t *stream, uint8_t *buffer, uint32_t length);
+int32_t instream_seek(instream_t *stream, uint32_t offset, int32_t origin);
+
+/* Create, open, close, write to, and seek an output stream */
 outstream_t *outstream_create();
+int32_t outstream_open(outstream_t *stream, fs_node_t *file);
+int32_t outstream_close(outstream_t *stream);
+uint32_t outstream_write(outstream_t *stream, uint8_t *buffer, uint32_t length);
+int32_t outstream_seek(outstream_t *stream, uint32_t offset, int32_t origin);
 
 #endif
