bb0ddf965a0fa5ba96909721ddedfab55c456d59
Noah Singer <singerng@gmail.com>
2013-07-08 12:10:24 -0400

    Added C code for the E820 BIOS memory map

--- bigbang/stage3/src/init/main.c
+++ bigbang/stage3/src/init/main.c
@@ -5,12 +5,22 @@
 #include <storage/partition.h>
 #include <fs/ext2.h>
 #include <elf/elf.h>
+#include <mm/os_info_x86.h>
 
 extern unsigned int *pd;
 
-void main(unsigned int *os_info)
+void main(os_info_x86_t *os_info_x86)
 {
 	init_text_mode(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
+	
+	kprintf("There are %04X entries at %08X\n", sizeof(e820_entry_t), os_info_x86->mem_map);
+	int index;
+	for(index = 0; index < os_info_x86->mem_map_num_entries; index++)
+	{
+		e820_entry_t *entry = ((e820_entry_t*) os_info_x86->mem_map) + index;
+		kprintf("Base %08X, length %08X, type %08X\n", entry->base_low, entry->length_low, entry->type);
+	}
+	while(1);
 	
 	partition_t *part = get_mbr_partition(0, get_active_mbr_entry(0));
 	
@@ -24,8 +34,6 @@ void main(unsigned int *os_info)
 	
 	elf_header_t *kernel_elf = kmalloc(kernel_inode->low_size);
 	ext2_read(part, superblock, kernel_inode, kernel_elf, kernel_inode->low_size);
-	
-	elf_read_header(kernel_elf);
 	
 	elf_run_executable(kernel_elf);
 	
