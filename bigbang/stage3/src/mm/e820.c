d6064d9dea35130a8133971b5871a79c15328262
Noah Singer <singerng@gmail.com>
2013-07-10 08:34:24 -0400

    E820 code experiencing weird bug

--- bigbang/stage3/src/mm/e820.c
+++ bigbang/stage3/src/mm/e820.c
@@ -7,9 +7,9 @@
 e820_entry_t* sort_memory_map(e820_entry_t* map, unsigned int num_entries)
 {
 	int index;
-	for(index = 0; index < num_entries - 1; index++)
+	for (index = 0; index < num_entries - 1; index++)
 	{
-		if(map[index].base > map[index + 1].base)
+		if (map[index].base > map[index + 1].base)
 		{
 			e820_entry_t temp = map[index];
 			map[index] = map[index + 1];
@@ -31,7 +31,7 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 	e820_linked_entry_t *first_linked = linked;
 	
 	int index;
-	for(index = 0; index < os_info->mem_map_entries; index++)
+	for (index = 0; index < os_info->mem_map_entries; index++)
 	{
 		linked->base = sorted[index].base;
 		linked->length = sorted[index].length;
@@ -39,7 +39,7 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 		linked->attrib = sorted[index].attrib;
 		linked->spec_flags = 0;
 		
-		if(index == os_info->mem_map_entries - 1)
+		if (index == os_info->mem_map_entries - 1)
 		{
 			linked->next = 0;
 		}
@@ -54,12 +54,12 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 	
 	/* Get rid of unused entries */
 	linked = first_linked;
-	while(linked != 0)
+	while (linked != 0)
 	{
-		if(linked->length == 0 || !linked->attrib & E820_ACPI3_KEEP)
+		if (linked->length == 0 || !linked->attrib & E820_ACPI3_KEEP)
 		{
 			/* Ignore this entry */
-			if(linked->prev)
+			if (linked->prev)
 			{
 				linked->prev->next = linked->next;
 			}
@@ -69,7 +69,7 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 				first_linked = first_linked->next;
 			}
 			
-			if(linked->next)
+			if (linked->next)
 			{
 				linked->next->prev = linked->prev;
 			}
@@ -80,9 +80,9 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 	
 	/* Change unknown type entries to reserved (2) */
 	linked = first_linked;
-	while(linked != 0)
+	while (linked != 0)
 	{
-		if(linked->type < E820_FREE || linked->type > E820_BAD)
+		if (linked->type < E820_FREE || linked->type > E820_BAD)
 		{
 			linked->type = E820_RESERVED;
 			linked->spec_flags |= E820_SPEC_INVALID;
@@ -91,14 +91,16 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 		linked = linked->next;
 	}
 	
+	/* Here, we need to write the code to mark overlapping areas as reserved and invalid */
+	
 	/* We want a contiguous map, so insert entries between non-contiguous entries */
 	linked = first_linked;
-	while(linked != 0)
+	while (linked != 0)
 	{
-		if(linked->next)
+		if (linked->next)
 		{
 			/* If it's non-contiguous */
-			if(linked->base + linked->length < linked->next->base)
+			if (linked->base + linked->length < linked->next->base)
 			{
 				/* Insert an entry */
 				e820_linked_entry_t *new_entry = kmalloc(sizeof(e820_linked_entry_t));
@@ -119,7 +121,7 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 	}
 	
 	/* Make it contiguous from the beginning */
-	if(first_linked->base != 0x00)
+	if (first_linked->base != 0x00)
 	{
 		e820_linked_entry_t *new_entry = kmalloc(sizeof(e820_linked_entry_t));
 		new_entry->base = 0;
@@ -134,11 +136,85 @@ mem_map_entry_t* e820_convert_mem_map(os_info_x86_t *os_info, unsigned int *entr
 		first_linked = new_entry;
 	}
 	
+	/* Combine adjacent regions of the same type */
 	linked = first_linked;
-	while(linked != 0)
+	while (linked != 0)
 	{
-		kprintf("Base %08X, length %08X, type %08X\n", (unsigned int) linked->base, (unsigned int) linked->length, linked->type);
+		if (linked->next)
+		{
+			if (linked->type == linked->next->type && linked->attrib == linked->next->attrib && linked->spec_flags == linked->next->spec_flags)
+			{
+				linked->length += linked->next->length;
+				linked->next->next->prev = linked;
+				linked->next = linked->next->next;
+			}
+		}
+		
 		linked = linked->next;
 	}
-	while(1);
+
+	/* Find the number of entries in the list */
+	*entries = 0;
+	linked = first_linked;
+	while (linked != 0)
+	{
+		(*entries)++;
+		linked = linked->next;
+	}
+	
+	/* Translate the linked list back into an array */
+	mem_map_entry_t *mem_map = kmalloc(sizeof(mem_map_entry_t) * (*entries));
+	
+	linked = first_linked;
+	for (index = 0; index < *entries; index++)
+	{
+		mem_map[index].length = linked->length;
+		
+		/* We start out assuming some stuff */
+		mem_map[index].flags |= MEM_MAP_FLAG_FREE;
+		mem_map[index].flags |= MEM_MAP_FLAG_VALID;
+		mem_map[index].flags |= MEM_MAP_FLAG_EXISTS;
+		mem_map[index].flags &= ~MEM_MAP_FLAG_ACPI;
+		
+		if (linked->type == E820_FREE)
+		{
+			mem_map[index].flags |= MEM_MAP_FLAG_FREE;
+		}
+		else
+		{
+			mem_map[index].flags &= ~MEM_MAP_FLAG_FREE;
+		}
+		
+		/* Temporary */
+		if (linked->attrib & E820_ACPI3_NONVOLATILE)
+		{
+			mem_map[index].flags &= ~MEM_MAP_FLAG_FREE;
+		}
+		
+		if (linked->spec_flags & E820_SPEC_INVALID || linked->type == E820_BAD)
+		{
+			mem_map[index].flags &= ~MEM_MAP_FLAG_VALID;
+		}
+		
+		if (linked->spec_flags & E820_SPEC_NONEXISTANT)
+		{
+			mem_map[index].flags &= ~MEM_MAP_FLAG_EXISTS;
+			mem_map[index].flags &= ~MEM_MAP_FLAG_VALID;
+		}
+		
+		if (linked->type == E820_ACPI_VOLATILE)
+		{
+			mem_map[index].flags |= MEM_MAP_FLAG_ACPI;
+			mem_map[index].flags |= MEM_MAP_FLAG_ACPI_VOLATILE;
+		}
+		else if (linked->type == E820_ACPI_NONVOLATILE)
+		{
+			mem_map[index].flags |= MEM_MAP_FLAG_ACPI;
+			mem_map[index].flags &= ~MEM_MAP_FLAG_ACPI_VOLATILE;
+		}
+			
+		linked = linked->next;
+	}
+	
+	return mem_map;
 }
--- src/hal/i386/asm/loader.s
+++ src/hal/i386/asm/loader.s
@@ -6,9 +6,15 @@ section .text
 ; This is the entry point of our kernel, it is referenced in the linker script
 global start
 start:
+	pop ebx						; Save the os_info pointer
+eternal:
+	jmp eternal
+	
 	mov esp, stack
     
 	extern kernel_main 			; Tell NASM that our higher half function is in another file
+	
+	push ebx					; Push the os_info pointer
 	call kernel_main   			; Call our higher half function
 
 section .bss
