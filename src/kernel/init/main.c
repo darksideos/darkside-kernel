404acb1054d3b8fda8aacd4cfe766b95cdb0b677
Noah Singer <singerng@gmail.com>
2013-07-11 09:46:05 -0400

    Added E820 code to pmm.c

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -3,23 +3,15 @@
 #include <drivers/graphics/vga.h>
 #include <lib/libc/stdint.h>
 #include <lib/libc/string.h>
-#include <kernel/mm/os_info.h>
+#include <kernel/init/os_info.h>
 
 void kernel_main(os_info_t *os_info)
 {
 	/* Start the VGA text mode driver */
 	init_text_mode(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
 	
-	kprintf("Address: %08X\n", os_info);
-	int index;
-	for(index = 0; index < os_info->mem_map_entries; index++)
-	{
-		kprintf("Length %08X, flags %08X\n", (unsigned int) os_info->mem_map[index].length, os_info->mem_map[index].flags);
-	}
-	while(1);
-	
 	/* Call the HAL main function to initialize the CPU */
-	hal_main(0);
+	hal_main(os_info);
 
 	/* Initialize the kernel heap */
 	init_kheap();
