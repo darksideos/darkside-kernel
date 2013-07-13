dd49d4a6e5022c57ec39d67a13b749fdfc7aa2de
George Klees <george.t.klees@gmail.com>
2013-07-12 22:16:12 -0400

    Added unregister_blockdev()

--- src/hal/i386/asm/loader.s
+++ src/hal/i386/asm/loader.s
@@ -8,29 +8,13 @@ global start
 start:
 	pop ebx						; Save the os_info pointer
 	
-	mov esp, stack
+	mov esp, stack				; Set up a new stack
     
-	extern kernel_main 			; Tell NASM that our higher half function is in another file
+	extern kernel_main 			; Tell NASM that our kernel main function is in another file
 	
 	push ebx					; Push the os_info pointer
-	call kernel_main   			; Call our higher half function
+	call kernel_main   			; Call our kernel main function
 
 section .bss
 	resb 65536               ; This reserves 64KBytes of memory here
 stack:
-
-; Initial page structures
-
-global pd
-global pt_lower
-global pt_higher
-global pt_bitmap
-
-pd:
-	resb 4096
-pt_lower:
-	resb 4096
-pt_higher:
-	resb 4096
-pt_bitmap:
-	resb 4096
