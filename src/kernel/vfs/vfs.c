fd6ed88752da86d6acd104c63afc1ef7d79c2429
George Klees <george.t.klees@gmail.com>
2013-07-13 13:31:47 -0400

    Added registering/unregistering filesystems and mounting/unmounting filesystems

--- src/kernel/vfs/vfs.c
+++ src/kernel/vfs/vfs.c
@@ -3,6 +3,7 @@
 #include <lib/libadt/list.h>
 #include <kernel/mm/heap.h>
 #include <kernel/vfs/vfs.h>
+#include <kernel/vfs/stat.h>
 
 /* Filesystems and mountpoints */
 list_t filesystems, mountpoints;
@@ -10,13 +11,113 @@ list_t filesystems, mountpoints;
 /* Root of the VFS */
 inode_t *vfs_root;
 
+/* Filesystem information structure */
+typedef struct fs_info
+{
+	uint8_t *name;
+	filesystem_t *fs;
+} fs_info_t;
+
 /* Register a filesystem */
+int32_t register_filesystem(filesystem_t *fs, uint8_t *name)
+{
+	/* Create a filesystem information structure */
+	fs_info_t fsi;
+	fsi.name = name;
+	fsi.fs = fs;
+
+	/* Add it to the list of filesystems */
+	list_append(filesystems, &fsi);
+
+	return 0;
+}
 
 /* Unregister a filesystem */
+int32_t unregister_filesystem(uint8_t *name)
+{
+	/* Go through each registered filesystem */
+	uint32_t i;
+	for (i = 0; i < list_length(filesystems); i++)
+	{
+		/* Get the filesystem identification */
+		fs_info_t *fsi = list_get(filesystems, i);
+
+		/* Is its name the one we're looking for? */
+		if (strequal(fsi->name, name))
+		{
+			/* Remove it from the list */
+			list_remove(filesystems, i);
+			return 0;
+		}
+	}
+	return 1;
+}
 
 /* Mount a filesystem */
+int32_t vfs_mount(uint8_t *name, inode_t *node)
+{
+	/* Is the filesystem already mounted? */
+	uint32_t i;
+	for (i = 0; i < list_length(mountpoints); i++)
+	{
+		/* Get the mountpoint */
+		mountpoint_t *mp = *(mountpoint_t**) list_get(mountpoints, i);
+
+		/* Does the node match */
+		if (mp->node == node)
+		{
+			return 1;
+		}
+	}
+
+	/* Create a mountpoint structure */
+	mountpoint_t *mp = kmalloc(sizeof(mountpoint_t));
+
+	/* Go through each registered filesystem */
+	for (i = 0; i < list_length(filesystems); i++)
+	{
+		/* Get the filesystem identification */
+		fs_info_t *fsi = list_get(filesystems, i);
+
+		/* Is its name the one we're looking for? */
+		if (strequal(fsi->name, name))
+		{
+			/* Set up the mountpoint */
+			mp->node = node;
+			mp->fs = fsi->fs;
+
+			/* Add it to the list */
+			list_append(mountpoints, &mp);
+		}
+	}
+	kfree(mp);
+	return 1;
+}
 
 /* Unmount a filesystem */
+int32_t vfs_unmount(inode_t *node)
+{
+	/* Is the filesystem mounted? */
+	uint32_t i;
+	for (i = 0; i < list_length(mountpoints); i++)
+	{
+		/* Get the mountpoint */
+		mountpoint_t *mp = *(mountpoint_t**) list_get(mountpoints, i);
+
+		/* Does the node match */
+		if (mp->node == node)
+		{
+			/* Remove the mountpoint from the list */
+			list_remove(mountpoints, i);
+
+			/* Free the mountpoint's memory */
+			kfree(mp);
+
+			return 0;
+		}
+	}
+	return 0;
+}
 
 /* Open a file */
 inode_t *vfs_open(uint8_t *path)
@@ -46,7 +147,7 @@ uint64_t vfs_read(inode_t *node, uint8_t *buffer, uint64_t offset, uint64_t leng
 {
 	if (node->type != INODE_TYPE_DIR)
 	{
-		return node->mountpoint->fs->read(node->mountpoint->fs, node, buffer, offset, length);
+		return node->fs->read(node->fs, node, buffer, offset, length);
 	}
 
 	return 0;
@@ -57,7 +158,7 @@ uint64_t vfs_write(inode_t *node, uint8_t *buffer, uint64_t offset, uint64_t len
 {
 	if (node->type != INODE_TYPE_DIR)
 	{
-		return node->mountpoint->fs->write(node->mountpoint->fs, node, buffer, offset, length);
+		return node->fs->write(node->fs, node, buffer, offset, length);
 	}
 
 	return 0;
@@ -68,7 +169,7 @@ list_t vfs_readdir(inode_t *dir)
 {
 	if (node->type == INODE_TYPE_DIR)
 	{
-		return node->mountpoint->fs->readdir(node->mountpoint->fs, dir);
+		return node->fs->readdir(node->fs, dir);
 	}
 }
 
@@ -77,7 +178,7 @@ inode_t *vfs_finddir(inode_t *dir, uint8_t *name)
 {
 	if (node->type == INODE_TYPE_DIR)
 	{
-		return node->mountpoint->fs->finddir(node->mountpoint->fs, dir, name);
+		return node->fs->finddir(node->fs, dir, name);
 	}
 
 	return 0;
@@ -88,34 +189,34 @@ int32_t vfs_link(inode_t *node, uint8_t *newpath)
 {
 	if (node->type != INODE_TYPE_DIR)
 	{
-		return node->mountpoint->fs->link(node->mountpoint->fs, node, newpath);
+		return node->fs->link(node->fs, node, newpath);
 	}
 }
 
 /* Remove a directory entry */
 int32_t vfs_unlink(uint8_t *path)
 {
-	return node->mountpoint->fs->unlink(node->mountpoint->fs, path);
+	return node->fs->unlink(node->fs, path);
 }
 
 /* Create a new symbolic link to a file */
 int32_t vfs_symlink(inode_t *node, uint8_t *newpath)
 {
-	return node->mountpoint->fs->symlink(node->mountpoint->fs, node, newpath);
+	return node->fs->symlink(node->fs, node, newpath);
 }
 
 /* Rename a directory entry */
 int32_t vfs_rename(uint8_t *oldpath, uint8_t *newpath)
 {
-	return node->mountpoint->fs->rename(node->mountpoint->fs, oldpath, newpath);
+	return node->fs->rename(node->fs, oldpath, newpath);
 }
 
 /* Issue a device specific request */
 int32_t vfs_ioctl(struct inode *node, int32_t request, uint8_t *buffer, uint32_t length)
 {
-	if (node->mountpoint->fs->ioctl)
+	if (node->fs->ioctl)
 	{
-		return node->mountpoint->fs->ioctl(node->mountpoint->fs, node, request, buffer, length);
+		return node->fs->ioctl(node->fs, node, request, buffer, length);
 	}
 
 	return -1;
@@ -124,17 +225,21 @@ int32_t vfs_ioctl(struct inode *node, int32_t request, uint8_t *buffer, uint32_t
 /* Initialize the VFS */
 void init_vfs()
 {
+	/* Create the filesystems and mountpoints lists */
+	filesystems = list_create(sizeof(fs_info_t), 4);
+	mountpoints = list_create(sizeof(mountpoint_t*), 4);
+
 	/* Create the root node */
 	vfs_root = (inode_t*) kmalloc(sizeof(inode_t));
 
 	/* Fill out it's information */
-	vfs_root->mountpoint = 0;
+	vfs_root->filesystem = 0;
 	vfs_root->type = INODE_TYPE_DIR;
 	vfs_root->parent = 0;
 	vfs_root->children = list_create(sizeof(inode_t), 4);
 
 	vfs_root->size = 0;
-	vfs_root->mode = 0777;
+	vfs_root->mode = S_IRWXU | S_IRWXG | S_IRWXO;
 	vfs_root->nlink = 0;
 	vfs_root->uid = 0;
 	vfs_root->gid = 0;
