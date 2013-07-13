a8bd1052fd22790146bcc3cec1a58beed97d7cee
Noah Singer <singerng@gmail.com>
2013-07-12 20:03:03 -0400

    Fixed E820 code, builds and works correctly

--- include/lib/libc/math.h
+++ include/lib/libc/math.h
@@ -6,6 +6,6 @@
 uint64_t pow(uint64_t num, uint64_t exp);
 uint64_t ceil(uint64_t num, uint64_t div);
 uint64_t floor(uint64_t num, uint64_t div);
-uint64_t abs(uint64_t num);
+uint64_t abs(int64_t num);
 
 #endif
