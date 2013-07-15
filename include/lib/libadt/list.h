73a20537c7e843416392c46acd004a911f66a11a
George Klees <george.t.klees@gmail.com>
2013-07-15 18:58:39 -0400

    Fixed problems with ADT functions (most take pointers now)

--- include/lib/libadt/list.h
+++ include/lib/libadt/list.h
@@ -14,11 +14,11 @@ typedef struct list
 
 /* List functions */
 list_t list_create(uint32_t itemsz, uint32_t nitems);
-void list_destroy(list_t list);
-uint32_t list_append(list_t list, void *item);
-void list_remove(list_t list, uint32_t index);
-void *list_get(list_t list, uint32_t index);
-void list_set(list_t list, uint32_t index, void *item);
-uint32_t list_length(list_t list);
+void list_destroy(list_t *list);
+uint32_t list_append(list_t *list, void *item);
+void list_remove(list_t *list, uint32_t index);
+void *list_get(list_t *list, uint32_t index);
+void list_set(list_t *list, uint32_t index, void *item);
+uint32_t list_length(list_t *list);
 
 #endif
