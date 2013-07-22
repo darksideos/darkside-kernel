718a8101b31a02ad4244b1a60a6f5863e9a595ee
George Klees <gksharkboy@gmail.com>
2013-07-21 20:40:20 -0400

    Added character device functions

--- src/kernel/device/dev.c
+++ src/kernel/device/dev.c
@@ -1,11 +1,59 @@
 #include <lib/libc/types.h>
-#include <kernel/console/log.h>
+#include <kernel/console/kprintf.h>
 #include <kernel/device/dev.h>
 #include <kernel/mm/heap.h>
 #include <kernel/vfs/vfs.h>
 
 /* Dev filesystem */
 filesystem_t *vfs_dev;
+
+/* Create a character device structure */
+chardev_t *chardev_create()
+{
+	/* Create a character device and make sure it's 0 */
+	chardev_t *chardev = (chardev_t*) kmalloc(sizeof(chardev_t));
+	memset(chardev, 0, sizeof(chardev_t));
+}
+
+/* Destroy a character device structure */
+void chardev_destroy(chardev_t *chardev)
+{
+	/* Set its fields to 0 */
+	chardev->id = 0;
+	chardev->data = 0;
+
+	chardev->read = 0;
+	chardev->write = 0;
+	chardev->ioctl = 0;
+
+	/* Destroy the character device structure */
+	kfree(chardev);
+}
+
+/* Initialize a character device structure */
+void chardev_init(chardev_t *chardev, dev_t id)
+{
+	/* Set the device ID */
+	chardev->id = id;
+}
+
+/* Read from a character device */
+uint64_t chardev_read(chardev_t *chardev, uint8_t *buffer, uint64_t length)
+{
+	return chardev->read(chardev, buffer, offset, length);
+}
+
+/* Write to a character device */
+uint64_t chardev_write(chardev_t *chardev, uint8_t *buffer, uint64_t length)
+{
+	return chardev->write(chardev, buffer, offset, length);
+}
+
+/* Issue a device specific request */
+int32_t chardev_ioctl(chardev_t *chardev, int32_t request, uint8_t *buffer, uint32_t length)
+{
+	return chardev->ioctl(chardev, request, buffer, length);
+}
 
 /* Create a block device structure */
 blockdev_t *blockdev_create()
@@ -53,6 +101,50 @@ uint64_t blockdev_write(blockdev_t *blockdev, uint8_t *buffer, uint64_t offset, 
 int32_t blockdev_ioctl(blockdev_t *blockdev, int32_t request, uint8_t *buffer, uint32_t length)
 {
 	return blockdev->ioctl(blockdev, request, buffer, length);
+}
+
+/* Register a character device */
+void register_chardev(chardev_t *chardev, uint8_t *name)
+{
+	/* Create a new dev node for the character device */
+	inode_t *node = (inode_t*) kmalloc(sizeof(inode_t));
+	int result = vfs_dev->mknod(vfs_dev, name, INODE_TYPE_CHARDEV, chardev->id, inode);
+
+	if (result == -1)
+	{
+		kprintf(LOG_ERROR, "Failed to register character device (unable to create inode for device)");
+	}
+
+	/* Set the inode specific data to the character device */
+	node->data = (void*) chardev;
+}
+
+/* Unregister a character device */
+void unregister_chardev(chardev_t *chardev, uint8_t *name)
+{
+	/* Find the inode for the character device by name */
+	inode_t *node = (inode_t*) kmalloc(sizeof(inode_t));
+	int result = vfs_dev->finddir(vfs_dev, vfs_dev->root, name, node);
+
+	if (result == -1)
+	{
+		kprintf(LOG_ERROR, "Failed to unregister character device (unable to find dev entry)");
+		return;
+	}
+
+	/* Make sure it matches with the character device */
+	if (node->data == chardev && node->id == chardev->id)
+	{
+		/* Unlink the node from the dev directory */
+		result = vfs_dev->unlink(vfs_dev, name);
+		if (result == -1)
+		{
+			kprintf(LOG_ERROR, "Failed to unregister character device (unable to unlink inode)");
+			return;
+		}
+	}
+
+	kprintf(LOG_ERROR, "Failed to unregister character device (inode and character device do not match)");
 }
 
 /* Register a block device */
