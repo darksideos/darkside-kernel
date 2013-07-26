54bddd76bce3d3698fc8f4a0a4ef2af9fc899c69
George Klees <george.t.klees@gmail.com>
2013-07-26 10:42:01 -0400

    Finished all MP structures except extended table (likely not needed)

--- src/drivers/motherboard/legacy/mptable.c
+++ src/drivers/motherboard/legacy/mptable.c
@@ -46,7 +46,7 @@ typedef struct mp_table
 	/* Number of entries in the base table */
 	uint16_t entry_count;
 
-	/* Local APIC address for every CPU */
+	/* Local APIC base address for every CPU */
 	uint32_t lapic_phys_start;
 
 	/* Length of the extended table */
@@ -74,10 +74,16 @@ typedef struct mp_cpu_entry
 	/* Is the processor the BSP? */
 	uint8_t bsp				: 1;
 
+	/* Unused */
+	uint8_t unused1			: 6;
+
 	/* CPU signature */
 	uint8_t cpu_stepping	: 4;
 	uint8_t cpu_model		: 4;
 	uint8_t cpu_family		: 4;
+
+	/* Unused */
+	uint32_t unused2		: 20;
 
 	/* CPUID feature flags */
 	uint32_t cpuid_flags;
@@ -95,3 +101,97 @@ typedef struct mp_bus_entry
 	/* Bus type string */
 	uint8_t bus_type[6];
 } mp_bus_entry_t;
+
+/* MP table I/O APIC entry structure */
+typedef struct mp_ioapic_entry
+{
+	/* Entry type, must be 2 */
+	uint8_t entry_type;
+
+	/* I/O APIC ID */
+	uint8_t ioapic_id;
+
+	/* I/O APIC version */
+	uint8_t ioapic_version;
+
+	/* Is the I/O APIC usable? */
+	uint8_t usable;
+
+	/* I/O APIC base address */
+	uint32_t ioapic_phys_base;
+} mp_ioapic_entry_t;
+
+/* MP table I/O APIC IRQ assignment entry structure */
+typedef struct mp_ioapic_irq_entry
+{
+	/* Entry type, must be 3 */
+	uint8_t entry_type;
+
+	/* Interrupt type */
+	uint8_t interrupt_type;
+
+	/* Pin polarity */
+	uint8_t pin_polarity	: 2;
+
+	/* Trigger mode */
+	uint8_t trigger_mode	: 2;
+
+	/* Unused */
+	uint16_t unused			: 14;
+
+	/* Source bus ID */
+	uint8_t src_bus_id;
+
+	/* Source bus IRQ */
+	uint8_t src_bus_irq;
+
+	/* Destination I/O APIC ID */
+	uint8_t dest_ioapic_id;
+
+	/* Destination I/O APIC IRQ */
+	uint8_t dest_ioapic_irq;
+} mp_ioapic_irq_entry_t;
+
+/* MP table Local APIC IRQ assignment entry structure */
+typedef struct mp_lapic_irq_entry
+{
+	/* Entry type, must be 4 */
+	uint8_t entry_type;
+
+	/* Interrupt type */
+	uint8_t interrupt_type;
+
+	/* Pin polarity */
+	uint8_t pin_polarity	: 2;
+
+	/* Trigger mode */
+	uint8_t trigger_mode	: 2;
+
+	/* Unused */
+	uint16_t unused			: 14;
+
+	/* Source bus ID */
+	uint8_t src_bus_id;
+
+	/* Source bus IRQ */
+	uint8_t src_bus_irq;
+
+	/* Destination Local APIC ID */
+	uint8_t dest_lapic_id;
+
+	/* Destination Local APIC IRQ */
+	uint8_t dest_lapic_irq;
+} mp_lapic_irq_entry_t;
+
+/* MP table entry types */
+#define MP_ENTRY_TYPE_CPU			0
+#define MP_ENTRY_TYPE_BUS			1
+#define MP_ENTRY_TYPE_IOAPIC		2
+#define MP_ENTRY_TYPE_IOAPIC_IRQ	3
+#define MP_ENTRY_TYPE_LAPIC_IRQ		4
+
+/* MP table bus types */
+#define MP_BUS_TYPE_EISA	"EISA"
+#define MP_BUS_TYPE_ISA		"ISA"
+#define MP_BUS_TYPE_PCI		"PCI"
+#define MP_BUS_TYPE_PCMCIA	"PCMCIA"
