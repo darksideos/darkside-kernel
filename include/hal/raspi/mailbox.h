3c129a317107f479654ffd2b45ac74afff46ca73
Artemis Tosini <me@artem.ist>
2013-07-28 20:41:17 -0400

    Merge mailbox and framebuffer files

--- include/hal/raspi/mailbox.h
+++
@@ -1,9 +0,0 @@
-#include <lib/libc/stdint.h>
-
-#ifndef MAILBOX_H
-#define MAILBOX_H
-
-extern uint32_t read_mailbox(uint32_t channel);
-extern void write_mailbox(uint32_t channel, uint32_t data);
-
-#endif	/* MAILBOX_H */
