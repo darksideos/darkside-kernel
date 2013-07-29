91e55f64d6f3602fc1cae37c7cf01cbc62bdac29
George Klees <george.t.klees@gmail.com>
2013-07-28 22:59:32 -0400

    Added a few functions to the raspi VMM

--- src/hal/raspi/vmm.c
+++ src/hal/raspi/vmm.c
@@ -13,8 +13,43 @@ uint32_t page_size = 0x1000;
 /* Get the physical address mapping of a virtual address */
 
 /* Map a virtual address to a physical address */
+void map_page(uint32_t table, uint32_t virtual_address, uint32_t physical_address, bool present, bool rw, bool user, bool global)
+{
+	/* Construct the page flags */
+	uint32_t flags;
+
+	if (present)
+	{
+		flags |= PAGE_FLAG_PRESENT;
+	}
+
+	if (!global)
+	{
+		flags |= PAGE_FLAG_NOTGLOBAL;
+	}
+
+	/* Return the page that corresponds to the virtual address, creating it if it doesn't already exist */
+	page_t *page = get_page(table, virtual_address, true);
+
+	/* Map the page in the table to the physical address */
+	*((uint32_t*) page) = physical_address | flags;
+}
 
 /* Unmap a virtual address */
+void unmap_page(uint32_t table, uint32_t virtual_address)
+{
+	/* Return the page that corresponds to the virtual address */
+	page_t *page = get_page(table, virtual_address, false);
+
+	/* If the page already does not exist, return */
+	if (!page)
+	{
+		return;
+	}
+
+	/* Set the page to not present */
+	*((uint32_t*)page) = 0;
+}
 
 /* Create a new blank page table */
 
