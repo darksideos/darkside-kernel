9be92cada33431e47cb2a5b5b6d29c26929aa866
Artemis Tosini <me@artem.ist>
2013-07-28 20:41:17 -0400

    Merge mailbox and framebuffer files

--- include/drivers/graphics/raspi/textutils.h
+++
@@ -1,9 +0,0 @@
-#include <lib/libc/stdint.h>
-
-#ifndef TEXTUTILS_H
-#define TEXTUTILS_H
-
-extern int8_t *tohex(uint32_t value, uint32_t size);
-extern int8_t *todec(uint32_t value, int leading);
-
-#endif	/* TEXTUTILS_H */
