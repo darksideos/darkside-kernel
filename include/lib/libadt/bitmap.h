69562b2139740c5df8ca744fdd29fbcaa249d95f
George Klees <george.t.klees@gmail.com>
2013-07-30 09:33:36 -0400

    Finished bitmap ADT

--- include/lib/libadt/bitmap.h
+++ include/lib/libadt/bitmap.h
@@ -10,4 +10,16 @@ typedef struct bitmap
 	uint32_t max_bits;
 } bitmap_t;
 
+/* Initialize a bitmap */
+void bitmap_init(bitmap_t *bitmap, uint8_t *storage, uint32_t max_bits);
+
+/* Set a bit in a bitmap */
+void bitmap_set(bitmap_t *bitmap, uint32_t index);
+
+/* Clear a bit in a bitmap */
+void bitmap_clear(bitmap_t *bitmap, uint32_t index)
+
+/* Find the first clear bit in a bitmap */
+uint32_t bitmap_first_clear(bitmap_t *bitmap);
+
 #endif
