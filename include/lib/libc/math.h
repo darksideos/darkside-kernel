ab77ef1246917670062e9e6fb88351d1811fa42e
George Klees <george.t.klees@gmail.com>
2013-08-01 13:42:49 -0400

    Added log2() to math

--- include/lib/libc/math.h
+++ include/lib/libc/math.h
@@ -3,9 +3,15 @@
 
 #include <lib/libc/types.h>
 
+/* Exponential and logarithmic functions */
 uint64_t pow(uint64_t num, uint64_t exp);
-uint64_t ceil(uint64_t num, uint64_t div);
+uint64_t log2(uint64_t num);
+
+/* Rounding and remainder functions */
 uint64_t floor(uint64_t num, uint64_t div);
+uint64_t ceil(uint64_t num, uint64_t div);
+
+/* Absolute value */
 uint64_t abs(int64_t num);
 
 #endif
--- src/lib/libc/math.c
+++ src/lib/libc/math.c
@@ -4,6 +4,7 @@
 extern uint64_t __udivdi3(uint64_t num, uint64_t div);
 extern uint64_t __umoddi3(uint64_t num, uint64_t div);
 
+/* Raise a number to an exponent */
 uint64_t pow(uint64_t num, uint64_t exp)
 {
 	if(exp == 0)
@@ -16,16 +17,39 @@ uint64_t pow(uint64_t num, uint64_t exp)
 	}
 }
 
-uint64_t ceil(uint64_t num, uint64_t div)
+/* Calculate the binary logarithm of a number */
+uint64_t log2(uint64_t num)
 {
-	return (num % div == 0) ? num / div : ((num - num % div) / div + 1);
+	/* Find the number of leading zeroes */
+	uint64_t lz = 0;
+
+	for (uint8_t bit = 31; bit >= 0; bit--)
+	{
+		if ((num & (1 << bit)) == 1)
+		{
+			break;
+		}
+
+		lz++;
+	}
+
+	/* Calculate and return the binary logarithm */
+	return 63 - lz;
 }
 
+/* Divide a number by a divisor and round the result down */
 uint64_t floor(uint64_t num, uint64_t div)
 {
 	return (num % div == 0) ? num / div : ((num - num % div) / div);
 }
 
+/* Divide a number by a divisor and round the result up */
+uint64_t ceil(uint64_t num, uint64_t div)
+{
+	return (num % div == 0) ? num / div : ((num - num % div) / div + 1);
+}
+
+/* Calculate the absolute value of a number */
 uint64_t abs(int64_t num)
 {
 	return num > 0 ? num : -num;
