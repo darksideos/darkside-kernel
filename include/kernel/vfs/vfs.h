07952d13013f376b9c9a61b8a4d75b86c6749c93
George Klees <george.t.klees@gmail.com>
2013-08-01 16:45:52 -0400

    VFS filesystem functions now take partition as an argument

--- include/kernel/vfs/vfs.h
+++ include/kernel/vfs/vfs.h
@@ -19,31 +19,28 @@ typedef struct filesystem
 	void *data;
 
 	/* Read a specified amount of data at the given offset from a file into a buffer */
-	uint64_t (*read)(struct filesystem *fs, struct inode *node, uint8_t *buffer, uint64_t offset, uint64_t length);
+	uint64_t (*read)(struct filesystem *fs, partition_t *partition, struct inode *node, uint8_t *buffer, uint64_t offset, uint64_t length);
 
 	/* Write a specified amount of data from a buffer into a file at the given offset */
-	uint64_t (*write)(struct filesystem *fs, struct inode *node, uint8_t *buffer, uint64_t offset, uint64_t length);
+	uint64_t (*write)(struct filesystem *fs, partition_t *partition, struct inode *node, uint8_t *buffer, uint64_t offset, uint64_t length);
 
 	/* Return a list of directory entries in a directory */
-	list_t (*readdir)(struct filesystem *fs, struct inode *dir);
-
-	/* Get an inode by name, returning -1 if the directory entry doesn't exist */
-	int (*finddir)(struct filesystem *fs, struct inode *dir, uint8_t *name, struct inode *node);
+	list_t (*readdir)(struct filesystem *fs, partition_t *partition, struct inode *dir);
 
 	/* Create a new directory entry to an inode, returning -1 on failure */
-	int32_t (*link)(struct filesystem *fs, struct inode *node, uint8_t *newpath);
+	int32_t (*link)(struct filesystem *fs, partition_t *partition, struct inode *node, uint8_t *newpath);
 
 	/* Remove a directory entry, returning -1 on failure */
-	int32_t (*unlink)(struct filesystem *fs, uint8_t *path);
+	int32_t (*unlink)(struct filesystem *fs, partition_t *partition, uint8_t *path);
 
 	/* Create a new symbolic link to an inode, returning -1 on failure */
-	int32_t (*symlink)(struct filesystem *fs, struct inode *node, uint8_t *newpath);
+	int32_t (*symlink)(struct filesystem *fs, partition_t *partition, struct inode *node, uint8_t *newpath);
 
 	/* Create a new inode, returning -1 on failure */
-	int32_t (*mknod)(struct filesystem *fs, uint8_t *path, int32_t type, dev_t dev, struct inode *node);
+	int32_t (*mknod)(struct filesystem *fs, partition_t *partition, uint8_t *path, int32_t type, dev_t dev, struct inode *node);
 
 	/* Rename a directory entry, returning -1 on failure */
-	int32_t (*rename)(struct filesystem *fs, uint8_t *oldpath, uint8_t *newpath);
+	int32_t (*rename)(struct filesystem *fs, partition_t *partition, uint8_t *oldpath, uint8_t *newpath);
 
 	/* Issue a device specific request on an inode, returning -1 on failure */
 	int32_t (*ioctl)(struct filesystem *fs, struct inode *node, int32_t request, uint8_t *buffer, uint32_t length);
@@ -70,8 +67,8 @@ typedef struct mountpoint
 /* Inode structure */
 typedef struct inode
 {
-	/* Filesystem the inode resides on */
-	filesystem_t *fs;
+	/* Mountpoint the inode resides on */
+	mountpoint_t *mp;
 
 	/* Inode type */
 	int32_t type;
@@ -128,7 +125,6 @@ inode_t *vfs_finddir(inode_t *dir, uint8_t *name);
 int32_t vfs_link(inode_t *node, uint8_t *newpath);
 int32_t vfs_unlink(uint8_t *path);
 int32_t vfs_symlink(inode_t *node, uint8_t *newpath);
-int32_t vfs_mknod(uint8_t *path, int32_t type, dev_t dev);
 int32_t vfs_rename(uint8_t *oldpath, uint8_t *newpath);
 int32_t vfs_ioctl(inode_t *node, int32_t request, uint8_t *buffer, uint32_t length);
 
