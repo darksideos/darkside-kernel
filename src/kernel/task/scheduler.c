dd49448f5219e7052e0010bb39d12aa96cac8e98
George Klees <george.t.klees@gmail.com>
2013-08-04 19:19:19 -0400

    Moved some thread functions from scheduler.c to thread.c

--- src/kernel/task/scheduler.c
+++ src/kernel/task/scheduler.c
@@ -19,41 +19,6 @@ void scheduler_enqueue(thread_t *thread)
 {
 }
 
-/* Put the current thread to sleep */
-void thread_sleep()
-{
-	/* Set the thread's state to sleeping */
-	current_thread->state = THREAD_SLEEP;
-
-	/* Yield the current thread */
-	thread_yield();
-}
-
-/* Wake up a thread */
-void thread_wake(thread_t *thread)
-{
-	/* Set the thread's state to ready */
-	thread->state = THREAD_READY;
-
-	/* Enqueue the thread in the scheduler */
-	scheduler_enqueue(thread);
-}
-
-/* Yield the current thread */
-void thread_yield()
-{
-	/* Save the thread's register context */
-
-	/* If the sleep wasn't put to sleep or killed, enqueue it */
-	if (current_thread->state == THREAD_RUN)
-	{
-		scheduler_enqueue(current_thread);
-	}
-
-	/* Run the scheduler */
-	scheduler_run(current_thread->context);
-}
-
 /* Get the current process */
 process_t *process_current()
 {
@@ -64,4 +29,9 @@ process_t *process_current()
 thread_t *thread_current()
 {
 	return current_thread;
+}
+
+/* Initialize the scheduler */
+void init_scheduler()
+{
 }
