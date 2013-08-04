19e5a871c62c5a1190d658725f64d95dd2cc0826
George Klees <george.t.klees@gmail.com>
2013-08-04 12:30:53 -0400

    Finished process and thread structures (except for stacks)

---
+++ include/kernel/task/scheduler.h
@@ -0,0 +1,14 @@
+#ifndef __SCHEDULER_H
+#define __SCHEDULER_H
+
+/* Thread priorities */
+#define THREAD_MIN_PRIORITY		0
+#define THREAD_MAX_PRIORITY		31
+
+/* Thread states */
+#define THREAD_READY			0
+#define THREAD_RUN				1
+#define THREAD_SLEEP			2
+#define THREAD_DEAD				3
+
+#endif
