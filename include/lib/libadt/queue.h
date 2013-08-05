eae104219a69a9e053744d367ec95cbca4b6fc8e
Noah Singer <singerng@gmail.com>
2013-08-05 17:05:15 -0400

    Wrote queue header file

---
+++ include/lib/libadt/queue.h
@@ -0,0 +1,15 @@
+#ifndef __QUEUE_H
+#define __QUEUE_H
+
+#include <lib/libadt/linkedlist.h>
+#include <lib/libc/stdint.h>
+
+typedef linkedlist_t queue_t;
+
+queue_t queue_create();
+void queue_enqueue(queue_t queue, void *value);
+void *queue_dequeue(queue_t queue);
+void *queue_peek(queue_t queue);
+uint32_t queue_length(queue_t queue);
+
+#endif
