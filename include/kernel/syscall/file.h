0d26d4654ec6eee29968890f72cdf89b45e74d4b
George Klees <gksharkboy@gmail.com>
2013-08-05 09:50:31 -0400

    Made syscalls prefixed with sys_

--- include/kernel/syscall/file.h
+++ include/kernel/syscall/file.h
@@ -2,24 +2,16 @@
 #define __FILE_H
 
 #include <lib/libc/types.h>
-#include <kernel/vfs/old/vfs.h>
 
 /* Kernel mode file syscall implementations */
-int32_t create(const int8_t *name, int32_t mode);
-int32_t open(const int8_t *name, int32_t flags, int32_t mode);
-int32_t close(int32_t file);
-int32_t read(int32_t file, int8_t *ptr, int32_t len);
-int32_t write(int32_t file, int8_t *ptr, int32_t len);
-int32_t lseek(int32_t file, int32_t ptr, int32_t dir);
-int32_t symlink(int8_t *old, int8_t *new_node);
-int32_t hardlink(int8_t *old, int8_t *new_node);
-int32_t unlink(int8_t *name);
-int32_t rm(int8_t *name);
-int32_t rmdir(int8_t *name);
-int32_t rfrm(int8_t *name);
-int32_t chown(int8_t *name, int32_t owner, int32_t group);
-int32_t fstat(int32_t file, struct stat *st);
-int32_t stat(int8_t *name, struct stat *st);
-int32_t isatty(int32_t file);
+int32_t sys_create(uint8_t *path, int32_t mode);
+int32_t sys_open(uint8_t *path, uint32_t flags, int32_t mode);
+int32_t sys_close(int32_t file);
+uint64_t sys_read(int32_t file, uint8_t *buffer, uint64_t length);
+uint64_t sys_write(int32_t file, uint8_t *buffer, uint64_t length);
+int64_t sys_seek(int32_t file, int64_t offset, int32_t origin);
+int32_t sys_link(uint8_t *oldpath, uint8_t *newpath);
+int32_t sys_unlink(uint8_t *path);
+int32_t sys_symlink(uint8_t *oldpath, uint8_t *newpath);
 
 #endif
--- include/kernel/vfs/file.h
+++ include/kernel/vfs/file.h
@@ -3,7 +3,6 @@
 
 #include <lib/libc/types.h>
 #include <kernel/vfs/vfs.h>
-#include <kernel/vfs/stat.h>
 
 /* File flags */
 #define FILE_READ	0x01
@@ -22,7 +21,7 @@ typedef struct file
 
 /* Create, open, close, read from, write to, and seek a file */
 file_t *file_create();
-int32_t file_open(file_t *file, uint8_t *path, int32_t flags, mode_t mode);
+int32_t file_open(file_t *file, uint8_t *path, uint32_t flags, int32_t mode);
 int32_t file_close(file_t *file);
 uint64_t file_read(file_t *file, uint8_t *buffer, uint64_t length);
 uint64_t file_write(file_t *file, uint8_t *buffer, uint64_t length);
