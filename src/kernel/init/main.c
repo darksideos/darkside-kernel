0edd71fce764a10468044de0c8d20da4e70aa43c
George Klees <george.t.klees@gmail.com>
2013-08-09 16:15:55 -0400

    Added scheduler test code to main.c

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -1,27 +1,44 @@
 #include <kernel/init/hal.h>
 #include <kernel/init/os_info.h>
 #include <kernel/mm/heap.h>
+#include <kernel/task/scheduler.h>
 #include <drivers/graphics/vga.h>
-#include <lib/libadt/list.h>
 
 #include <kernel/console/kprintf.h>
 
-#include <kernel/task/scheduler.h>
+#include <kernel/task/process.h>
+#include <kernel/task/thread.h>
 
 void kernel_main(os_info_t *os_info)
 {
 	/* Start the VGA text mode driver */
 	init_text_mode(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
 
-	/* Call the HAL main function to initialize the CPU */
+	/* Initialize the BSP */
 	hal_main(os_info);
 	
 	/* Initialize the kernel heap */
 	init_kheap();
 
- 	/* Sched test code */
+	/* Initialize the VFS */
+
+	/* Register the default executable formats */
+
+	/* Initialize the device manager */
+
+	/* Start the rest of the CPUs in the system */
+
+ 	/* Initialize the scheduler */
 	init_scheduler();
- 	scheduler_run(0, 0);
+
+	/* Load init from the root filesystem */
+
+	/* Create and run the init process */
+
+	process_t *process1 = process_create("Process 1", 0, 0);
+	process_t *process2 = process_create("Process 2", 0, 0);
+
+	thread_t *thread3 = thread_create(process1, 0, 0, THREAD_DEF_USTACK_SIZE);
 
 	while(1);
 }
