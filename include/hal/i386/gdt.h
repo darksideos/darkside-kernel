3f91eccd8d227b7d71f77fe2389e498b5d128dec
George Klees <george.t.klees@gmail.com>
2013-08-22 22:01:26 -0400

    Modified threads so context is stored on the kernel stack, but page faults at address 0x8

--- include/hal/i386/gdt.h
+++ include/hal/i386/gdt.h
@@ -1,17 +1,6 @@
 #ifndef __GDT_H
 #define __GDT_H
 
-#include <lib/libc/types.h>
-
-/* Segment registers */
-#define KERNEL_CODE_SEG	0x08
-#define KERNEL_DATA_SEG	0x10
-#define USER_CODE_SEG	0x18
-#define USER_DATA_SEG	0x20
-
-/* Set the kernel stack in the TSS and the kernel stack MSR */
-void set_kernel_stack(uint32_t stack);
-
 /* Install the GDT */
 void gdt_install();
 
--- include/hal/task.h
+++ include/hal/task.h
@@ -3,14 +3,26 @@
 
 #include <lib/libc/stdint.h>
 
-/* Create a CPU register context */
-void *create_cpu_context(void (*fn)(void *arg), bool user);
+/* struct regs */
+#if defined(__i386__)
+#include <hal/x86/i386/regs.h>
+#elif defined(__x86_64__)
+#include <hal/x86/amd64/regs.h>
+#elif defined(__arm__)
+#include <hal/arm/regs.h>
+#elif defined(__ppc__)
+#include <hal/ppc/ppc32/regs.h>
+#elif defined(__ppc64__)
+#include <hal/ppc/ppc64/regs.h>
+#else
+#error "Unsupported architecture"
+#endif
 
-/* Copy a CPU register context */
-void copy_cpu_context(void *dest, void *src);
+/* Initialize a CPU register context */
+void init_cpu_context(struct regs *context, void (*fn)(void *arg), bool user);
 
 /* Switch the current CPU's register context */
-void switch_cpu_context(void *context);
+void switch_cpu_context(struct regs *context);
 
 /* Set the current CPU's kernel stack */
 void set_kernel_stack(uint64_t stack);
