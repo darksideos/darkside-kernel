0195dfe290b9d4d17012e8aac1bdb11db367d117
George Klees <george.t.klees@gmail.com>
2013-08-24 14:46:50 -0400

    Added device.h, driver.h, and module.h

--- include/kernel/device/device.h
+++ include/kernel/device/device.h
@@ -3,6 +3,19 @@
 
 #include <lib/libc/types.h>
 #include <lib/libadt/list.h>
+#include <kernel/device/device.h>
+
+/* Device types */
+#define DEVICE_TYPE_TEXTMODE	0	/* Text mode display device */
+#define DEVICE_TYPE_GRAPHICS	1	/* Graphics display device */
+#define DEVICE_TYPE_STORAGE		2	/* Storage device */
+#define DEVICE_TYPE_NETWORK		3	/* Network device */
+#define DEVICE_TYPE_KEYBOARD	4	/* Keyboard */
+#define DEVICE_TYPE_MOUSE		5	/* Mouse */
+#define DEVICE_TYPE_SOUND		6	/* Sound device */
+#define DEVICE_TYPE_BUS			7	/* Bus */
+#define DEVICE_TYPE_CONTROLLER	8	/* Controller */
+#define DEVICE_TYPE_OTHER		9	/* Other device */
 
 /* Device structure */
 typedef struct device
@@ -14,15 +27,18 @@ typedef struct device
 	struct device *parent;
 	list_t children;
 
-	/* Device and function number */
-	uint16_t slot, function;
+	/* PCI bus, device, and function numbers */
+	uint16_t pci_bus, pci_device, pci_function;
 
-	/* Vendor and device ID */
-	uint32_t vendor_id, device_id;
+	/* PCI vendor and device ID */
+	uint32_t pci_vendor_id, pci_device_id;
 
-	/* Class code and programming interface */
-	uint16_t class_code;
-	uint8_t prog_if;
+	/* PCI class code and programming interface */
+	uint16_t pci_class_code;
+	uint8_t pci_prog_if;
+
+	/* Driver */
+	driver_t *driver;
 
 	/* Port I/O Addresses */
 	list_t pio_addresses;
@@ -32,9 +48,6 @@ typedef struct device
 
 	/* IRQ */
 	int32_t irq;
-
-	/* DMA Address */
-	uint64_t dma_address;
 
 	/* Power state */
 	uint8_t power_state;
---
+++ include/kernel/device/driver.h
@@ -0,0 +1,31 @@
+#ifndef __DRIVER_H
+#define __DRIVER_H
+
+#include <lib/libc/types.h>
+#include <kernel/device/device.h>
+#include <kernel/modules/module.h>
+
+/* Driver structure */
+typedef struct driver
+{
+	/* Base object */
+	module_t base;
+
+	/* Read a specified amount of data at the given offset from a device into a buffer */
+	uint64_t (*read)(device_t *device, uint8_t *buffer, uint64_t offset, uint64_t length);
+
+	/* Write a specified amount of data from a buffer into a device at the given offset */
+	uint64_t (*write)(device_t *device, uint8_t *buffer, uint64_t offset, uint64_t length);
+} driver_t;
+
+/* Load, unload, initialize, and finalize a driver */
+driver_t *driver_load(uint8_t *path);
+void driver_unload(driver_t *driver);
+int32_t driver_init(driver_t *driver);
+int32_t driver_fini(driver_t *driver);
+
+/* Read from, write to, and issue a device specific request to a device */
+uint64_t driver_read(driver_t *driver, device_t *device, uint8_t *buffer, uint64_t offset, uint64_t length);
+uint64_t driver_write(driver_t *driver, device_t *device, uint8_t *buffer, uint64_t offset, uint64_t length);
+
+#endif
