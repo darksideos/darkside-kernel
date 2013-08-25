b0059df7f3ee4d916a908c1a5d0e89eefdbde498
Noah Singer <singerng@gmail.com>
2013-08-25 10:43:05 -0400

    Started module code

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -3,6 +3,9 @@
 #include <kernel/init/hal.h>
 #include <kernel/mm/heap.h>
 #include <kernel/task/scheduler.h>
+#include <kernel/modules/module.h>
+#include <kernel/modules/exec.h>
+#include <kernel/console/kprintf.h>
 
 void kernel_main(os_info_t *os_info)
 {
@@ -15,6 +18,9 @@ void kernel_main(os_info_t *os_info)
 	/* Initialize the kernel heap */
 	init_kheap();
 	
+	/* Create the modules heap */
+	modules_init();
+	
 	/* Initialize the VFS */
 
 	/* Register the default executable formats */
@@ -22,6 +28,9 @@ void kernel_main(os_info_t *os_info)
 	/* Initialize the device manager */
 
 	/* Start the rest of the CPUs in the system */
+	
+	module_t *module = module_create("ELF", "Run ELF executables, load ELF libraries, relocate ELF modules", "Noah Singer", 0, 0, 0, MODULE_TYPE_EXEC, 0, 2);
+	kprintf(LOG_DEBUG, "Module addr: %08X\n", module);
 	
  	/* Initialize the scheduler */
 	init_scheduler();
