2e857be9ba6acdf65d901b6cee0a73a8b8243757
Noah Singer <singerng@gmail.com>
2013-09-01 20:06:55 -0400

    Added stdarg/valist code in module_tree.c

--- include/kernel/modules/module_tree.h
+++ include/kernel/modules/module_tree.h
@@ -6,8 +6,9 @@
 typedef struct module_pair
 {
 	uint32_t index;
+	
 	void *data;
-} module_pair_node_t;
+} module_pair_t;
 
 typedef struct module_tree_node
 {
@@ -16,7 +17,7 @@ typedef struct module_tree_node
 	list_t children;
 } module_tree_node_t;
 
-module_tree_node_t *create_module_tree_node();
-void module_tree_node_insert(module_tree_node_t *root, module_tree_node_t *module, ...);
+module_tree_node_t *module_tree_node_create();
+void module_tree_insert(module_tree_node_t *root, module_tree_node_t *module, uint8_t levels, ...);
 
 #endif
--- src/kernel/modules/module.c
+++ src/kernel/modules/module.c
@@ -1,15 +1,19 @@
 #include <lib/libc/types.h>
 #include <kernel/modules/module.h>
+#include <kernel/modules/module_tree.h>
 #include <kernel/mm/heap.h>
 #include <kernel/mm/addresses.h>
 
-static heap_t *module_heap;
+heap_t *module_heap;
 
-void modules_init()
+void init_modules()
 {
 	module_heap = create_heap(KERNEL_MODULES_START, KERNEL_MODULES_START + KERNEL_MODULES_INITIAL_SIZE,
 		KERNEL_MODULES_START + KERNEL_MODULES_MIN_SIZE, KERNEL_MODULES_START + KERNEL_MODULES_MAX_SIZE,
 		KERNEL_MODULES_INDEX_SIZE, false, true);
+	
+	module_tree_node_t *node = module_tree_node_create();
+	module_tree_insert(node, 0, 7, 0, 1, 2, 3, 4, 5, 6);
 } 
 
 module_t *module_create(uint8_t *name, uint8_t *desc, uint8_t *author, uint8_t major, uint8_t minor, uint8_t patch)
