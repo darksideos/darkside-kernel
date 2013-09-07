655dd51204952f6c2ce02a9d2689ccf33d3476ca
George Klees <george.t.klees@gmail.com>
2013-09-07 12:02:20 -0400

    ATA code not working

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -10,6 +10,12 @@
 #include <kernel/task/thread.h>
 #include <kernel/ipc/event.h>
 
+#include <kernel/modules/module.h>
+#include <kernel/device/classes.h>
+#include <kernel/device/device.h>
+#include <kernel/device/driver.h>
+#include <lib/libadt/list.h>
+
 uint16_t *vidmem = 0xB8000;
 
 void t1()
@@ -37,6 +43,9 @@ void event1()
 {
 	vidmem[3] = 0x7044;
 }
+
+uint64_t ata_driver_read(device_t *device, uint8_t *buffer, uint64_t offset, uint64_t length);
+uint64_t ata_driver_write(device_t *device, uint8_t *buffer, uint64_t offset, uint64_t length);
 
 void kernel_main(os_info_t *os_info)
 {
@@ -79,14 +88,50 @@ void kernel_main(os_info_t *os_info)
 		str = strtok(0, "/", &saveptr);
 	}*/
 
-	thread_t *thread = thread_create(0, &t1, 0, 256);
+	/*thread_t *thread = thread_create(0, &t1, 0, 256);
 	thread_create(0, &t2, 0, 256);
 	thread_create(0, &t3, 0, 256);
 
 	event_t *event = event_create(&event1);
 	event_send(event, thread);
 
-	enable_interrupts();
+	enable_interrupts();*/
+
+	driver_t *driver = (driver_t*) kmalloc(sizeof(driver_t));
+
+	driver->base.name = "Generic ATA driver";
+	driver->base.desc = "Generic ATA driver";
+	driver->base.author = "George Klees, Noah Singer";
+
+	driver->base.major = 0;
+	driver->base.minor = 1;
+	driver->base.patch = 0;
+
+	driver->base.state = MODULE_LOADED;
+
+	driver->read = &ata_driver_read;
+	driver->write = &ata_driver_write;
+
+	device_t *device = (device_t*) kmalloc(sizeof(device_t));
+
+	device->class_code = DEVICE_STORAGE_HDD;
+	
+	device->driver = driver;
+
+	list_append(&device->pio_addresses, 0x1F0);
+	list_append(&device->pio_addresses, 0x3F6);
+	list_append(&device->pio_addresses, 0x170);
+	list_append(&device->pio_addresses, 0x376);
+	
+	uint8_t *buffer = kmalloc(512);
+	device_read(device, buffer, 0, 512);
+
+	uint32_t i = 0;
+	while (i < 512)
+	{
+		putch(buffer[i]);
+		i++;
+	}
 
 	while(1);
 }
