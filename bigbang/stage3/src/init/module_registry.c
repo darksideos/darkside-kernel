9eefc316e5eaf212b2ab4b2df30bc6c974561077
Noah Singer <singerng@gmail.com>
2013-09-07 21:21:29 -0400

    Fixed some bugs in ext2 code

--- bigbang/stage3/src/init/module_registry.c
+++ bigbang/stage3/src/init/module_registry.c
@@ -1,7 +1,32 @@
 #include <fs/ext2.h>
 #include <lib/libc/string.h>
 #include <init/os_info.h>
+#include <init/kprintf.h>
+#include <vga/vga.h>
+#include <lib/libc/string.h>
+
+extern partition_t *part;
+extern superblock_t *superblock;
+extern inode_t *root_inode;
+extern inode_t *boot_inode;
 
 void parse_registry(os_info_t *os_info)
 {
+	unsigned int modules = ext2_finddir(part, superblock, boot_inode, "modules");
+	inode_t *modules_inode = read_inode(part, superblock, modules);
+
+	unsigned int registry = ext2_finddir(part, superblock, modules_inode, "registry");
+	inode_t *registry_inode = read_inode(part, superblock, registry);
+	
+	unsigned char *registry_data = kmalloc(registry_inode->low_size + 1);
+	memset(registry_data, 0, registry_inode->low_size + 1);
+	ext2_read(part, superblock, registry_inode, registry_data, registry_inode->low_size);
+	
+	unsigned char *saveptr = 0;
+	unsigned char *line = strtok(registry_data, "\n", &saveptr);
+	while(line != 0)
+	{
+		kprintf("%s\n", line);
+		line = strtok(0, "\n", &saveptr);
+	}
 }
