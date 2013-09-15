b943d63b3390c8175f1912a85764883965740e8c
Noah Singer <singerng@gmail.com>
2013-09-15 16:00:04 -0400

    Many changes, foremost module registry code for bootloader *should* work, added log levels to bigbang kprintf(), bigbang disassembly, others

--- bigbang/stage3/include/init/kprintf.h
+++ bigbang/stage3/include/init/kprintf.h
@@ -4,7 +4,15 @@
 #include <lib/libc/stdint.h>
 #include <lib/libc/stdarg.h>
 
+/* Log levels */
+#define LOG_DEBUG	0x05
+#define LOG_INFO	0x04
+#define LOG_WARNING	0x03
+#define LOG_ERROR	0x02
+#define LOG_ALERT	0x01
+#define LOG_PANIC	0x00
+
 /* Print formatted data to the console */
-void kprintf(const char *fmt, ...);
+void kprintf(unsigned char loglevel, const char *fmt, ...);
 
 #endif
