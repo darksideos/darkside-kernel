9f5d03114b8e143c93cd5965df15d2c9c7d06248
George Klees <gksharkboy@gmail.com>
2013-09-20 20:49:11 -0400

    Fixed libadt building

--- include/lib/libadt/index_tree.h
+++ include/lib/libadt/index_tree.h
@@ -1,12 +1,12 @@
 #ifndef __INDEX_TREE_H
 #define __INDEX_TREE_H
 
-typedef struct index_tree_node index_tree_node_t;
+struct index_tree_node;
 
 /* Index tree structure */
 typedef struct index_tree
 {
-	index_tree_node_t *root;
+	struct index_tree_node *root;
 } index_tree_t;
 
 /* Creating trees */
@@ -17,9 +17,9 @@ void index_tree_insert(index_tree_t *tree, void *data, uint32_t levels, ...);
 void *index_tree_lookup(index_tree_t *tree, uint32_t levels, ...);
 
 /* Node info */
-index_tree_node_t *index_tree_node_parent(index_tree_node_t *child);
+struct index_tree_node *index_tree_node_parent(struct index_tree_node *child);
 
 /* Manipulating nodes */
-void index_tree_node_insert(index_tree_node_t *parent, void *data, uint32_t tree_index);
+void index_tree_node_insert(struct index_tree_node *parent, void *data, uint32_t tree_index);
 
 #endif
