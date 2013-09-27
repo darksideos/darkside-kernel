5feea44dd869e5d1709ebfa7aa3b620734f550b2
George Klees <george.t.klees@gmail.com>
2013-09-27 17:16:20 -0400

    Fixed module registry test code

--- src/kernel/init/main.c
+++ src/kernel/init/main.c
@@ -94,13 +94,13 @@ void kernel_main(os_info_t *os_info)
 	module_t *module;
 	
 	module = (module_t*) index_tree_lookup(&os_info->module_registry, 3, hash("DEVICE DRIVERS"), hash("BUSES"), hash("0A00"));
-	kprintf(LOG_DEBUG, "Author: %s\n", module->name);
+	kprintf(LOG_DEBUG, "Name: %s\n", module->name);
 
-	module = (module_t*) index_tree_lookup(&os_info->module_registry, 2, hash("DEVICE DRIVERS"), hash("GRAPHICS DISPLAY DEVICES"), hash("pci"), hash("80860116"));
-	kprintf(LOG_DEBUG, "Author: %s\n", module->author);
+	module = (module_t*) index_tree_lookup(&os_info->module_registry, 4, hash("DEVICE DRIVERS"), hash("GRAPHICS DISPLAY DEVICES"), hash("pci"), hash("80860116"));
+	kprintf(LOG_DEBUG, "Name: %s\n", module->name);
 
 	module = (module_t*) index_tree_lookup(&os_info->module_registry, 2, hash("FILESYSTEMS"), hash("ext2"));
-	kprintf(LOG_DEBUG, "Author: %s\n", module->name);
+	kprintf(LOG_DEBUG, "Name: %s\n", module->name);
 
 	/*thread_t *thread = thread_create(0, &t1, 0, 256);
 	thread_create(0, &t2, 0, 256);
