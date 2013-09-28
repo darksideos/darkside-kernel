29899a3d48c365fd268939ba899d90e555983945
George Klees <george.t.klees@gmail.com>
2013-09-28 11:21:13 -0400

    Boot PMM finished but not tested

--- bigbang/stage3/src/mm/pmm.c
+++ bigbang/stage3/src/mm/pmm.c
@@ -4,7 +4,7 @@
 #include <mm/pmm.h>
 
 /* Physical memory map */
-mem_map_entry_t *phys_mem_map;
+e820_linked_entry_t *phys_mem_map;
 unsigned int phys_mem_map_num_entries;
 
 /* Page align an address */
@@ -23,26 +23,44 @@ unsigned int page_align(unsigned int address)
 /* Allocate a physical page from the boot PMM */
 unsigned int pmm_alloc_page()
 {
+	e820_linked_entry_t *linked = phys_mem_map;
+
 	unsigned int i;
 	for (i = 0; i < phys_mem_map_num_entries; i++)
 	{
-		if (!(mem_map[i].flags & MEM_MAP_FLAG_FREE) && (mem_map[i].flags & MEM_MAP_FLAG_VALID)
-			&& (mem_map[i].flags & MEM_MAP_FLAG_EXISTS) &&
-			!(mem_map[i].flags & MEM_MAP_FLAG_ACPI) && !(mem_map[i].flags & MEM_MAP_FLAG_ACPI_VOLATILE))
+		if (linked->type == E820_FREE)
 		{
 			/* Do some calculations */
-			unsigned int start = (unsigned int) mem_map[i].base;
-			unsigned int end = (unsigned int) mem_map[i].base + mem_map[i].length;
+			unsigned int start = (unsigned int) linked->base;
+			unsigned int end = (unsigned int) linked->base + linked->length;
 
 			/* Can we allocate here without going over the limit */
 			if ((page_align(start) <= end) && ((end - page_align(start)) >= 0x1000))
 			{
-				/* Split the memory map entry */
-				mem_map[i].length = 0x1000;
-				mem_map[i].flags &= ~MEM_MAP_FLAG_FREE;
-			}
+				/* Mark this memory as used */
+				linked->type = E820_RESERVED;
 
-			continue;
+				/* Can we reclaim a page or more afterwards? */
+				if ((end - page_align(start)) >= 0x1000)
+				{
+					/* Add a new entry after it */
+					e820_linked_entry_t *after = (e820_linked_entry_t*) kmalloc(sizeof(e820_linked_entry_t));
+
+					after->next = linked->next;
+					after->prev = linked;
+					after->base = page_align(start) + 0x1000;
+					after->length = end - page_align(start);
+					after->type = E820_FREE;
+					after->attrib = 0;
+					after->spec_flags = 0;
+
+					/* Modify the E820 entry */
+					linked->next = after;
+					linked->length = (page_align(start) + 0x1000) - start;
+				}
+
+				return page_align(start);
+			}
 		}
 	}
 
@@ -51,8 +69,8 @@ unsigned int pmm_alloc_page()
 }
 
 /* Initialize the boot PMM */
-void init_pmm(mem_map_entry_t *mem_map, unsigned int mem_map_num_entries)
+void init_pmm(e820_linked_entry_t *mem_map_linked, unsigned int mem_map_num_entries)
 {
-	phys_mem_map = mem_map;
+	phys_mem_map = mem_map_linked;
 	phys_mem_map_num_entries = mem_map_num_entries;
 }
