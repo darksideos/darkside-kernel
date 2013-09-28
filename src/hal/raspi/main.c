b63cf7c42f9234b2697c74960e4eedf641860ec0
Theo Tosini <theo.tosini@gmail.com>
2013-09-28 13:25:59 -0400

    Add GPIO write and structs, doesn't work

--- src/hal/raspi/main.c
+++ src/hal/raspi/main.c
@@ -2,6 +2,10 @@
 /* Raspi Initilization Function */
 void raspi_main()
 {
+
+	/* Initilize GPIO 16 (internal LED) */
+	gpio_setmode(4, OUTPUT);
+	gpio_write(4, 1);
 	/* Wait Forever */	
 	while(1);
 }
