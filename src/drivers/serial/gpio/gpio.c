d6467c849222f81253b073c97fe4ee16b639d97a
Artemis Tosini <me@artem.ist>
2013-09-28 13:25:59 -0400

    Add GPIO write and structs, doesn't work

--- src/drivers/serial/gpio/gpio.c
+++ src/drivers/serial/gpio/gpio.c
@@ -1,19 +1,45 @@
 #include <drivers/serial/gpio/gpio.h>
-#include <hal/raspi/registers.h>
+#include <lib/libc/stdbool.h>
+
+/* Create a GPIO Object */
+gpio_t* gpio = (gpio_t*)0x20200000;
 
 /* Set the mode of a pin to a function from enum gpio_function */
-int gpio_setmode(int pin, gpio_function func)
+bool gpio_setmode(int pin, gpio_function func)
 {
-	/* Return -1 for invalid pins */
+	/* Return false for invalid pins */
 	if(pin < 0 || pin > 53)
-		return -1;
-	/* 10 pins are in each register, with 3 bits for each */	
-	unsigned int regnum = pin / 10;
-	unsigned int regindex = pin % 10;
+		return false;
+	/* 10 pins are in each register, with 3 bits for each */
+	unsigned int regindex = pin % 10;	
+	unsigned int regnum = (pin - regindex) / 10;
 	unsigned int mask = ~(0b111 << (regindex * 3));
 
 	/* Set the GPIO Function Select Registers */
 	gpio->gpfsel[regnum] &= mask;
 	gpio->gpfsel[regnum] |= (func << (regindex * 3));
-	return 0;
+	return true;
+}
+
+/* Write to a pin */
+bool gpio_write(int pin, bool value)
+{
+	/* Return false for invalid pins */
+	if(pin < 0 || pin > 53)
+		return false;
+	/* Pins 0-32 are in gpset/clr0, and the rest are in gpset/clr1 */
+	unsigned int regnum = ((pin > 31));
+	unsigned int regindex = pin % 32;
+	unsigned int mask = ~(1 << regindex);
+	
+	/* Set the GPIO Set / Clr registers */
+	if(value)
+	{
+		gpio->gpset[regnum] = mask;
+	}
+	else
+	{
+		gpio->gpclr[regnum] = mask;
+	}
+	return true;
 }
