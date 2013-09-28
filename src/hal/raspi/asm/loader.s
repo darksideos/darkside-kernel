05f1960d431811befda0310f8706185714856b54
George Klees <george.t.klees@gmail.com>
2013-09-28 10:25:47 -0400

    Fixed loader.s for Raspi

--- src/hal/raspi/asm/loader.s
+++ src/hal/raspi/asm/loader.s
@@ -1,7 +1,14 @@
-/* This function is run on startup */
+; Here is the code
+.section .text
+
+; This is the entry point of our kernel, it is referenced in the linker script
 .global _start
 _start:
-	/* 0x8000 is the first part of memory that is not read-only */
-	mov sp, #0x8000
-	/* Jump to the raspi initilization function */
-	bl raspi_main
+	mov sp, stack		; Set up a new stack
+	add sp, sp, #65536
+
+	push r0				; Push the OS info pointer
+	bl hal_init_bsp		; Call our kernel main function
+	
+.section .bss
+	.lcomm stack, 65536
