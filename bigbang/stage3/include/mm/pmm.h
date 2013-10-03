c69e561b36d4a64b632448a5a6d0f22d18636f46
Noah Singer <singerng@gmail.com>
2013-10-03 18:32:41 -0400

    Corrected a couple PMM issues

--- bigbang/stage3/include/mm/pmm.h
+++ bigbang/stage3/include/mm/pmm.h
@@ -2,6 +2,6 @@
 #define __PMM_H
 
 unsigned int pmm_alloc_page();
-void init_pmm(e820_linked_entry_t *mem_map_linked, unsigned int mem_map_num_entries);
+void init_pmm(e820_linked_entry_t *mem_map_linked, unsigned int *mem_map_num_entries);
 
 #endif
