8ac9310c1a84230c1289ab56f1204e3f4cdd266f
Marionumber1 <gksharkboy@gmail.com>
2013-10-17 20:23:53 -0400

    Kernel finally actually boots

--- src/kernel/init/report.c
+++ src/kernel/init/report.c
@@ -15,7 +15,13 @@ void report(os_info_t *os_info)
 	range_t *entry = os_info->ranges;
 	for(uint32_t index = 0; index < os_info->num_ranges; index++)
 	{
-		kprintf(LOG_NONE, "\tBase: %08X\tLength: %08X\tFlags: %08X\n", (uint32_t) entry->base, (uint32_t) entry->length, entry->flags);
+		kprintf(LOG_NONE, "\tBase: %08X\tLength: %08X", (uint32_t) entry->base, (uint32_t) entry->length);
+
+		if (entry->flags & 1)
+			kprintf(LOG_NONE, "\tFree\n");
+		else
+			kprintf(LOG_NONE, "\tUsed\n");
+
 		entry++;
 	}
 	
