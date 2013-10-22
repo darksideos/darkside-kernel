6909e53d69c612d91cf0f3f05702a5f335302316
George Klees <george.t.klees@gmail.com>
2013-10-22 17:31:25 -0400

    Added more .h files to device

---
+++ include/kernel/device/bus.h
@@ -0,0 +1,10 @@
+#ifndef __BUS_H
+#define __BUS_H
+
+/* Bus structure */
+typedef struct bus
+{
+	device_t device;
+}
+
+#endif
--- include/kernel/device/driver.h
+++ include/kernel/device/driver.h
@@ -2,26 +2,12 @@
 #define __DRIVER_H
 
 #include <lib/libc/types.h>
-#include <kernel/device/device.h>
 #include <kernel/modules/module.h>
-
-struct device;
 
 /* Driver structure */
 typedef struct driver
 {
-	/* Base object */
 	loaded_module_t base;
-
-	/* Read a specified amount of data at the given offset from a device into a buffer */
-	uint64_t (*read)(struct device *device, uint8_t *buffer, uint64_t offset, uint64_t length);
-
-	/* Write a specified amount of data from a buffer into a device at the given offset */
-	uint64_t (*write)(struct device *device, uint8_t *buffer, uint64_t offset, uint64_t length);
 } driver_t;
-
-/* Read from, write to, and issue a device specific request to a device */
-uint64_t driver_read(driver_t *driver, struct device *device, uint8_t *buffer, uint64_t offset, uint64_t length);
-uint64_t driver_write(driver_t *driver, struct device *device, uint8_t *buffer, uint64_t offset, uint64_t length);
 
 #endif
