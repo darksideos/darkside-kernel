33ae269ac93a34dbe9c10e075c9761cf0ec2c6a5
Marionumber1 <gksharkboy@gmail.com>
2013-11-28 10:00:37 -0500

    Finished fs code, results in error

--- bigbang/stage3/src/fs/fs.c
+++ bigbang/stage3/src/fs/fs.c
@@ -1,3 +1,4 @@
+#include <lib/libc/string.h>
 #include <fs/fs.h>
 #include <fs/ext2.h>
 
@@ -7,10 +8,34 @@ fs_context_t *fs_context_init(partition_t *partition)
 	return ext2_fs_context_init(partition);
 }
 
-/* Reads from a given FS context */
-int fs_read(fs_context_t *context, unsigned char *buffer, unsigned int length)
+/* Open an inode */
+void *fs_open(fs_context_t *context, unsigned char *path)
+{
+	ext2_fs_context_t *ext2_context = (ext2_fs_context_t*) context;
+
+	inode_t *current_inode = read_inode(ext2_context->context.partition, ext2_context->superblock, 2);
+
+	unsigned char *saveptr;
+	unsigned char *name = strtok(path, "/", &saveptr);
+	while (name)
+	{
+		if (*name)
+		{
+			unsigned int inode = ext2_finddir(ext2_context->context.partition, ext2_context->superblock, current_inode, name);
+			current_inode = read_inode(ext2_context->context.partition, ext2_context->superblock, inode);
+		}
+
+		name = strtok(0, "/", &saveptr);
+	}
+
+	return current_inode;
+}
+
+
+/* Read data from an inode */
+int fs_read(fs_context_t *context, void *inode, unsigned char *buffer, unsigned int length)
 {
 	ext2_fs_context_t *ext2_context = (ext2_fs_context_t*) context;
 	
-	//return ext2_read(ext2c->context.partition, ext2c->superblock, 
+	return ext2_read(ext2_context->context.partition, ext2_context->superblock, inode, buffer, length);
 }
