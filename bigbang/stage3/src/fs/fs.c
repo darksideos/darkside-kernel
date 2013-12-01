58a22fc240af19a7dcdbf2b3ea82aeed6ee38875
Noah Singer <singerng@gmail.com>
2013-12-01 18:01:27 -0500

    Got rid of fs_length()

--- bigbang/stage3/src/fs/fs.c
+++ bigbang/stage3/src/fs/fs.c
@@ -10,29 +10,6 @@ fs_context_t *fs_context_init(partition_t *partition)
 	return ext2_fs_context_init(partition);
 }
 
-/* Gets the length of a given FS context */
-int fs_length(fs_context_t *context, unsigned char *fname)
-{
-	ext2_fs_context_t *ext2c = (ext2_fs_context_t*) context;
-	
-	unsigned char *saveptr;
-	unsigned char *lname;
-	
-	strtok(fname, "/", &saveptr);
-	
-	unsigned int inode = 2;
-	inode_t *inode_data = read_inode(ext2c->context.partition, ext2c->superblock, 2);
-	
-	while (lname = strtok(0, "/", &saveptr))
-	{
-		/* Read the kernel */
-		inode = ext2_finddir(ext2c->context.partition, ext2c->superblock, inode_data, lname);
-		inode_data = read_inode(ext2c->context.partition, ext2c->superblock, inode);
-	}
-	
-	return inode_data->low_size;
-}
-
 /* Open an inode */
 void *fs_open(fs_context_t *context, unsigned char *path)
 {
