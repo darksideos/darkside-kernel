4f2a329ad9633770ab819a900a64bef8b0813dda
Marionumber1 <gksharkboy@gmail.com>
2013-12-14 11:00:32 -0500

    Added error messages and boot signature to stage1

--- bigbang/x86-pc/stage1/src/stage1.s
+++ bigbang/x86-pc/stage1/src/stage1.s
@@ -37,6 +37,12 @@ bootstrap_start:
 	mov [DAP(lba_start_l)], dword 0x0
 	mov [DAP(lba_start_h)], dword 0x0
 	
+; Initialize the video mode
+init_video:
+	mov al, 0x03
+	mov ah, 0x00
+	int 0x10
+	
 ; Find the active MBR partition, placing it in the local data structure
 find_active_part:
 	mov ax, 0
@@ -52,7 +58,8 @@ find_active_part:
 	inc ax
 	jmp .loop
 .fail:
-	jmp $
+	mov ax, error_mbr
+	jmp error
 .success:
 	mov [DATA(partition)], ax
 	
@@ -74,11 +81,35 @@ load_stage2:
 	int 0x13
 	
 	; Hang if the disk read failed
-	jc $
+	mov ax, error_stage2
+	jc error
 	
 	; Jump to stage2
 	jmp 0x0000:STAGE2_LOC
+	
+; Error function
+error:
+	mov bp, ax					; message
+	mov al, 0x01				; write mode
+	mov ah, 0x13				; interrupt #
+	mov bh, 0x00				; page #
+	mov bl, 0x04				; color (red)
+	mov cx, 45					; string length
+	mov dh, 0x00				; row
+	mov dl, 0x00				; column
+	int 0x10
+	jmp $
+
+error_mbr db "No active partitions in MBR. Aborting..."
+error_stage2 db "Unable to load stage2 from disk. Aborting..."
 
 ; End of the bootstrap code
 bootstrap_end:
+
+; Fill the remaining 446 bytes with zeroes
+times 446 - ($ - $$) db 0
+
+; Boot signature
+db 0x55
+db 0xAA
 	
