bfe250c78f18ad21622fcda1e03016c85d7a170c
Noah Singer <singerng@gmail.com>
2013-12-23 09:28:04 -0500

    Fixed the Makefiles

--- img/utils/updateimage.sh
+++ img/utils/updateimage.sh
@@ -2,10 +2,10 @@
 
 if [ "$(uname -s)" == "Darwin" ]
 	then
-		sudo fuse-ext2 -o rw,force ../ext2.img /mnt/hdd
+		sudo fuse-ext2 -o rw,force ../images/ext2.img /mnt/hdd
 		read -p "Waiting... "
-		sudo cp ../../build-i386/kernel-i386.elf /mnt/hdd/boot
-		sudo cp ../../build-i386/symtab /mnt/hdd/boot
+#		sudo cp ../../build-i386/kernel-i386.elf /mnt/hdd/boot
+#		sudo cp ../../build-i386/symtab /mnt/hdd/boot
 		read -p "Waiting... "
 		sudo hdiutil eject /mnt/hdd
 fi
@@ -25,7 +25,7 @@ if [[ "$(uname -s)" == CYGWIN* ]]
 		read -p "Waiting for kernel-i386.elf to be copied into ext2.img [Press Enter to continue]..."
 fi
 
-dd if=../ext2.img of=../os.img bs=512 seek=2048 conv=notrunc
+dd if=../images/ext2.img of=../images/os.img bs=512 seek=2048 conv=notrunc
 
 if [[ "$(uname -s)" == CYGWIN* ]]
 	then
