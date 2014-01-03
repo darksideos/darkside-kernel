b3cda62e22c904f21b2f242de88458bf1317f69d
George Klees <george.t.klees@gmail.com>
2014-01-03 16:08:54 -0500

    Rewritten MBR, should work, but infinitely loops at 0x7dd4

--- bigbang/x86-pc/stage1/src/stage1.s
+++ bigbang/x86-pc/stage1/src/stage1.s
@@ -12,6 +12,9 @@ start:
 	mov ds, ax
 	mov es, ax
 	mov ss, ax
+	
+	; Set up a stack
+	mov esp, ORG_LOC
 .setup_data:
 	mov [DATA(drive)], dl
 .setup_dap:
@@ -28,76 +31,23 @@ init_video:
 	mov ah, 0x00
 	int 0x10
 	
-;jmp find_active_part
-	
-; Get the BIOS memory map
-do_e820:
-	xor bp, bp						; Keep the number of entries in BP
-	
-	mov di, E820_LOC				; Address of E820 map
-	mov [DATA(e820_map)], edi		; goes into the data structure
-	
-	xor ebx, ebx					; Clear EBX
-	mov edx, 0x534D4150				; Move "SMAP" into EDX
-	mov eax, 0xE820
-	mov [es:di + 20], dword 1		; Force a valid ACPI 3.x entry
-	mov ecx, 24						; 24 byte entry
-	int 0x15						; Get the memory map
-	
-	jc short .failed				; Error message on failure
-	mov edx, 0x534D4150				; Some BIOSes trash EDX
-	cmp eax, edx					; On success, EAX == "SMAP"
-	jne short .failed				; If not, error message
-	test ebx, ebx					; EBX = 0 means 1 entry
-	je short .failed				; If so, error message
-	jmp short .jmpin				; Continue
-.e820lp:
-	mov eax, 0xe820					; EAX gets trashed on every int 0x15 call
-	mov [es:di + 20], dword 1		; force a valid ACPI 3.X entry
-	mov ecx, 24						; ECX also trashed
-	int 0x15
-	jc short .e820f					; Carry means "end of list already reached"
-	mov edx, 0x534D4150				; repair potentially trashed register
-.jmpin:
-	jcxz .skipent					; Skip any 0 length entries
-	cmp cl, 20						; Got a 24 byte ACPI 3.X response?
-	jbe short .notext
-	test byte [es:di + 20], 1		; If so, is the "ignore this data" bit clear?
-	je short .skipent
-.notext:
-	mov ecx, [es:di + 8]			; get lower dword of memory region length
-	or ecx, [es:di + 12]			; "or" it with upper dword to test for zero
-	jz .skipent						; if length qword is 0, skip entry
-	inc bp							; got a good entry: ++count, move to next storage spot
-	add di, 24
-.skipent:
-	test ebx, ebx					; if ebx resets to 0, list is complete
-	jne short .e820lp
-.e820f:
-	mov [DATA(e820_entries)], bp	; Store the entry count in the OS info structure
-	clc								; There is a carry flag at this point, so it must be cleared
-	jmp find_active_part			; Leave the function
-.failed:
-	mov ax, error_mem_map
-	jmp error
-	
 ; Find the active MBR partition, placing it in the local data structure
 find_active_part:
 	xor ax, ax
-	xor edx, edx
+	xor ecx, ecx
 .loop:
 	; Make sure we only read the first 4 entries
 	cmp ax, 4
 	jge .fail
 	
 	; Find out if the partition is active
-	mov bx, [MBR(edx, bootable)]
+	mov bx, [MBR(ecx, bootable)]
 	bt bx, 7
 	jc .success
 	
 	; If not, loop
 	inc ax
-	add dx, 0x10
+	add cx, 0x10
 	jmp .loop
 .fail:
 	mov ax, error_mbr
@@ -117,6 +67,11 @@ mbr_relocate:
 
 ; Load stage2 from the partition
 load_stage2:
+	; Set up ES:SI for the VBR handoff and push on the stack
+	mov esi, (RELOC_LOC + MBR_LOC - ORG_LOC)
+	add esi, edx
+	push esi
+	
 	; Get the start of the partition
 	mov eax, [MBR(edx, lba_start)]
 	
@@ -137,6 +92,9 @@ load_stage2:
 	jc near (RELOC_LOC + error - ORG_LOC)
 	
 	; Jump to stage2
+	mov dl, [DATA(drive)]
+	pop esi
+	
 	jmp 0x0000:STAGE2_LOC
 	
 ; Error function
@@ -153,7 +111,6 @@ error:
 	
 	jmp $						; Hang forever
 
-error_mem_map db "Unable to get E820 map........"
 error_mbr	  db "No active partitions in MBR..."
 error_stage2  db "Unable to load stage2........."
 
