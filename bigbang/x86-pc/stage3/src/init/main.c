7888c8eacc3a77205695fc831b756bf004365cc4
George Klees <george.t.klees@gmail.com>
2014-01-22 17:52:39 -0500

    printf isn't successfully printing with format strings

--- bigbang/x86-pc/stage3/src/init/main.c
+++ bigbang/x86-pc/stage3/src/init/main.c
@@ -1,4 +1,5 @@
 #include <types.h>
+#include <stdio.h>
 #include <graphics/vga.h>
 
 void main()
@@ -6,7 +7,7 @@ void main()
 	/* Initialize the boot video driver */
 	bootvid_init(COLOR_WHITE, COLOR_BLACK);
 
-	bootvid_puts("HELLO WORLD\n");
+	printf("40 + 2 = %d\n", 42);
 
 	while(1);
 }
