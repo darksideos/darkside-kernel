71454783c03527c8c67ef5cbfbf7679142f95a08
George Klees <george.t.klees@gmail.com>
2014-01-25 11:03:28 -0500

    Added non-recursive E820 sorting code, untested

--- bigbang/pc/stage3/src/mm/e820.c
+++ bigbang/pc/stage3/src/mm/e820.c
@@ -1,4 +1,34 @@
 #include <types.h>
+#include <list.h>
 #include <mm/e820.h>
 
 /* Sanitize the E820 memory map */
+list_t e820_map_sanitize(e820_entry_t *e820_entries, uint32_t num_e820_entries)
+{
+	/* Sort the map in address order */
+	bool sorted = true;
+	do
+	{
+		sorted_thistime = true;
+
+		for (size_t i = 0; i < num_e820_entries - 1; i++)
+		{
+			if (map[i].base > map[i + 1].base)
+			{
+				sorted_thistime = false;
+
+				e820_entry_t tmp = map[i];
+				map[i] = map[i + 1];
+				map[i + 1] = tmp;
+
+			}
+
+			if (i == (num_e820_entries - 2) && sorted_thistime)
+			{
+				sorted = true;
+			}
+		}
+	}
+	while (!sorted);
+
+	/* Convert it into a linked list */
