26c8fe369688e1fb0d531fa56714f22ea2c17141
George Klees <george.t.klees@gmail.com>
2014-01-24 21:46:30 -0500

    About to write E820 memory map functions

---
+++ bigbang/pc/stage3/include/mm/pmm.h
@@ -0,0 +1,10 @@
+#ifndef __PMM_H
+#define __PMM_H
+
+/* Allocate a physical page */
+uint32_t pmm_alloc_page();
+
+/* Initialize the physical memory manager */
+void pmm_init(e820_entry_t **e820_entries, uint32_t num_e820_entries);
+
+#endif
