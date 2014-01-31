9c5bf6a8e4148776cb93991139ecbf7247a78929
George Klees <george.t.klees@gmail.com>
2014-01-31 17:35:59 -0500

    pmm_claim_page() works

--- bigbang/pc/stage3/include/mm/pmm.h
+++ bigbang/pc/stage3/include/mm/pmm.h
@@ -22,7 +22,10 @@ typedef struct mem_map_entry
 } mem_map_entry_t;
 
 /* Allocate a physical page */
-uint32_t pmm_alloc_page();
+uint64_t pmm_alloc_page();
+
+/* Claim a physical page */
+void pmm_claim_page(uint64_t address);
 
 /* Initialize the physical memory manager */
 void pmm_init(e820_entry_t *e820_entries, uint32_t num_e820_entries);
