c2cabccee7bb49d36f3d3eb5a2aa0320f185c2af
Marionumber1 <gksharkboy@gmail.com>
2014-02-05 10:10:45 -0500

    Added disk reading code, but still untested

---
+++ bigbang/pc/stage3/include/storage/disk.h
@@ -0,0 +1,10 @@
+#ifndef __DISK_H
+#define __DISK_H
+
+/* Read from the disk */
+void disk_read(void *buffer, uint32_t sector, uint32_t numsectors);
+
+/* Initialize the disk */
+void disk_init(uint32_t drive_number);
+
+#endif
