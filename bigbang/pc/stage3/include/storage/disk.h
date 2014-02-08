431fe351a509d91b4b4ef1d4e6c90e5f94fef877
Marionumber1 <gksharkboy@gmail.com>
2014-02-08 12:42:59 -0500

    Disk and partition code now work

--- bigbang/pc/stage3/include/storage/disk.h
+++ bigbang/pc/stage3/include/storage/disk.h
@@ -4,7 +4,7 @@
 /* Read from the disk */
 void disk_read(void *buffer, uint32_t sector, uint32_t numsectors);
 
-/* Initialize the disk */
+/* Initialize the disk driver */
 void disk_init(uint32_t drive_number);
 
 #endif
