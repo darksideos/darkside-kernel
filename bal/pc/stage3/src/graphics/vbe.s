a66561d5161b6e8eda462e69760d913ea8610083
George Klees <george.t.klees@gmail.com>
2014-03-13 15:17:11 +0000

    Added vbe_set_mode()

--- bal/pc/stage3/src/graphics/vbe.s
+++ bal/pc/stage3/src/graphics/vbe.s
@@ -127,10 +127,90 @@ vbe_get_mode:
 	lidt [idt16]
 	sti
 	
-	; Get the controller info
+	; Get the mode info
 	mov ax, 0x4F01
 	mov cx, [ebp + 8]
 	mov di, MODE_LOC
+	int 0x10
+	
+	; Save return value
+	push eax
+	
+	; Reenable protected mode and paging
+	cli
+	mov eax, cr0
+	or eax, 0x80000001
+	mov cr0, eax
+	
+	; Jump to our 32-bit protected mode entry point
+	jmp 0x08:.pm32_exit
+[BITS 32]
+.pm32_exit:
+	; Reload the segment registers
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	
+	; Restore return value
+	pop eax
+	
+	; Return from the function
+	pop edi
+	pop esi
+	pop ebx
+	pop ebp
+	ret
+	
+; Set a VBE mode
+global vbe_set_mode
+vbe_set_mode:
+	; Set up the stack frame
+	push ebp
+	mov ebp, esp
+	
+	; Save registers
+	push ebx
+	push esi
+	push edi
+	
+	; Jump to our 16-bit protected mode entry
+	jmp 0x18:.pm16_entry
+[BITS 16]
+.pm16_entry:
+	; Reload the segment registers
+	mov ax, 0x20
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	
+	; Disable protected mode and paging
+	mov eax, cr0
+	and eax, 0x7FFFFFFE
+	mov cr0, eax
+	
+	; Jump to our real mode entry
+	jmp 0x0000:.rm_entry
+.rm_entry:
+	; Reload the segment registers
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	
+	; Load the real mode IDT
+	lidt [idt16]
+	sti
+	
+	; Set the mode
+	mov ax, 0x4F02
+	mov bx, [ebp + 8]
 	int 0x10
 	
 	; Save return value
