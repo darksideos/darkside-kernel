d73bb518f4193e3c376ce666fa4c5dc2239971d0
George Klees <george.t.klees@gmail.com>
2014-03-13 17:02:52 -0400

    Cleaned up BIOS code handling with disk and VBE code

--- bal/pc/stage3/src/graphics/vbe.s
+++ bal/pc/stage3/src/graphics/vbe.s
@@ -1,3 +1,4 @@
+%include "src/init/rmode.inc"
 %include "src/graphics/vbe.inc"
 
 [BITS 32]
@@ -6,46 +7,8 @@ section .text
 ; Initialize VBE
 global vbe_init
 vbe_init:
-	; Set up the stack frame
-	push ebp
-	mov ebp, esp
-	
-	; Save registers
-	push ebx
-	push esi
-	push edi
-	
-	; Jump to our 16-bit protected mode entry
-	jmp 0x18:.pm16_entry
-[BITS 16]
-.pm16_entry:
-	; Reload the segment registers
-	mov ax, 0x20
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Disable protected mode and paging
-	mov eax, cr0
-	and eax, 0x7FFFFFFE
-	mov cr0, eax
-	
-	; Jump to our real mode entry
-	jmp 0x0000:.rm_entry
-.rm_entry:
-	; Reload the segment registers
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Load the real mode IDT
-	lidt [idt16]
-	sti
+	; Switch from protected mode to real mode
+	pm2rm
 	
 	; Get the controller info
 	mov ax, 0x4F00
@@ -55,77 +18,14 @@ vbe_init:
 	; Save return value
 	push eax
 	
-	; Reenable protected mode and paging
-	cli
-	mov eax, cr0
-	or eax, 0x80000001
-	mov cr0, eax
-	
-	; Jump to our 32-bit protected mode entry point
-	jmp 0x08:.pm32_exit
-[BITS 32]
-.pm32_exit:
-	; Reload the segment registers
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Restore return value
-	pop eax
-	
-	; Return from the function
-	pop edi
-	pop esi
-	pop ebx
-	pop ebp
-	ret
+	; Switch from real mode to protected mode
+	rm2pm
 	
 ; Get a VBE mode
 global vbe_get_mode
 vbe_get_mode:
-	; Set up the stack frame
-	push ebp
-	mov ebp, esp
-	
-	; Save registers
-	push ebx
-	push esi
-	push edi
-	
-	; Jump to our 16-bit protected mode entry
-	jmp 0x18:.pm16_entry
-[BITS 16]
-.pm16_entry:
-	; Reload the segment registers
-	mov ax, 0x20
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Disable protected mode and paging
-	mov eax, cr0
-	and eax, 0x7FFFFFFE
-	mov cr0, eax
-	
-	; Jump to our real mode entry
-	jmp 0x0000:.rm_entry
-.rm_entry:
-	; Reload the segment registers
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Load the real mode IDT
-	lidt [idt16]
-	sti
+	; Switch from protected mode to real mode
+	pm2rm
 	
 	; Get the mode info
 	mov ax, 0x4F01
@@ -136,77 +36,14 @@ vbe_get_mode:
 	; Save return value
 	push eax
 	
-	; Reenable protected mode and paging
-	cli
-	mov eax, cr0
-	or eax, 0x80000001
-	mov cr0, eax
-	
-	; Jump to our 32-bit protected mode entry point
-	jmp 0x08:.pm32_exit
-[BITS 32]
-.pm32_exit:
-	; Reload the segment registers
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Restore return value
-	pop eax
-	
-	; Return from the function
-	pop edi
-	pop esi
-	pop ebx
-	pop ebp
-	ret
+	; Switch from real mode to protected mode
+	rm2pm
 	
 ; Set a VBE mode
 global vbe_set_mode
 vbe_set_mode:
-	; Set up the stack frame
-	push ebp
-	mov ebp, esp
-	
-	; Save registers
-	push ebx
-	push esi
-	push edi
-	
-	; Jump to our 16-bit protected mode entry
-	jmp 0x18:.pm16_entry
-[BITS 16]
-.pm16_entry:
-	; Reload the segment registers
-	mov ax, 0x20
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Disable protected mode and paging
-	mov eax, cr0
-	and eax, 0x7FFFFFFE
-	mov cr0, eax
-	
-	; Jump to our real mode entry
-	jmp 0x0000:.rm_entry
-.rm_entry:
-	; Reload the segment registers
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Load the real mode IDT
-	lidt [idt16]
-	sti
+	; Switch from protected mode to real mode
+	pm2rm
 	
 	; Set the mode
 	mov ax, 0x4F02
@@ -216,37 +53,10 @@ vbe_set_mode:
 	; Save return value
 	push eax
 	
-	; Reenable protected mode and paging
-	cli
-	mov eax, cr0
-	or eax, 0x80000001
-	mov cr0, eax
-	
-	; Jump to our 32-bit protected mode entry point
-	jmp 0x08:.pm32_exit
-[BITS 32]
-.pm32_exit:
-	; Reload the segment registers
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Restore return value
-	pop eax
-	
-	; Return from the function
-	pop edi
-	pop esi
-	pop ebx
-	pop ebp
-	ret
+	; Switch from real mode to protected mode
+	rm2pm
 	
 section .data
 
 ; Real mode IDT
-idt16:
-	dw 0x3FF
-	dd 0
+idt
--- bal/pc/stage3/src/storage/disk.s
+++ bal/pc/stage3/src/storage/disk.s
@@ -6,46 +6,8 @@ section .text
 ; Read a sector from the disk
 global disk_read
 disk_read:
-	; Set up the stack frame
-	push ebp
-	mov ebp, esp
-	
-	; Save registers
-	push ebx
-	push esi
-	push edi
-	
-	; Jump to our 16-bit protected mode entry
-	jmp 0x18:.pm16_entry
-[BITS 16]
-.pm16_entry:
-	; Reload the segment registers
-	mov ax, 0x20
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Disable protected mode and paging
-	mov eax, cr0
-	and eax, 0x7FFFFFFE
-	mov cr0, eax
-	
-	; Jump to our real mode entry
-	jmp 0x0000:.rm_entry
-.rm_entry:
-	; Reload the segment registers
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Load the real mode IDT
-	lidt [idt16]
-	sti
+	; Switch from protected mode to real mode
+	pm2rm
 	
 	; Set up the DAP
 	mov eax, dword [ebp + 8]
@@ -63,30 +25,8 @@ disk_read:
 	mov dl, [drive_num]
 	int 0x13
 	
-	; Reenable protected mode and paging
-	cli
-	mov eax, cr0
-	or eax, 0x80000001
-	mov cr0, eax
-	
-	; Jump to our 32-bit protected mode entry point
-	jmp 0x08:.pm32_exit
-[BITS 32]
-.pm32_exit:
-	; Reload the segment registers
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	
-	; Return from the function
-	pop edi
-	pop esi
-	pop ebx
-	pop ebp
-	ret
+	; Switch from real mode to protected mode
+	rm2pm
 	
 ; Initialize the disk
 global disk_init
@@ -123,6 +63,4 @@ drive_num:
 	dd 0
 
 ; Real mode IDT
-idt16:
-	dw 0x3FF
-	dd 0
+idt
