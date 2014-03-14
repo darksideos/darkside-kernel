2e7cec8ea1bd500098c302d68750fdd7f6315748
George Klees <george.t.klees@gmail.com>
2014-03-13 23:53:17 -0400

    Started hacky VBE code, page fault at framebuffer vaddr

--- kldr/src/init/main.c
+++ kldr/src/init/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <init/loader.h>
+#include <graphics/graphics.h>
 
 /* Boot Application main function */
 void ba_main(loader_block_t *loader_block)
 {
-	printf("Hello world\n");
+	/* Initialize graphics */
+	framebuffer_t *fb = graphics_init(1920, 1280, 32);
+
+	unsigned char *buffer = (unsigned char*) fb->buffer;
+
+	for (int i = 0; i < 8; i++)
+	{
+		buffer[i] = 4;
+	}
+
 	while(1);
 }
