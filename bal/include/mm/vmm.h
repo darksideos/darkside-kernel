385ab7b1eba391e07a12cc49788ab9aa5844fbf4
George Klees <george.t.klees@gmail.com>
2014-03-15 20:43:21 -0400

    Writing to framebuffer still won't work

--- bal/include/mm/vmm.h
+++ bal/include/mm/vmm.h
@@ -5,6 +5,7 @@
 #define PAGE_READ		0x02
 #define PAGE_WRITE		0x04
 #define PAGE_EXECUTE	0x08
+#define PAGE_NOCACHE	0x10
 
 /* Map a virtual address to a physical address */
 void map_page(uint64_t virtual_address, uint64_t physical_address, int flags);
