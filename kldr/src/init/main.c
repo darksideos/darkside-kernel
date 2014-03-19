8364e9d2f01c985957a91ee41340f700913e1a87
Marionumber1 <gksharkboy@gmail.com>
2014-03-19 19:02:52 -0400

    Line drawing fixed

--- kldr/src/init/main.c
+++ kldr/src/init/main.c
@@ -50,6 +50,7 @@ void ba_main(loader_block_t *loader_block)
 	uint32_t *line = (uint32_t*) fb->buffer;
 	line[0] = 0x00FF00FF;
 	int x = 0, y = 0;
+	uint32_t color = 0x00FF0000;
 
 	uint8_t status = inportb(0x64);
 	uint8_t scancode = 0;
@@ -65,7 +66,7 @@ void ba_main(loader_block_t *loader_block)
 			case 0x4d:
 				if (x < fb->width)
 				{
-					line[++x] = 0x00FF00FF;
+					line[++x] = color;
 				}
 
 				break;
@@ -74,7 +75,7 @@ void ba_main(loader_block_t *loader_block)
 				if (y < fb->height)
 				{
 					line = (uint32_t*) (((uint8_t*) line) + (fb->width * 4) + fb->pitch);
-					line[x] = 0x00FF00FF;
+					line[x] = color;
 					y++;
 				}
 
@@ -84,15 +85,35 @@ void ba_main(loader_block_t *loader_block)
 				if (y > 0)
 				{
 					line = (uint32_t*) (((uint8_t*) line) - (fb->width * 4) - fb->pitch);
-					line[x] = 0x00FF00FF;
+					line[x] = color;
 					y--;
 				}
+
+				break;
 			/* Left */
 			case 0x4b:
 				if (x > 0)
 				{
-					line[--x] = 0x00FF00FF;
+					line[--x] = color;
 				}
+
+				break;
+			/* R */
+			case 0x13:
+				color = 0x00FF0000;
+				break;
+			/* G */
+			case 0x22:
+				color = 0x0000FF00;
+				break;
+			/* B */
+			case 0x30:
+				color = 0x000000FF;
+				break;
+			/* W */
+			case 0x11:
+				color = 0x00FFFFFF;
+				break;
 			}
 		}
 
