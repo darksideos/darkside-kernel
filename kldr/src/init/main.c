1c95e3f54dd3a2db58dc8121271215aa097e94e0
Artemis Tosini <me@artem.ist>
2014-03-20 13:01:29 -0400

    Added support for SHIFT but need to fix the following keys: `, 3, 6, -, =, \, ], [

--- kldr/src/init/main.c
+++ kldr/src/init/main.c
@@ -51,6 +51,46 @@ unsigned char kbdus[128] =
     0,	/* All other keys are undefined */
 };
 
+unsigned char kbdus_shft[128] =
+{
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	/* 9 */
+  '(', ')', '_', '+', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'Q', 'W', 'E', 'R',	/* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',	/* 39 */
+ '"', '~',   0,		/* Left shift */
+ '|', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+  'M', '<', '>', '?',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};
+
 /* Boot Application main function */
 void ba_main(loader_block_t *loader_block)
 {
@@ -101,16 +141,35 @@ void ba_main(loader_block_t *loader_block)
 	
 	uint8_t status = inportb(0x64);
 	uint8_t scancode = 0;
+	uint8_t shift = 0;
 	while(1)
 	{	
 		if (status & 1)
 		{
 			scancode = inportb(0x60);
 			
-			/* Make sure it wasn't being released */
-			if(!(scancode & 0x80))
+			/* Released */
+			if(scancode & 0x80)
 			{
-				terminal_putch(kbdus[scancode]);
+				if(scancode == 0xB6 || scancode == 0xAA)
+				{
+					shift = 0;
+				}
+			}
+			else
+			{
+				if(scancode == 0x2A || scancode == 0x36)
+				{
+					shift = 1;
+				}
+				else if(shift)
+				{
+					terminal_putch(kbdus_shft[scancode]);
+				}
+				else
+				{
+					terminal_putch(kbdus[scancode]);
+				}
 			}
 // 
 // 			switch (scancode)
