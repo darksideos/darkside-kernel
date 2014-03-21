ee7ac2b6304279f62a86cc13ef61476477479d42
Artemis Tosini <me@artem.ist>
2014-03-21 11:29:08 -0400

    Started MBR code

---
+++ bal/pc/stage3/include/storage/partition.h
@@ -0,0 +1,20 @@
+#ifndef __PARTITION_H
+#define __PARTITION_H
+
+#include <storage/blockdev.h>
+
+/* Partition structure */
+typedef struct partition
+{
+	/* Generic block device */
+	blockdev_t blockdev;
+
+	/* Parent block device */
+	blockdev_t *parent;
+
+	/* Start and number of sectors */
+	uint64_t start;
+	uint64_t numsectors;
+} partition_t;
+
+#endif
