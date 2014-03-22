526cf81364701f25d7bc3da75547d981f0ff8d9b
George Klees <george.t.klees@gmail.com>
2014-03-22 19:04:37 -0400

    Fixed building

--- libadt/include/dict.h
+++ libadt/include/dict.h
@@ -9,7 +9,7 @@ typedef map_t dict_t;
 /* Dictionary methods */
 dict_t dict_create();
 void dict_destroy(dict_t *dict);
-void dict_insert(dict_t *dict, char *key, void *item);
+void dict_append(dict_t *dict, char *key, void *item);
 void *dict_remove(dict_t *dict, char *key);
 void *dict_get(dict_t *dict, char *key);
 void dict_set(dict_t *dict, char *key, void *item);
