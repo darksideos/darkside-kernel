04c74127f2529372487f212e06ab9ea5c7afd849
Artemis Tosini <me@artem.ist>
2014-04-04 14:21:54 -0400

    Added strtok_r() code from FreeBSD, works

--- libc/src/string.c
+++ libc/src/string.c
@@ -93,7 +93,7 @@ char *strncat(char *dest, char *src, size_t count)
 	return strncpy(dest + strlen(dest), src, count);
 }
 
-char *strtok_r(char *str, char *delimeter, char **saveptr)
+char *my_strtok_r(char *str, char *delimeter, char **saveptr)
 {
 	/* First call */
 	if (str)
@@ -132,6 +132,64 @@ char *strtok_r(char *str, char *delimeter, char **saveptr)
 	(*saveptr) += strlen(delimeter);
 	
 	return retval;
+}
+
+/* Taken from the FreeBSD project */
+char *strtok_r(char *s, const char *delim, char **last)
+{
+	char *spanp, *tok;
+	int c, sc;
+
+	if (s == NULL && (s = *last) == NULL)
+	{
+		return (NULL);
+	}
+
+	/*
+	 * Skip (span) leading delimiters (s += strspn(s, delim), sort of).
+	 */
+cont:
+	c = *s++;
+	for (spanp = (char *)delim; (sc = *spanp++) != 0;)
+	{
+		if (c == sc)
+		{
+			goto cont;
+		}
+	}
+
+	if (c == 0)	/* no non-delimiter characters */
+	{
+		*last = NULL;
+		return (NULL);
+	}
+	tok = s - 1;
+
+	/*
+	 * Scan token (scan for delimiters: s += strcspn(s, delim), sort of).
+	 * Note that delim must have one NUL; we stop if we see that, too.
+	 */
+	for (;;)
+	{
+		c = *s++;
+		spanp = (char *)delim;
+		do
+		{
+			if ((sc = *spanp++) == c)
+			{
+				if (c == 0)
+				{
+					s = NULL;
+				}
+				else
+				{
+					s[-1] = '\0';
+				}
+				*last = s;
+				return (tok);
+			}
+		} while (sc != 0);
+	}
 }
 
 size_t strlen(char *str)
