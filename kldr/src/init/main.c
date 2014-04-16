4668c41fdbcd921855bdcb1552feb48943a93e68
George Klees <george.t.klees@gmail.com>
2014-04-16 11:49:49 -0400

    Trying to figure out why printf() crashes in bios_disk_read()

--- kldr/src/init/main.c
+++ kldr/src/init/main.c
@@ -25,17 +25,6 @@ void ba_main(loader_block_t *loader_block)
 
 	/* Load the boot modules into memory */
 
-	/* Map the graphics framebuffer into memory */
-	paddr_t base = fb->buffer_phys;
-	vaddr_t length = fb->length;
-
-	for (vaddr_t i = 0; i < length; i += 0x1000)
-	{
-		map_page(0x80000000 + i, base + i, PAGE_READ | PAGE_WRITE | PAGE_NOCACHE);
-	}
-
-	fb->buffer = (void*) 0x80000000;
-
 	/* Call the kernel, passing it the loader block */
 
 	while(1);
