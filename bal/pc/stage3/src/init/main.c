86eaad2ce679e89aefc9f18898afc948ccb93abd
George Klees <george.t.klees@gmail.com>
2014-04-16 12:01:33 -0400

    Numbers also fail to display properly with printf()

--- bal/pc/stage3/src/init/main.c
+++ bal/pc/stage3/src/init/main.c
@@ -39,8 +39,15 @@ void bal_main(data_t *_data)
 	list_t *phys_mem_map = pmm_init(data->e820_entries, data->num_e820_entries);
 	vmm_init();
 
+	printf("Initialized the MM\n");
+	printf("By the way, %X\n", 0xDEADBEEF);
+	while(1);
+
 	/* Initialize the storage tree */
 	storage_init(data->drive_number, data->partition_start);
+
+	printf("Storage initialized\n");
+	printf("By the way, %X\n", 0xDEADC0DE);
 
 	/* Initialize the filesystem and each filesystem driver */
 	fs_init();
