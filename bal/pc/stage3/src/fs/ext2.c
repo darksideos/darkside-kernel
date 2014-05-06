4f0c4638b2f3f5303f1c0de62e45acfdcdf9b2ff
Marionumber1 <gksharkboy@gmail.com>
2014-05-05 20:43:46 -0400

    Added infinite loop to read_block_pointer() if level is greater than 3

--- bal/pc/stage3/src/fs/ext2.c
+++ bal/pc/stage3/src/fs/ext2.c
@@ -154,7 +154,12 @@ static uint32_t read_block_pointer(filesystem_t *filesystem, void *buffer, uint3
 
 		return length - bytes_left;
 	}
-	return 0;
+	/* Condition that should never happen */
+	else
+	{
+		while(1);
+		return 0;
+	}
 }
 
 /* Read data from an inode into a buffer */
