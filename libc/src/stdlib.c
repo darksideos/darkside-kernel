12c5cc4c3f2cc978342e0b0235131df7b919c4cd
Noah Singer <singerng@gmail.com>
2014-05-05 21:13:23 -0400

    Create stdlib.c

---
+++ libc/src/stdlib.c
@@ -0,0 +1,15 @@
+#include <types.h>
+#include <stdlib.h>
+ 
+uint32_t str2dec(uint8_t *str)
+{
+        uint32_t num = 0;
+ 
+        while (*str)
+        {
+                num = (num * 10) + ((*str) - '0');
+                str++;
+        }
+ 
+        return num;
+}
