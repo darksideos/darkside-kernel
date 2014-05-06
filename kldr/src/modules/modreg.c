075aa2752f48d3c942708a9185ef1a822923fd98
Noah Singer <singerng@gmail.com>
2014-05-05 21:04:06 -0400

    Update modreg.c
    Making important changing and cleaning up

--- kldr/src/modules/modreg.c
+++ kldr/src/modules/modreg.c
@@ -1,46 +1,49 @@
-#include <init/module_registry.h>
-#include <fs/ext2.h>
+#include <types.h>
+#include <modules/modreg.h>
+
 #include <fs/fs.h>
-#include <lib/libc/string.h>
-#include <init/os_info.h>
-#include <init/kprintf.h>
-#include <vga/vga.h>
-#include <lib/libc/string.h>
-#include <lib/libc/str2int.h>
-#include <lib/libc/types.h>
-#include <lib/libadt/map.h>
-#include <lib/libadt/index_tree.h>
+#include <modules/tree_utils.h>
+#include <modules/text_utils.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <str2int.h>
+#include <tree.h>
 
 extern fs_context_t *fs;
 
-void parse_registry(os_info_t *os_info)
+tree_t parse_registry()
 {	
 	/* Read the module registry into memory */
 	inode_t *registry_inode = fs_open(fs, "/boot/modules/registry");
 	
-	unsigned char *registry_data = kmalloc(registry_inode->low_size + 1);
+	uint8_t *registry_data = malloc(registry_inode->low_size + 1);
 	fs_read(fs, registry_inode, registry_data, registry_inode->low_size);
 	
-	unsigned char *saveptr = 0;
-	unsigned char *line = strtok(registry_data, "\n", &saveptr);
-	unsigned int lineNumber = 0;
+	/* We're reading line-by-line */
+	uint8_t *saveptr = 0;
+	uint8_t *line = strtok(registry_data, "\n", &saveptr);
+	uint32_t lineNumber = 0;
 	
 	module_t *module = 0;
-	int oldLevel = 0;
+	uint32_t oldLevel = 0;
 	
-	index_tree_t tree = index_tree_create();
-	struct index_tree_node *parent = tree.root;
+	tree_t tree = index_tree_create();
+	struct tree_node *parent = tree.root;
 	
+	/* Until we encounter EOF... */
 	while(line != 0)
 	{
 		/* Separate out tabs from data, returning the number of tabs and advancing the string pointer */
 		unsigned int tabLevel = separate_indents(&line);
+		
+		/* newLevel is the level at the end, oldLevel is the level at the beginning of the loop body */
 		unsigned int newLevel = oldLevel;
 		
 		/* We're in "module mode" */
 		if(module)
 		{
-			/* Continuing a module declaration, so we're inside */
+			/* The current level is one more than the level which opened the module declaration */
 			if(tabLevel == oldLevel + 1)
 			{
 				if(strnequal("@NAME", line, 5))
@@ -65,8 +68,8 @@ void parse_registry(os_info_t *os_info)
 				}
 				else if(strnequal("@VERSION", line, 8))
 				{
-					unsigned char *saveptr2 = 0;
-					unsigned char *num = strtok(line + 9, ".", &saveptr2);
+					uint8_t *saveptr2 = 0;
+					uint8_t *num = strtok(line + 9, ".", &saveptr2);
 					module->major = str2dec(num);
 					num = strtok(0, ".", &saveptr2);
 					module->minor = str2dec(num);
@@ -75,57 +78,60 @@ void parse_registry(os_info_t *os_info)
 				}
 				else
 				{
-					kprintf(LOG_PANIC, "Syntax error: module registry command unrecognized at line %d: %s\n", lineNumber, line);
+					printf("Syntax error: module registry command unrecognized at line %d: %s\n", lineNumber, line);
 					while(1);
 				}
 			}
-			/* Ending a module declaration */
+			/* Ending a module declaration, because we're no longer one
+			 * further in than the line that started the declaration */
 			else
 			{
 				/* Write the module to the tree */
-				index_tree_node_set_data(parent, module);
+				tree_node_set_data(parent, module);
 				module = 0;
 				
-				parent = index_tree_node_parent(parent);
+				parent = tree_node_parent(parent);
 			}
 		}
 		
-		/* We're changing levels */
+		/* Since we're not in a module declaration, we need to shift tab levels */
 		if(!module)
 		{
-			/* Sub out */
+			/* We are "subbing out" -- because we were farther in than we need to be */
 			while (newLevel > tabLevel)
 			{
-				parent = index_tree_node_parent(parent);
+				parent = tree_node_parent(parent);
 				newLevel--;
 			}
 			
 			/* We are beginning a module declaration */
 			if(strequal(line, "@MODULE"))
 			{
-				module = kmalloc(sizeof(module_t));
+				module = malloc(sizeof(module_t));
 			}
-			/* We are "subbing in" -- going another level into the tree */
+			/* We are "subbing in" -- going another level into the tree
+			 * Verifying that it's not an empty line */
 			else if(!strequal(line, ""))
 			{
-				struct index_tree_node *child = index_tree_node_create(parent, 0);
-				index_tree_node_insert(parent, child, tree_index(line, lineNumber));
+				struct tree_node *child = index_tree_node_create(parent, 0);
+				tree_node_insert(parent, child, tree_index(line, lineNumber));
 				parent = child;
 				
 				newLevel = tabLevel;
 			}
 		}
 		
+		/* Update the level at which we start the next iteration */
 		oldLevel = newLevel;
 		line = strtok(0, "\n", &saveptr);
 		lineNumber++;
 	}
 	
+	/* In case we're not done the declaration when we get EOF */
 	if (module)
 	{	
-		index_tree_node_set_data(parent, module);
+		tree_node_set_data(parent, module);
 	}
-	os_info->module_registry = tree;
 	
-	kprintf(LOG_INFO, "Parsed module registry\n");
+	return tree;
 }
