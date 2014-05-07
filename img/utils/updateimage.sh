085616fe893f672d56399dbf5fbfffbbb6d7aa3c
Noah Singer <singerng@gmail.com>
2014-05-06 22:14:12 -0400

    Fixed Artemis's screw up with dd (thanks Artemis), os.img is no longer 8GB

--- img/utils/updateimage.sh
+++ img/utils/updateimage.sh
@@ -35,7 +35,9 @@ if [[ "$(uname -s)" == CYGWIN* ]]
 		read -p "Waiting for kernel-i386.elf to be copied into ext2.img [Press Enter to continue]..."
 fi
 
-dd if=img/images/ext2.img of=img/images/os.img bs=4M seek=2048 conv=notrunc
+read -p "HI"
+
+dd if=img/images/ext2.img of=img/images/os.img bs=1048576 seek=1 conv=notrunc
 
 if [[ "$(uname -s)" == CYGWIN* ]]
 	then
