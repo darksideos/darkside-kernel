14f7a435de14d5cc95b11eb1aeb82831a12b6114
Noah Singer <singerng@gmail.com>
2014-05-06 22:27:53 -0400

    Fixed a potential mistake in ext2.c

--- bal/pc/stage3/src/fs/ext2.c
+++ bal/pc/stage3/src/fs/ext2.c
@@ -170,37 +170,40 @@ static uint64_t ext2_inode_read(inode_t *node, void *buffer, uint64_t offset, ui
 	/* Number of bytes left to read and number of direct blocks read */
 	uint32_t bytes_left = (uint32_t) length;
 	uint32_t direct_blocks_read = 0;
+	
+	void *newbuf = buffer;
+	uint64_t newlen = length;
 
 	/* First, read each direct block pointer */
 	while (bytes_left > 0 && direct_blocks_read < 12)
 	{
-		bytes_left -= read_block_pointer(node->filesystem, buffer, ext2_node->direct_block[direct_blocks_read], bytes_left, 0);
-		buffer += length - bytes_left;
-		length -= (length - bytes_left);
+		bytes_left -= read_block_pointer(node->filesystem, newbuf, ext2_node->direct_block[direct_blocks_read], bytes_left, 0);
+		newbuf = buffer + length - bytes_left;
+		newlen = length - bytes_left;
 		direct_blocks_read++;
 	}
 
 	/* If that's not enough, try the singly, doubly, and triply indirect block pointers */
 	if (bytes_left > 0)
 	{
-		bytes_left -= read_block_pointer(node->filesystem, buffer, ext2_node->single_block, bytes_left, 1);
-		buffer += length - bytes_left;
-		length -= (length - bytes_left);
+		bytes_left -= read_block_pointer(node->filesystem, newbuf, ext2_node->single_block, bytes_left, 1);
+		newbuf = buffer + length - bytes_left;
+		newlen = length - bytes_left;
 	}
 	if (bytes_left > 0)
 	{
-		bytes_left -= read_block_pointer(node->filesystem, buffer, ext2_node->double_block, bytes_left, 2);
-		buffer += length - bytes_left;
-		length -= (length - bytes_left);
+		bytes_left -= read_block_pointer(node->filesystem, newbuf, ext2_node->double_block, bytes_left, 2);
+		newbuf = buffer + length - bytes_left;
+		newlen = length - bytes_left;
 	}
 	if (bytes_left > 0)
 	{
-		bytes_left -= read_block_pointer(node->filesystem, buffer, ext2_node->triple_block, bytes_left, 3);
-		buffer += length - bytes_left;
-		length -= (length - bytes_left);
+		bytes_left -= read_block_pointer(node->filesystem, newbuf, ext2_node->triple_block, bytes_left, 3);
+		newbuf = buffer + length - bytes_left;
+		newlen = length - bytes_left;
 	}
 
-	return length;
+	return newlen;
 }
 
 /* Get a child inode by name from the inode */
