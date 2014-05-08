423dda46cb3f754c827445224ef22e3259967c81
Artemis Tosini <me@artem.ist>
2014-05-08 10:36:42 -0400

    Pushing all work on laptop

--- kldr/src/executable/elf.c
+++ kldr/src/executable/elf.c
@@ -1,13 +1,27 @@
 #include <types.h>
 #include <mm/pmm.h>
 #include <mm/vmm.h>
+#include <fs/fs.h>
 #include <executable/executable.h>
 #include <executable/elf.h>
 
 /* Load a standard executable */
 executable_t *elf_executable_load_executable(char *filename)
 {
-	return NULL;
+	/* Open the ELF file */
+	inode_t *executable = fs_open(filename)
+	if (!executable)
+	{
+		return NULL;
+	}
+
+	/* Read and check the header */
+	elf_header_t header;
+	uint64_t bytes_read = fs_read(executable, &header, 0, sizeof(elf_header_t));
+	if (bytes_read != sizeof(elf_header_t))
+	{
+		return NULL;
+	}
 }
 
 /* Load an object file */
