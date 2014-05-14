eb1281e033ed3361f5e06650b46e0c7e69535155
George Klees <george.t.klees@gmail.com>
2014-05-14 18:25:49 -0400

    We can now use C in the kernel

---
+++ kernel/include/init/loader.h
@@ -0,0 +1,21 @@
+#ifndef __LOADER_H
+#define __LOADER_H
+
+#include <list.h>
+#include <init/graphics.h>
+
+/* Loader block structure */
+typedef struct loader_block
+{
+	/* Physical memory map */
+	list_t *phys_mem_map;
+
+	/* Graphics framebuffer */
+	framebuffer_t *fb;
+
+	/* Module registry */
+
+	/* Loaded modules */
+} loader_block_t;
+
+#endif
