e7701e5b619dbf8956e7f95d4c6a020861d06d50
Marionumber1 <gksharkboy@gmail.com>
2014-05-14 20:33:53 -0400

    Written PFN database code

---
+++ kernel/src/mm/heap.c
@@ -0,0 +1,21 @@
+#include <types.h>
+
+/* Allocate memory from the kernel heap */
+void *malloc(size_t size)
+{
+	/* Not implemented */
+	return NULL;
+}
+
+/* Free memory from the kernel heap */
+void free(void *ptr)
+{
+	/* Not implemented */
+}
+
+/* Reallocate memory from the kernel heap */
+void *realloc(void *ptr, size_t size)
+{
+	/* Not implemented */
+	return NULL;
+}
