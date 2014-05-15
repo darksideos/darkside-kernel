7e0f350eb50714d61d364afa9e23d4f3ba7d8821
Marionumber1 <gksharkboy@gmail.com>
2014-05-15 19:31:31 -0400

    Starting freelist.c

--- kernel/src/mm/freelist.c
+++ kernel/src/mm/freelist.c
@@ -1,6 +1,9 @@
 #include <types.h>
 #include <mm/page.h>
 
+/* Free page lists for 32-bit and 64-bit processes */
+static page_t *free_list_32, *free_list_64;
+
 /* Initialize the free memory list */
 void freelist_init()
 {
