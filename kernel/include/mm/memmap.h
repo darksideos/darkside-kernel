4a34313ee10b8b2dfbdb607264e7dec1a74f03f5
George Klees <george.t.klees@gmail.com>
2014-05-14 20:10:12 -0400

    Adding PMM headers

---
+++ kernel/include/mm/memmap.h
@@ -0,0 +1,24 @@
+#ifndef __MEMMAP_H
+#define __MEMMAP_H
+
+/* Memory map entry flags */
+#define MEM_FLAG_USABLE		0x01
+#define MEM_FLAG_FREE		0x02
+#define MEM_FLAG_BOOT		0x04
+#define MEM_FLAG_SYSTEM		0x08
+#define MEM_FLAG_FIRMWARE	0x10
+#define MEM_FLAG_ROM		0x20
+#define MEM_FLAG_MMIO		0x40
+#define MEM_FLAG_VOLATILE	0x80
+#define MEM_FLAG_HOTPLUG	0x100
+
+/* Memory map entry structure */
+typedef struct mem_map_entry
+{
+	uint64_t base;
+	uint64_t length;
+	uint32_t flags;
+	uint32_t numa_domain;
+} mem_map_entry_t;
+
+#endif
---
+++ kernel/include/mm/page.h
@@ -0,0 +1,41 @@
+#ifndef __PAGE_H
+#define __PAGE_H
+
+#include <list.h>
+#include <microkernel/lock.h>
+
+/* Memory map entry flags */
+#define PAGE_FLAG_USABLE	0x01
+#define PAGE_FLAG_FREE		0x02
+#define PAGE_FLAG_BOOT		0x04
+#define PAGE_FLAG_SYSTEM	0x08
+#define PAGE_FLAG_FIRMWARE	0x10
+#define PAGE_FLAG_ROM		0x20
+#define PAGE_FLAG_MMIO		0x40
+#define PAGE_FLAG_VOLATILE	0x80
+#define PAGE_FLAG_HOTPLUG	0x100
+
+/* Page information structure */
+typedef struct page
+{
+	/* Page flags and NUMA domain */
+	uint32_t flags;
+	uint32_t numa_domain;
+
+	/* Reference count */
+	uint32_t refcount;
+
+	/* Spinlock */
+	spinlock_t lock;
+
+	/* Next page in its list */
+	struct page *next;
+} page_t;
+
+/* Get a page in the PFN database by address */
+page_t *pfn_database_get(paddr_t address);
+
+/* Initialize the PFN database from a physical memory map */
+void pfn_database_init(vaddr_t location, list_t *phys_mem_map);
+
+#endif
