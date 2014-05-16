eb0b0ced5f5970cd4fc5e55344ed977e62ac012d
George Klees <george.t.klees@gmail.com>
2014-05-15 20:21:55 -0400

    bal_enter_kernel() is planned to allocate the per-CPU and per-NUMA domain regions

--- bal/pc/stage3/src/init/kernel.c
+++ bal/pc/stage3/src/init/kernel.c
@@ -4,6 +4,9 @@
 /* Jump to the kernel */
 void bal_enter_kernel(vaddr_t entry_point, loader_block_t *loader_block)
 {
+	/* Allocate the per-CPU and per-NUMA domain regions */
+
+	/* Jump to the kernel entry point, passing the loader block */
 	__asm__ ("push %0\n\tjmp *%1"
 	:
 	: "r" (loader_block), "r" (entry_point));
