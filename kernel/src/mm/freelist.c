2af3b88c9f30be26d8d94b48443f64deb9e319df
George Klees <george.t.klees@gmail.com>
2014-05-17 17:53:40 -0400

    About to write freelist code, but still lacking some things

--- kernel/src/mm/freelist.c
+++ kernel/src/mm/freelist.c
@@ -1,8 +1,6 @@
 #include <types.h>
+#include <mm/pfn.h>
 #include <mm/page.h>
-
-/* Free page lists for 32-bit and 64-bit processes */
-static page_t *free_list_32, *free_list_64;
 
 /* Initialize the free memory list */
 void freelist_init()
