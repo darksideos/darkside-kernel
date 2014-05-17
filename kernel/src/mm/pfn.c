7522e852b5d81ab83ceac0fe288f91099cd31602
George Klees <george.t.klees@gmail.com>
2014-05-17 10:39:21 -0400

    PFN database creation code crashes somewhere inside the free area

--- kernel/src/mm/pfn.c
+++ kernel/src/mm/pfn.c
@@ -35,7 +35,7 @@ void pfn_database_init(loader_block_t *loader_block)
 	paddr_t index = 0;
 	while (entry)
 	{
-		printf("Base: 0x%08X, length: 0x%08X\n", (uint32_t) entry->base, (uint32_t) entry->length);
+		printf("Base: 0x%08X, length: 0x%08X, flags: %d\n", (uint32_t) entry->base, (uint32_t) entry->length, entry->flags);
 
 		/* Create page entries for each page in the entry */
 		for (uint64_t i = 0; i < entry->base + entry->length; i += 0x1000)
