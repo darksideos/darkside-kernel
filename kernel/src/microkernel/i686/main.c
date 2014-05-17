173e381f981030cda249bcf26fbb727627213c7d
Marionumber1 <gksharkboy@gmail.com>
2014-05-16 22:12:22 -0400

    printf() works in the kernel now

--- kernel/src/microkernel/i686/main.c
+++ kernel/src/microkernel/i686/main.c
@@ -1,5 +1,6 @@
 #include <types.h>
 #include <string.h>
+#include <bootvid.h>
 #include <init/loader.h>
 #include <mm/pfn.h>
 #include <mm/freelist.h>
@@ -12,6 +13,9 @@ void microkernel_init(loader_block_t *_loader_block, int cpu)
 	/* Running on the BSP */
 	if (cpu == 0)
 	{
+		/* Initialize the boot video driver */
+		bootvid_init(COLOR_WHITE, COLOR_BLACK);
+
 		/* Copy the loader block onto the initial kernel stack */
 		loader_block_t loader_block;
 		memcpy(&loader_block, _loader_block, sizeof(loader_block_t));
@@ -20,7 +24,7 @@ void microkernel_init(loader_block_t *_loader_block, int cpu)
 		printf("Hello world!\n");
 		printf("0x%08X in decimal is %u\n", 0xDEADBEEF, 0xDEADBEEF);
 		printf("I really enjoy %s\n", "printing strings");
-		//while(1);
+		while(1);
 
 		/* Use the physical memory map to create the PFN database */
 		pfn_database_init(&loader_block);
