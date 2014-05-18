bbeff70c6540344773fa36e53edd92968dd0502f
Marionumber1 <gksharkboy@gmail.com>
2014-05-17 21:42:17 -0400

    Moved PFN database allocation into a separate file

--- bal/pc/stage3/src/init/kernel.c
+++ bal/pc/stage3/src/init/kernel.c
@@ -1,10 +1,18 @@
 #include <types.h>
 #include <init/loader.h>
+#include <mm/pmm.h>
+#include <mm/vmm.h>
 
 /* Jump to the kernel */
 void bal_enter_kernel(vaddr_t entry_point, loader_block_t *loader_block)
 {
-	/* Allocate the per-CPU and per-NUMA domain regions */
+	/* Allocate the per-CPU and per-NUMA domain data areas */
+	vaddr_t cpu_data_area = loader_block->pfn_database + ((loader_block->phys_mem_size / 0x1000) * 24);
+	if (cpu_data_area & 0xFFF)
+	{
+		cpu_data_area = (cpu_data_area & 0xFFFFF000) + 0x1000;
+	}
+	map_page(cpu_data_area, pmm_alloc_page(), PAGE_READ | PAGE_WRITE);
 
 	/* Jump to the kernel entry point, passing the loader block */
 	__asm__ ("push %0\n\tjmp *%1"
