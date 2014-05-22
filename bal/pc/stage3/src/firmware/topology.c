2e45b18915dbba77b476b3320cbf5d91f747027c
George Klees <george.t.klees@gmail.com>
2014-05-21 23:37:08 -0400

    Written per-CPU and NUMA domain allocation code (may be very buggy)

--- bal/pc/stage3/src/firmware/topology.c
+++ bal/pc/stage3/src/firmware/topology.c
@@ -1,8 +1,11 @@
 #include <types.h>
+#include <map.h>
 #include <init/loader.h>
-
+#include <mm/pmm.h>
+#include <mm/vmm.h>
 #include <firmware/acpi.h>
 #include <firmware/madt.h>
+#include <firmware/srat.h>
 
 /* Allocate the per-CPU and NUMA domain data structures */
 void per_cpu_numa_area_alloc(loader_block_t *loader_block)
@@ -16,7 +19,7 @@ void per_cpu_numa_area_alloc(loader_block_t *loader_block)
 	loader_block->cpu_data_area = cpu_data_area;
 
 	/* Get the MADT */
-	struct madt *madt = acpi_find_table(MADT_SIGNATURE);
+	struct madt *madt = (struct madt*) acpi_find_table(MADT_SIGNATURE);
 	
 	/* Allocate the per-CPU data structures */
 	void *data = ((void*) madt) + sizeof(struct madt);
@@ -45,7 +48,64 @@ void per_cpu_numa_area_alloc(loader_block_t *loader_block)
 		}
 		
 		/* Continue in the loop */
-		offset += entry->length;
+		i += entry->length;
+		data += entry->length;
+	}
+
+	/* Calculate the start of the per-NUMA domain data area */
+	vaddr_t numa_domain_data_area = cpu_data_area;
+	loader_block->numa_domain_data_area = numa_domain_data_area;
+
+	/* Get the SRAT */
+	struct srat *srat = (struct srat*) acpi_find_table(SRAT_SIGNATURE);
+
+	/* Create a map of NUMA domain numbers to NUMA domain data structures */
+	map_t numa_domains = map_create();
+
+	/* Allocate the per-NUMA domain data structures */
+	data = ((void*) srat) + sizeof(struct srat);
+	i = 0;
+	while (i < srat->header.length - sizeof(struct srat))
+	{
+		/* For a Local APIC entry, allocate the per-NUMA domain data structure */
+		struct srat_entry_header *entry = (struct srat_entry_header*) data;
+		if (entry->type == SRAT_TYPE_LAPIC)
+		{
+			/* This is a LAPIC entry */
+			struct srat_lapic_entry *lapic_entry = (struct srat_lapic_entry*) data;
+
+			/* Build the NUMA domain through weird bit shifts */
+			uint32_t numa_domain = (lapic_entry->numa_domain_high[0] << 24) | (lapic_entry->numa_domain_high[1] << 16) | (lapic_entry->numa_domain_high[2] << 8) | lapic_entry->numa_domain_low;
+
+			/* If it's not already in the NUMA domain map, allocate and map it */
+			vaddr_t current_numa_data_area = (vaddr_t) map_get(&numa_domains, numa_domain);
+			if (!current_numa_data_area)
+			{
+				/* Allocate a page and map it */
+				map_page(numa_domain_data_area, pmm_alloc_page(), PAGE_READ | PAGE_WRITE);
+				current_numa_data_area = numa_domain_data_area;
+				map_append(&numa_domains, numa_domain, (void*) numa_domain_data_area);
+
+				/* Advance one page */
+				numa_domain_data_area += 0x1000;
+			}
+
+			/* Find the CPU with the Local APIC ID and set its NUMA domain */
+			uint32_t *cpu = (uint32_t*) loader_block->cpu_data_area;
+			while ((cpu[0] != (uint32_t) lapic_entry->lapic_id) && (cpu < loader_block->numa_domain_data_area))
+			{
+				cpu += 0x400;
+			}
+
+			/* If we found the CPU */
+			if (cpu < loader_block->numa_domain_data_area)
+			{
+				cpu[1] = (uint32_t) current_numa_data_area;
+			}
+		}
+		
+		/* Continue in the loop */
+		i += entry->length;
 		data += entry->length;
 	}
 }
