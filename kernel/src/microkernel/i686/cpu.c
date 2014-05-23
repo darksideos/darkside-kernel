189a202b44bbec615edb1829cc767b046be2b932
George Klees <george.t.klees@gmail.com>
2014-05-22 21:41:07 -0400

    cpu_data_area() and numa_domain_data_area() should work, but cpu_data_area() just hardcodes CPU 0 for now

--- kernel/src/microkernel/i686/cpu.c
+++ kernel/src/microkernel/i686/cpu.c
@@ -8,13 +8,29 @@ static numa_domain_t *per_numa_domain_area;
 /* Get a pointer to the per-CPU data area */
 cpu_t *cpu_data_area(int cpu)
 {
+	/* Get the data area for the current CPU */
+	if (cpu == CPU_CURRENT)
+	{
+		/* TODO: Actually get the current CPU */
+		cpu = 0;
+	}
+
 	return &per_cpu_area[cpu];
 }
 
 /* Get a pointer to the per-NUMA domain data area */
 numa_domain_t *numa_domain_data_area(int numa_domain)
 {
-	return &per_numa_domain_area[numa_domain];
+	/* Get the the data area for the current NUMA domain */
+	if (numa_domain == NUMA_DOMAIN_CURRENT)
+	{
+		return cpu_data_area(CPU_CURRENT)->numa_domain;
+	}
+	/* Get the data area for a specific NUMA domain */
+	else
+	{
+		return &per_numa_domain_area[numa_domain];
+	}
 }
 
 /* Initialize the per-CPU data area */
