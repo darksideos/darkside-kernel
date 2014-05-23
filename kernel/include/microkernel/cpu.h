85ed2302d25b5661cba122a539e4737dde92150d
Marionumber1 <gksharkboy@gmail.com>
2014-05-22 21:41:07 -0400

    cpu_data_area() and numa_domain_data_area() should work, but cpu_data_area() just hardcodes CPU 0 for now

--- kernel/include/microkernel/cpu.h
+++ kernel/include/microkernel/cpu.h
@@ -8,6 +8,12 @@
 #include <microkernel/i686/cpu.h>
 #endif
 
+/* Macros for special CPU numbers */
+#define CPU_CURRENT				-1
+
+/* Macros for special NUMA domain numbers */
+#define NUMA_DOMAIN_CURRENT		-1
+
 /* Get a pointer to the per-CPU and per-NUMA domain data areas */
 cpu_t *cpu_data_area(int cpu);
 numa_domain_t *numa_domain_data_area(int numa_domain);
