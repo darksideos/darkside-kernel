7ce69c7465212384eb813f91a9266ce0fc095b2b
George Klees <george.t.klees@gmail.com>
2014-05-29 20:37:22 -0400

    Written code for merging memmap entry flags

--- kernel/src/mm/pfn.c
+++ kernel/src/mm/pfn.c
@@ -50,6 +50,103 @@ void pfn_database_init(loader_block_t *loader_block)
 	while (entry)
 	{
 		printf("Base: 0x%08X, length: 0x%08X, flags: 0x%08X\n", (uint32_t) entry->base, (uint32_t) entry->length, entry->flags);
+
+		/* If we're in the middle of a page in our entry, get to a page boundary */
+		if (entry->base & 0xFFF)
+		{
+			/* Try to get to a page boundary */
+			uint32_t to_next_page = 0;
+			to_next_page = 0x1000 - (entry->base & 0xFFF);
+			entry->base += to_next_page;
+
+			/* Can we remove this much and still have an entry left */
+			if (entry->length > to_next_page)
+			{
+				entry->length -= to_next_page;
+			}
+			else
+			{
+				entry->length = 0;
+			}
+
+			/* Merge the flags with the previous entry */
+			paddr_t index_prev = index - 1;
+			if (vmm_get_mapping(-1, (vaddr_t) &pfn_database_entries[index_prev]) != -1)
+			{
+				/* Pointer to the page info */
+				page_t *page = &pfn_database_entries[index_prev];
+
+				/* If the previous entry was in a different NUMA domain, it's invalid */
+				if (page->numa_domain != entry->numa_domain)
+				{
+					page->flags = 0;
+					page->numa_domain = 0xFFFFFFFF;
+				}
+				/* If the previous entry was usable and this is, make it unusable */
+				if ((page->flags & PAGE_FLAG_USABLE) && !(entry->flags & MEM_FLAG_USABLE))
+				{
+					page->flags = 0;
+				}
+				/* If the previous entry wasn't bad and this is, make it bad */
+				if (!(page->flags & PAGE_FLAG_BAD) && (entry->flags & MEM_FLAG_BAD))
+				{
+					page->flags |= PAGE_FLAG_BAD;
+				}
+				/* If the previous entry was free */
+				if (page->flags & PAGE_FLAG_FREE)
+				{
+					/* Ours is firmware volatile */
+					if ((entry->flags & MEM_FLAG_FIRMWARE) && (entry->flags & MEM_FLAG_VOLATILE))
+					{
+						page->flags &= ~PAGE_FLAG_FREE;
+						page->flags |= (PAGE_FLAG_FIRMWARE | PAGE_FLAG_VOLATILE);
+					}
+					/* Ours is firmware */
+					else if (entry->flags & MEM_FLAG_FIRMWARE)
+					{
+						page->flags &= ~PAGE_FLAG_FREE;
+						page->flags |= PAGE_FLAG_FIRMWARE;
+					}
+					/* Ours is something else */
+					else
+					{
+						page->flags &= ~PAGE_FLAG_FREE;
+					}
+				}
+				/* If the previous entry was firmware */
+				if (page->flags & PAGE_FLAG_FIRMWARE)
+				{
+					/* Theirs is firmware-volatile but ours is only firmware */
+					if ((page->flags & PAGE_FLAG_VOLATILE) && (entry->flags & MEM_FLAG_FIRMWARE) && !(entry->flags & MEM_FLAG_VOLATILE))
+					{
+						page->flags &= ~PAGE_FLAG_VOLATILE;
+					}
+					/* Theirs is firmware-volatile and ours is free */
+					else if ((page->flags & PAGE_FLAG_VOLATILE) && (entry->flags & MEM_FLAG_FREE))
+					{
+					}
+					/* Theirs is firmware and ours is something else */
+					else
+					{
+						page->flags &= ~PAGE_FLAG_FIRMWARE;
+					}
+				}
+				/* If the previous entry was MMIO and ours is not */
+				if ((page->flags & PAGE_FLAG_MMIO) && !(entry->flags & MEM_FLAG_MMIO))
+				{
+					page->flags &= ~PAGE_FLAG_MMIO;
+				}
+				/* If the previous entry was hotplug and ours is not */
+				if ((page->flags & PAGE_FLAG_HOTPLUG) && !(entry->flags & MEM_FLAG_HOTPLUG))
+				{
+					page->flags &= ~PAGE_FLAG_HOTPLUG;
+				}
+
+				/* Transfer over all other flags */
+				page->flags |= (entry->flags & (PAGE_FLAG_BOOT | PAGE_FLAG_SYSTEM));
+			}
+		}
+
 		/* Create page entries for each page in the entry */
 		for (uint64_t i = entry->base; i < entry->base + entry->length; i += 0x1000)
 		{
