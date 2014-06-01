7d7e0dc1d2a0ed4d78039d35af1e5982f425e83d
George Klees <george.t.klees@gmail.com>
2014-05-31 20:21:52 -0400

    Free list code will be in the microkernel

--- kernel/include/mm/freelist.h
+++ kernel/include/mm/freelist.h
@@ -1,22 +1,20 @@
 #ifndef __FREELIST_H
 #define __FREELIST_H
 
-#include <init/loader.h>
-
 /* Allocation flags */
-#define PAGE_DMA	0x00
-#define PAGE_32BIT	0x01
-#define PAGE_64BIT	0x02
+#define PAGE_DMA	0x01
+#define PAGE_32BIT	0x02
+#define PAGE_64BIT	0x03
 
 /* Allocate one or more physical pages */
-paddr_t pmm_alloc_page(int flags);
-paddr_t pmm_alloc_pages(int num_pages, int flags);
+paddr_t pmm_alloc_page(int flags, int color);
+paddr_t pmm_alloc_pages(int num_pages, int flags, int color);
 
 /* Free one or more physical pages */
 void pmm_free_page(paddr_t address);
 void pmm_free_pages(paddr_t address, int num_pages);
 
-/* Initialize the free memory list */
-void freelist_init(loader_block_t *loader_block);
+/* Initialize the free page list manager */
+void freelist_init();
 
 #endif
