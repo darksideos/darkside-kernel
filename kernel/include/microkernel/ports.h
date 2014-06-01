6eb11d4c751469a5d2a28429906d0d58f50946c7
George Klees <george.t.klees@gmail.com>
2014-06-01 16:18:40 -0400

    Added 8259 PIC code

--- kernel/include/microkernel/ports.h
+++ kernel/include/microkernel/ports.h
@@ -4,21 +4,24 @@
 #include <types.h>
 
 /* Read a byte from an I/O port */
-uint8_t io_read_8(uint8_t *port);
+uint8_t io_read_8(uint32_t port);
 
 /* Write a byte to an I/O port */
-void io_write_8(uint8_t *port, uint8_t data);
+void io_write_8(uint32_t port, uint8_t data);
 
 /* Read a word from an I/O port */
-uint16_t io_read_16(uint16_t *port);
+uint16_t io_read_16(uint32_t port);
 
 /* Write a word to an I/O port */
-void io_write_16(uint16_t *port, uint16_t data);
+void io_write_16(uint32_t port, uint16_t data);
 
 /* Read a dword from an I/O port */
-uint32_t io_read_32(uint32_t *port);
+uint32_t io_read_32(uint32_t port);
 
 /* Write a dword to an I/O port */
-void io_write_32(uint32_t *port, uint32_t data);
+void io_write_32(uint32_t port, uint32_t data);
+
+/* Wait for an I/O port access to complete */
+void io_wait();
 
 #endif
--- kernel/src/microkernel/i686/ports.c
+++ kernel/src/microkernel/i686/ports.c
@@ -1,7 +1,7 @@
 #include <types.h>
 
 /* Read a byte from an I/O port */
-uint8_t io_read_8(uint8_t *port)
+uint8_t io_read_8(uint32_t port)
 {
 	uint8_t rv;
 	__asm__ volatile ("inb %1, %0" : "=a" (rv) : "dN" ((uint16_t)port));
@@ -9,13 +9,13 @@ uint8_t io_read_8(uint8_t *port)
 }
 
 /* Write a byte to an I/O port */
-void io_write_8(uint8_t *port, uint8_t data)
+void io_write_8(uint32_t port, uint8_t data)
 {
 	__asm__ volatile ("outb %1, %0" : : "dN" ((uint16_t)port), "a" (data));
 }
 
 /* Read a word from an I/O port */
-uint16_t io_read_16(uint16_t *port)
+uint16_t io_read_16(uint32_t port)
 {
 	uint16_t rv;
 	__asm__ volatile ("inw %1, %0" : "=a" (rv) : "dN" ((uint16_t)port));
@@ -23,13 +23,13 @@ uint16_t io_read_16(uint16_t *port)
 }
 
 /* Write a word to an I/O port */
-void io_write_16(uint16_t *port, uint16_t data)
+void io_write_16(uint32_t port, uint16_t data)
 {
 	__asm__ volatile ("outw %1, %0" : : "dN" ((uint16_t)port), "a" (data));
 }
 
 /* Read a dword from an I/O port */
-uint32_t io_read_32(uint32_t *port)
+uint32_t io_read_32(uint32_t port)
 {
 	uint32_t rv;
 	__asm__ volatile ("inl %1, %0" : "=a" (rv) : "dN" ((uint16_t)port));
@@ -37,7 +37,13 @@ uint32_t io_read_32(uint32_t *port)
 }
 
 /* Write a dword to an I/O port */
-void io_write_32(uint32_t *port, uint32_t data)
+void io_write_32(uint32_t port, uint32_t data)
 {
 	__asm__ volatile ("outl %1, %0" : : "dN" ((uint16_t)port), "a" (data));
+}
+
+/* Wait for an I/O port access to complete */
+void io_wait()
+{
+    __asm__ volatile ("jmp 1f;1:jmp 1f;1:");
 }
