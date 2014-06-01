c5e233daf4ee38b4d877aed3e2234485ac415722
George Klees <george.t.klees@gmail.com>
2014-06-01 16:24:26 -0400

    Minor cleanup

--- kernel/src/microkernel/i686/pic.c
+++ kernel/src/microkernel/i686/pic.c
@@ -1,4 +1,5 @@
 #include <types.h>
+#include <string.h>
 #include <microkernel/ports.h>
 #include <microkernel/i686/idt.h>
 #include <microkernel/i686/isr.h>
@@ -10,22 +11,22 @@
 #define PIC_SLAVE_DATA	0xA1
 
 /* ASM IRQ handlers */
-void irq0();
-void irq1();
-void irq2();
-void irq3();
-void irq4();
-void irq5();
-void irq6();
-void irq7();
-void irq8();
-void irq9();
-void irq10();
-void irq11();
-void irq12();
-void irq13();
-void irq14();
-void irq15();
+void pic_irq0();
+void pic_irq1();
+void pic_irq2();
+void pic_irq3();
+void pic_irq4();
+void pic_irq5();
+void pic_irq6();
+void pic_irq7();
+void pic_irq8();
+void pic_irq9();
+void pic_irq10();
+void pic_irq11();
+void pic_irq12();
+void pic_irq13();
+void pic_irq14();
+void pic_irq15();
 
 /* C IRQ handlers */
 static isr_t handlers[16];
@@ -80,23 +81,23 @@ void pic_init()
 	io_write_8(PIC_MASTER_DATA, master_mask);
 	io_write_8(PIC_SLAVE_DATA, slave_mask);
 
-	/* Add the irq handlers to the IDT */
-    idt_set_gate(32, (uint32_t) irq0, IDT_GATE_INT, true);
-    idt_set_gate(33, (uint32_t) irq1, IDT_GATE_INT, true);
-    idt_set_gate(34, (uint32_t) irq2, IDT_GATE_INT, true);
-    idt_set_gate(35, (uint32_t) irq3, IDT_GATE_INT, true);
-    idt_set_gate(36, (uint32_t) irq4, IDT_GATE_INT, true);
-    idt_set_gate(37, (uint32_t) irq5, IDT_GATE_INT, true);
-    idt_set_gate(38, (uint32_t) irq6, IDT_GATE_INT, true);
-    idt_set_gate(39, (uint32_t) irq7, IDT_GATE_INT, true);
-    idt_set_gate(40, (uint32_t) irq8, IDT_GATE_INT, true);
-    idt_set_gate(41, (uint32_t) irq9, IDT_GATE_INT, true);
-    idt_set_gate(42, (uint32_t) irq10, IDT_GATE_INT, true);
-    idt_set_gate(43, (uint32_t) irq11, IDT_GATE_INT, true);
-    idt_set_gate(44, (uint32_t) irq12, IDT_GATE_INT, true);
-    idt_set_gate(45, (uint32_t) irq13, IDT_GATE_INT, true);
-    idt_set_gate(46, (uint32_t) irq14, IDT_GATE_INT, true);
-    idt_set_gate(47, (uint32_t) irq15, IDT_GATE_INT, true);
+	/* Add the IRQ handlers to the IDT */
+    idt_set_gate(32, (uint32_t) pic_irq0, IDT_GATE_INT, true);
+    idt_set_gate(33, (uint32_t) pic_irq1, IDT_GATE_INT, true);
+    idt_set_gate(34, (uint32_t) pic_irq2, IDT_GATE_INT, true);
+    idt_set_gate(35, (uint32_t) pic_irq3, IDT_GATE_INT, true);
+    idt_set_gate(36, (uint32_t) pic_irq4, IDT_GATE_INT, true);
+    idt_set_gate(37, (uint32_t) pic_irq5, IDT_GATE_INT, true);
+    idt_set_gate(38, (uint32_t) pic_irq6, IDT_GATE_INT, true);
+    idt_set_gate(39, (uint32_t) pic_irq7, IDT_GATE_INT, true);
+    idt_set_gate(40, (uint32_t) pic_irq8, IDT_GATE_INT, true);
+    idt_set_gate(41, (uint32_t) pic_irq9, IDT_GATE_INT, true);
+    idt_set_gate(42, (uint32_t) pic_irq10, IDT_GATE_INT, true);
+    idt_set_gate(43, (uint32_t) pic_irq11, IDT_GATE_INT, true);
+    idt_set_gate(44, (uint32_t) pic_irq12, IDT_GATE_INT, true);
+    idt_set_gate(45, (uint32_t) pic_irq13, IDT_GATE_INT, true);
+    idt_set_gate(46, (uint32_t) pic_irq14, IDT_GATE_INT, true);
+    idt_set_gate(47, (uint32_t) pic_irq15, IDT_GATE_INT, true);
 
 	/* Install the C handlers */
 	memset(handlers, 0, sizeof(isr_t) * 16);
