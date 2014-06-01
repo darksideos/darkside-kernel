137de4ea84c649373ae1d88405debf0d67a95919
George Klees <george.t.klees@gmail.com>
2014-06-01 18:03:11 -0400

    Added LAPIC spurious IRQ code

---
+++ kernel/src/microkernel/i686/lapic_asm.s
@@ -0,0 +1,41 @@
+[BITS 32]
+
+; Local APIC spurious IRQ
+global lapic_irq_spurious
+lapic_irq_spurious:
+	; Push the interrupt number and error code
+	push byte 0
+	push byte 32
+
+	; Save all registers
+	pusha
+	push ds
+	push es
+	push fs
+	push gs
+	
+	; Load the kernel segment registers
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+	; Call the C spurious IRQ handler
+	mov eax, esp
+	push eax
+	extern lapic_spurious_handler
+	mov eax, lapic_spurious_handler
+	call eax
+	pop eax
+	
+	; Restore all registers
+	pop gs
+	pop fs
+	pop es
+	pop ds
+	popa
+	
+	; Return from the interrupt
+	add esp, 8
+	iret
