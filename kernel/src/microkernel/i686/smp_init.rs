67357d9d2e2d64f307b1e533e34c06afef5b0252
George Klees <george.t.klees@gmail.com>
2014-06-08 13:41:01 -0400

    Working on AP trampoline (won't build)

---
+++ kernel/src/microkernel/i686/smp_init.rs
@@ -0,0 +1,67 @@
+%include "src/microkernel/i686/smp_init.inc"
+
+[ORG ORG_LOC]
+[BITS 16]
+section .text
+
+; Start of the bootstrap code
+global ap_trampoline
+ap_trampoline:
+	jmp 0x0000:.real_to_pm
+.real_to_pm:
+	; Set up the null GDT descriptor
+	mov eax, 0
+	mov [GDT(eax, limit_low)], word 0
+	mov [GDT(eax, base_low)], word 0
+	mov [GDT(eax, base_middle)], byte 0
+	mov [GDT(eax, access)], byte 0
+	mov [GDT(eax, granularity)], byte 0
+	mov [GDT(eax, base_high)], byte 0
+	
+	; Set up the 32-bit code GDT descriptor
+	mov eax, 0x08
+	mov [GDT(eax, limit_low)], word 0xFFFF
+	mov [GDT(eax, base_low)], word 0
+	mov [GDT(eax, base_middle)], byte 0
+	mov [GDT(eax, access)], byte 0x9A
+	mov [GDT(eax, granularity)], byte 0xCF
+	mov [GDT(eax, base_high)], byte 0
+	
+	; Set up the 32-bit data GDT descriptor
+	mov eax, 0x10
+	mov [GDT(eax, limit_low)], word 0xFFFF
+	mov [GDT(eax, base_low)], word 0
+	mov [GDT(eax, base_middle)], byte 0
+	mov [GDT(eax, access)], byte 0x92
+	mov [GDT(eax, granularity)], byte 0xCF
+	mov [GDT(eax, base_high)], byte 0
+	
+	; Load the GDT pointer
+	mov [GDTR(limit)], word 0x17
+	mov [GDTR(base)], dword GDT_LOC
+	lgdt [GDTR_LOC]
+	
+	; Switch to protected mode
+	cli
+	mov eax, cr0
+	or al, 1
+	mov cr0, eax
+	
+	; Jump to our 32-bit protected mode entry point
+	jmp 0x08:.pm_entry
+.pm_entry:
+	; Enable paging
+	
+	; Jump to microkernel_init()
+	
+; Initial 32-bit protected mode GDT
+gdt times 0x18 db 0
+gdtr times 0x06 db 0
+
+; Initial page directory/page directory pointer table
+global pdir
+pdir       dq 0
+
+; Address of microkernel_init()
+global kinit_func
+kinit_func dd 0
