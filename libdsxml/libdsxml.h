a53fbab257b97c8cc0487ea8ac34b3324c9c03a6
Noah Singer <singerng@gmail.com>
2014-06-10 18:51:01 -0400

    Creating libdsxml with headers

---
+++ libdsxml/libdsxml.h
@@ -0,0 +1,18 @@
+#ifndef __LIBDSXML_H
+#define __LIBDSXML_H
+
+#include <types.h>
+
+#include <dict.h>
+#include <list.h>
+
+typedef struct xml_node
+{
+	char *name;
+	dict_t attribs;
+	list_t children;
+} xml_node_t;
+
+xml_node_t parse_xml(char *string, uint32_t length);
+
+#endif
