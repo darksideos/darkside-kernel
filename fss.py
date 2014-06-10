e4b8a86ced5cfb2b4b883be8157d6e6ac5381767
Noah Singer <singerng@gmail.com>
2014-06-10 18:20:25 -0400

    Added sorting and ignoring .git and .gitignore in fss.py

--- fss.py
+++ fss.py
@@ -26,26 +26,30 @@ counter = \
 def recurse(counter, path=os.getcwd()):
     for item in os.listdir(path):
         subpath = os.path.join(path, item)
-        counter["paths"].append(subpath)              
+        counter["paths"].append(subpath)
+        
         if os.path.isfile(subpath):
-            counter["files"] += 1
-            counter["types"][os.path.splitext(subpath)[1]] += 1
-            try:
-                file = open(subpath)
-                contents = file.read()
-                counter["readable"] += 1
-            except:
-                counter["unreadable"] += 1
-            else:
-                counter["lines"] += contents.count("\n")
-                counter["chars"] += len(contents)
-            finally:
-                file.close()
-            counter["size"] += os.path.getsize(subpath)
+            if not subpath.endswith(".gitignore"):
+                counter["files"] += 1
+
+                counter["types"][os.path.splitext(subpath)[1]] += 1
+                try:
+                    file = open(subpath)
+                    contents = file.read()
+                    counter["readable"] += 1
+                except:
+                    counter["unreadable"] += 1
+                else:
+                    counter["lines"] += contents.count("\n")
+                    counter["chars"] += len(contents)
+                finally:
+                    file.close()
+                counter["size"] += os.path.getsize(subpath)
         else:
-            counter["folders"] += 1
-            counter["size"] += os.path.getsize(subpath)
-            recurse(counter, subpath)
+            if not subpath.endswith(".git"):
+                counter["folders"] += 1
+                counter["size"] += os.path.getsize(subpath)
+                recurse(counter, subpath)
 
 if __name__ == "__main__":
     recurse(counter)
@@ -54,7 +58,7 @@ if __name__ == "__main__":
     print("Total".ljust(15, "."), counter["files"] + counter["folders"], sep="")
     print()
     print("Types")
-    for type, count in counter["types"].items():
+    for type, count in sorted(counter["types"].items(), key=lambda item: item[1], reverse=True):
         print(("  %s" % (type if type != "" else "none")).ljust(15, "."),
                count, sep="")
     print()
@@ -66,3 +70,5 @@ if __name__ == "__main__":
     print()
     print("Size".ljust(15, "."), round(counter["size"] / 1.049e+6, 2), "MB",
           sep="")
+
+input()
