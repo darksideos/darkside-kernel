9437516ae98c8e6425e17490d828d096c67ac34c
Marionumber1 <gksharkboy@gmail.com>
2014-06-14 12:27:05 -0400

    Added bitmap ADT

---
+++ libadt/include/bitmap.h
@@ -0,0 +1,26 @@
+#ifndef __BITMAP_H
+#define __BITMAP_H
+
+/* Bitmap structure */
+typedef struct bitmap
+{
+	uint8_t *data;
+	size_t max_bits;
+} bitmap_t;
+
+/* Initialize a bitmap */
+void bitmap_init(bitmap_t *bitmap, void *storage, size_t max_bits);
+
+/* Set a bit in a bitmap */
+void bitmap_set(bitmap_t *bitmap, size_t index);
+
+/* Clear a bit in a bitmap */
+void bitmap_clear(bitmap_t *bitmap, size_t index);
+
+/* Test a bit in a bitmap */
+bool bitmap_test(bitmap_t *bitmap, size_t index);
+
+/* Find the first clear bit in a bitmap */
+int64_t bitmap_first_clear(bitmap_t *bitmap);
+
+#endif
---
+++ libadt/src/bitmap.c
@@ -0,0 +1,57 @@
+#include <types.h>
+#include <string.h>
+#include <math.h>
+#include <bitmap.h>
+
+/* Initialize a bitmap */
+void bitmap_init(bitmap_t *bitmap, void *storage, size_t max_bits)
+{
+	bitmap->data = (uint8_t*) storage;
+	bitmap->max_bits = max_bits;
+
+	memset(bitmap->data, 0, ceil(max_bits, 8));
+}
+
+/* Set a bit in a bitmap */
+void bitmap_set(bitmap_t *bitmap, size_t index)
+{
+	bitmap->data[index / 8] |= (1 << (index % 8));
+}
+
+/* Clear a bit in a bitmap */
+void bitmap_clear(bitmap_t *bitmap, size_t index)
+{
+	bitmap->data[index / 8] &= ~(1 << (index % 8));
+}
+
+/* Test a bit in a bitmap */
+bool bitmap_test(bitmap_t *bitmap, size_t index)
+{
+	return (bool) bitmap->data[index / 8] & (1 << (index % 8));
+}
+
+/* Find the first clear bit in a bitmap */
+int64_t bitmap_first_clear(bitmap_t *bitmap)
+{
+	int64_t offset;
+	for (offset = 0; offset < bitmap->max_bits / 8; offset++)
+	{
+		uint8_t byte = bitmap->data[offset];
+
+		/* Find the first clear bit */
+		uint8_t bit = 0;
+		while ((byte & 1) == 1)
+		{
+			bit++;
+			byte >>= 1;
+		}
+
+		/* If we didn't go past the MSB, return the bit we found */
+		if (bit != 8)
+		{
+			return (offset * 8) + bit;
+		}
+	}
+
+	return -1;
+}
