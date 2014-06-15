c0a7385d110302b5b0648addbe333b6e988b4dd2
George Klees <george.t.klees@gmail.com>
2014-06-15 12:33:20 -0400

    Added slab.h

---
+++ kernel/include/mm/slab.h
@@ -0,0 +1,34 @@
+#ifndef __SLAB_H
+#define __SLAB_H
+
+#include <microkernel/lock.h>
+
+/* Slab cache header structure */
+typedef struct slab_cache
+{
+	/* Size of the entire slab */
+	size_t slab_size;
+
+	/* Object size and number of objects (total and free) */
+	size_t object_size;
+	atomic_t num_total_objs, num_free_objs;
+
+	/* Lock protecting the slab */
+	spinlock_t lock;
+
+	/* Pointer to the next slab */
+	struct slab_cache *next;
+
+	/* Bitmap of free objects */
+	uint8_t free_bitmap[];
+} slab_cache_t;
+
+/* Create and destroy a slab cache */
+slab_cache_t *slab_cache_create(size_t object_size);
+void slab_cache_destroy(slab_cache_t *slab_cache);
+
+/* Allocate and free slabs */
+void *slab_cache_alloc(slab_cache_t *slab_cache);
+void slab_cache_free(slab_cache_t *slab_cache, void *ptr);
+
+#endif
