440c155414a71a06952e516e5b3849300be7abf8
Marionumber1 <gksharkboy@gmail.com>
2014-06-16 14:25:29 -0400

    addrspace_alloc() partially done

--- kernel/include/mm/addrspace.h
+++ kernel/include/mm/addrspace.h
@@ -1,7 +1,12 @@
 #ifndef __ADDRSPACE_H
 #define __ADDRSPACE_H
 
+#include <microkernel/lock.h>
 #include <mm/vad.h>
+
+/* Special address space pointers */
+#define ADDRSPACE_CURRENT	1
+#define ADDRSPACE_SYSTEM	2
 
 /* Address space structure */
 typedef struct addrspace
@@ -11,14 +16,20 @@ typedef struct addrspace
 
 	/* Used and free regions */
 	vad_t used, free;
+
+	/* NUMA domain */
+	int numa_domain;
+
+	/* Address space lock */
+	spinlock_t lock;
 } addrspace_t;
 
 /* Create and destroy an address space */
-addrspace_t *addrspace_create(paddr_t address_space);
+addrspace_t *addrspace_create();
 void addrspace_destroy(addrspace_t *addrspace);
 
 /* Allocate and free regions of a virtual address space */
-void *addrspace_alloc(addrspace_t *addrspace, size_t size_reserved, size_t size_committed);
+void *addrspace_alloc(addrspace_t *addrspace, size_t size_reserved, size_t size_committed, int flags);
 void addrspace_free(addrspace_t *addrspace, void *ptr, size_t size);
 
 #endif
