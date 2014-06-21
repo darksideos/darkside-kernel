703fcbdcbea2768fe6f4bbbf6683395f380aceff
George Klees <george.t.klees@gmail.com>
2014-06-21 13:48:37 -0400

    Added thread_t

--- kernel/include/microkernel/thread.h
+++ kernel/include/microkernel/thread.h
@@ -1,10 +1,13 @@
 #ifndef __THREAD_H
 #define __THREAD_H
 
-/* Thread ID type */
-typedef unsigned tid_t;
+/* i386 */
+#if defined(__i386__)
+#include <microkernel/i686/thread.h>
+#endif
 
-/* Get the current thread ID */
+/* Get the current thread and thread ID */
+thread_t *thread_current();
 tid_t tid_current();
 
 #endif
