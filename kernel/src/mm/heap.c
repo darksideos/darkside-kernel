c05d9db66b35040838f208bae8edf0c50aa18eba
Marionumber1 <gksharkboy@gmail.com>
2014-06-21 20:23:35 -0400

    Added stub heap_init() function

--- kernel/src/mm/heap.c
+++ kernel/src/mm/heap.c
@@ -1,4 +1,5 @@
 #include <types.h>
+#include <mm/slab.h>
 
 /* Allocate memory from the kernel heap */
 void *malloc(size_t size)
@@ -18,4 +19,9 @@ void *realloc(void *ptr, size_t size)
 {
 	/* Not implemented */
 	return NULL;
+}
+
+/* Initialize the kernel heap */
+void heap_init()
+{
 }
