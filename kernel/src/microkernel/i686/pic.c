54973c234d2a30749c19e4507f7c196b66003272
George Klees <george.t.klees@gmail.com>
2014-06-23 14:45:50 -0400

    Expanded 8259 PIC code

--- kernel/src/microkernel/i686/pic.c
+++ kernel/src/microkernel/i686/pic.c
@@ -34,15 +34,100 @@ static isr_t handlers[16];
 /* Common handler for all PIC IRQs */
 void pic_irq_handler(struct regs *regs)
 {
+	/* Spurious master PIC IRQ */
+	if ((regs->int_no - 32) == 7)
+	{
+		/* Read the ISR */
+		io_write_8(PIC_MASTER_CMD, 0x0B);
+		io_wait();
+		uint8_t isr = io_read_8(PIC_MASTER_CMD);
+
+		/* If a spurious interrupt, return */
+		if (!(isr & (1 << 7)))
+		{
+			return;
+		}
+	}
+	/* Spurious slave PIC IRQ */
+	else if ((regs->int_no - 32) == 15)
+	{
+		/* Read the ISR */
+		io_write_8(PIC_SLAVE_CMD, 0x0B);
+		io_wait();
+		uint8_t isr = io_read_8(PIC_SLAVE_CMD);
+
+		/* If a spurious interrupt, send EOI to the master PIC and return */
+		if (!(isr & (1 << 7)))
+		{
+			io_write_8(PIC_MASTER_CMD, 0x20);
+			return;
+		}
+	}	
+
+	/* IRQ is valid and has a handler */
 	if (handlers[regs->int_no - 32])
 	{
+		/* Dispatch the IRQ */
 		isr_t handler = handlers[regs->int_no - 32];
 		handler(regs);
+
+		/* Send the EOI */
+		if ((regs->int_no - 32) >= 8)
+		{
+			io_write_8(PIC_SLAVE_CMD, 0x20);
+		}
+		io_write_8(PIC_MASTER_CMD, 0x20);
 	}
+	/* Valid IRQ somehow occured with no handler */
 	else
 	{
-		printf("Unhandled IRQ 0x%08X\n", regs->int_no - 32);
-		while(1);
+		panic("Unhandled IRQ 0x%08X\n", regs->int_no - 32);
+	}
+}
+
+/* Register a PIC IRQ handler */
+void pic_irq_register_handler(int num, isr_t handler)
+{
+	/* Master PIC IRQ */
+	if (num >= 0 && num < 8)
+	{
+		handlers[num] = handler;
+
+		uint8_t master_mask = io_read_8(PIC_MASTER_DATA);
+		master_mask &= ~(1 << num);
+		io_write_8(PIC_MASTER_DATA, master_mask);
+	}
+	/* Slave PIC IRQ */
+	else if (num >= 8 && num < 16)
+	{
+		handlers[num] = handler;
+
+		uint8_t slave_mask = io_read_8(PIC_SLAVE_DATA);
+		slave_mask &= ~(1 << (num - 8));
+		io_write_8(PIC_SLAVE_DATA, slave_mask);
+	}
+}
+
+/* Unregister a PIC IRQ handler */
+void pic_irq_unregister_handler(int num)
+{
+	/* Master PIC IRQ */
+	if (num >= 0 && num < 8)
+	{
+		uint8_t master_mask = io_read_8(PIC_MASTER_DATA);
+		master_mask |= (1 << num);
+		io_write_8(PIC_MASTER_DATA, master_mask);
+
+		handlers[num] = NULL;
+	}
+	/* Slave PIC IRQ */
+	else if (num >= 8 && num < 16)
+	{
+		uint8_t slave_mask = io_read_8(PIC_SLAVE_DATA);
+		slave_mask |= (1 << (num - 8));
+		io_write_8(PIC_SLAVE_DATA, slave_mask);
+
+		handlers[num] = NULL;
 	}
 }
 
@@ -98,7 +183,4 @@ void pic_init()
     idt_set_gate(45, (uint32_t) pic_irq13, IDT_GATE_INT, true);
     idt_set_gate(46, (uint32_t) pic_irq14, IDT_GATE_INT, true);
     idt_set_gate(47, (uint32_t) pic_irq15, IDT_GATE_INT, true);
-
-	/* Install the C handlers */
-	memset(handlers, 0, sizeof(isr_t) * 16);
 }
