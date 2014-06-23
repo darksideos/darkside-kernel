7d5842bfed80971dac55799f6569d7540dd8b7f3
Marionumber1 <gksharkboy@gmail.com>
2014-06-22 21:41:59 -0400

    Added some more page flags for VADs

--- kernel/include/mm/vad.h
+++ kernel/include/mm/vad.h
@@ -1,6 +1,11 @@
 #ifndef __VAD_H
 #define __VAD_H
 
+/* Virtual address region flags */
+#define PAGE_PRIVATE			0x100
+#define PAGE_MAPPED_DATA		0x200
+#define PAGE_MAPPED_IMAGE		0x400
+
 /* Virtual address descriptor structure */
 typedef struct vad
 {
