fb63493e8885bc411b6d89151832bb4b4060f577
George Klees <george.t.klees@gmail.com>
2014-06-24 17:51:32 -0400

    Beginning heap code

--- kernel/include/mm/heap.h
+++ kernel/include/mm/heap.h
@@ -1,6 +1,16 @@
 #ifndef __HEAP_H
 #define __HEAP_H
 
+/* Heap size */
+#define HEAP_SIZE	0x4000
+
+/* Heap header structure */
+typedef struct heap_header
+{
+	/* Left and right children */
+	struct heap_header *left, *right;
+} heap_header_t;
+
 /* Initialize the kernel heap */
 void heap_init();
 
