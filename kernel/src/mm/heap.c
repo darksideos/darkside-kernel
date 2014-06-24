2b53b87d1658e1c4adfe05a99a4981adeb5a9a54
George Klees <george.t.klees@gmail.com>
2014-06-24 17:51:32 -0400

    Beginning heap code

--- kernel/src/mm/heap.c
+++ kernel/src/mm/heap.c
@@ -1,5 +1,9 @@
 #include <types.h>
-#include <mm/slab.h>
+#include <mm/addrspace.h>
+#include <mm/heap.h>
+
+/* Root of the kernel heap */
+static heap_header_t *heap_root;
 
 /* Allocate memory from the kernel heap */
 void *malloc(size_t size)
@@ -24,4 +28,5 @@ void *realloc(void *ptr, size_t size)
 /* Initialize the kernel heap */
 void heap_init()
 {
+	heap_root = (heap_header_t*) addrspace_alloc(ADDRSPACE_SYSTEM, HEAP_SIZE, HEAP_SIZE, PAGE_READ | PAGE_WRITE);
 }
