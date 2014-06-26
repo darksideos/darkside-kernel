cfbd3b40af210c002f7d6b439c9f511e1b121a5a
George Klees <george.t.klees@gmail.com>
2014-06-26 12:33:10 -0400

    Adding TLB shootdown fields to process_t

--- kernel/include/microkernel/i686/process.h
+++ kernel/include/microkernel/i686/process.h
@@ -1,6 +1,7 @@
 #ifndef __I686_PROCESS_H
 #define __I686_PROCESS_H
 
+#include <microkernel/lock.h>
 #include <mm/addrspace.h>
 
 /* Process ID type */
@@ -9,11 +10,20 @@ typedef unsigned pid_t;
 /* Process structure */
 typedef struct process
 {
+	/* Process ID */
+	pid_t pid;
+
+	/* Local APIC logical destination */
+	uint32_t logical_destination;
+
 	/* Address space */
 	addrspace_t addrspace;
 
-	/* Process ID */
-	pid_t pid;
+	/* TLB shootdown information */
+	bool tlb_shootdown;
+	vaddr_t tlb_shootdown_start;
+	int tlb_shootdown_num_pages;
+	spinlock_t tlb_shootdown_lock;
 
 	/* Scheduling policy and priority */
 	int policy, priority;
