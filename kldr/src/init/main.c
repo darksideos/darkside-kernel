53a285a174e36662120065303055434c6d354cef
George Klees <george.t.klees@gmail.com>
2014-06-29 11:27:24 -0400

    Adding VESA demo

--- kldr/src/init/main.c
+++ kldr/src/init/main.c
@@ -26,7 +26,20 @@ void ba_main(loader_block_t *loader_block)
 
 	/* Initialize graphics
 	framebuffer_t *fb = graphics_init(0, 0, 0);
-	loader_block->fb = fb; */
+	loader_block->fb = fb;
+
+	paddr_t base = fb->buffer_phys;
+	vaddr_t length = (fb->width * fb->height * fb->bpp) + ((fb->height - 1) * fb->pitch);
+
+	for (vaddr_t i = 0; i < length; i += 0x1000)
+	{
+		map_page(0x80000000 + i, base + i, PAGE_READ | PAGE_WRITE | PAGE_NOCACHE);
+	}
+
+	fb->buffer = (void*) 0x80000000;
+
+	draw_bmp_32(fb, "/boot/boot screen.bmp", 0, 0);
+	drawing_demo(fb); */
 
 	/* Create a list of modules */
 	list_t modules = list_create();
