4126148ee414c84cde60d52182032a7b4f871183
George Klees <george.t.klees@gmail.com>
2014-06-30 16:38:34 -0400

    Removing module registry code

--- kldr/include/modules/text_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TEXT_UTILS_H
-#define __TEXT_UTILS_H
-
-#include <types.h>
-
-uint32_t hash(char *key);
-uint32_t separate_indents(char **line);
-
-#endif
--- kldr/include/modules/tree_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TREE_UTILS_H
-#define __TREE_UTILS_H
-
-#include <types.h>
-
-uint32_t tree_index(char *line, uint32_t lineNumber);
-
-#endif
