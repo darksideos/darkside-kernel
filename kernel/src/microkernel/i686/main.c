f947895999e326d0b38f1ae695558c0a3e2d169f
Marionumber1 <gksharkboy@gmail.com>
2014-07-03 13:00:45 -0400

    loader.s now calls executive_init(), which calls microkernel_init()

--- kernel/src/microkernel/i686/main.c
+++ kernel/src/microkernel/i686/main.c
@@ -1,6 +1,5 @@
 #include <types.h>
 #include <string.h>
-#include <bootvid.h>
 #include <init/loader.h>
 #include <microkernel/cpu.h>
 #include <microkernel/i686/gdt.h>
@@ -20,26 +19,19 @@ extern uint64_t pdir;
 extern uint32_t kinit_stack, kinit_func;
 
 /* Initialize the core microkernel */
-void microkernel_init(loader_block_t *_loader_block, bool bsp)
+void microkernel_init(loader_block_t *loader_block, bool bsp)
 {
 	/* Running on the BSP */
 	if (bsp)
 	{
-		/* Initialize the boot video driver */
-		bootvid_init(COLOR_WHITE, COLOR_BLACK);
-
-		/* Copy the loader block onto the initial kernel stack */
-		loader_block_t loader_block;
-		memcpy(&loader_block, _loader_block, sizeof(loader_block_t));
-
 		/* If present, initialize the Local APIC */
-		if (loader_block.lapic)
+		if (loader_block->lapic)
 		{
-			lapic_init(&loader_block, bsp);
+			lapic_init(loader_block, bsp);
 		}
 
 		/* Initialize the per-CPU and NUMA domain data areas */
-		cpu_data_area_init(&loader_block);
+		cpu_data_area_init(loader_block);
 
 		/* Initialize the processor's GDT and IDT */
 		gdt_init();
@@ -49,10 +41,10 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 		exceptions_install();
 
 		/* Use the physical memory map to create the PFN database */
-		pfn_database_init(&loader_block);
+		pfn_database_init(loader_block);
 
 		/* Start up each secondary CPU */
-		if (loader_block.num_cpus > 1)
+		if (loader_block->num_cpus > 1)
 		{
 			/* Get the BSP's Local APIC ID */
 			uint32_t bsp_lapic_id = lapic_current_id();
@@ -63,7 +55,7 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 			memcpy((void*)0x7000, &ap_trampoline, &ap_trampoline_end - &ap_trampoline);
 
 			/* For each detected CPU */
-			for (int i = 0; i < loader_block.num_cpus; i++)
+			for (int i = 0; i < loader_block->num_cpus; i++)
 			{
 				/* Get the per-CPU data area */
 				cpu_t *cpu = cpu_data_area(i);
@@ -87,13 +79,6 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 				lapic_send_ipi(cpu->lapic_id, 0, IPI_DELIVER_INIT, false);
 				//udelay(10000);
 
-				/* DELAY */
-				//int num = 0;
-				//for (int i = 0; i < 2000000000; i++)
-				//{
-					//num++;
-				//}
-
 				/* Send a STARTUP IPI and wait for the AP to start */
 				lapic_send_ipi(cpu->lapic_id, 0x7, IPI_DELIVER_SIPI, false);
 				while (!(cpu->flags & CPU_MM_INIT));
@@ -102,37 +87,29 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 
 		/* Initialize the free list manager */
 		printf("Initializing free lists\n");
-		freelist_init(&loader_block, bsp);
+		freelist_init(loader_block, bsp);
 
 		/* Initialize paging, mapping our kernel and modules */
 		printf("Initializing paging\n");
-		paging_init(&loader_block, bsp);
+		paging_init(loader_block, bsp);
 
 		/* Initialize the system address space */
 		paddr_t address_space;
 		__asm__ volatile("mov %%cr3, %0" : "=r"(address_space));
 		printf("Initializing system address space\n");
-		addrspace_init(ADDRSPACE_SYSTEM, address_space, loader_block.system_free_start, 0xFFC00000 - loader_block.system_free_start);
+		addrspace_init(ADDRSPACE_SYSTEM, address_space, loader_block->system_free_start, 0xFFC00000 - loader_block->system_free_start);
 
 		/* Initialize the kernel heap */
 		printf("Initializing kernel heap\n");
 		heap_init();
 
-		printf("\nRSDP address: 0x%08X\n", loader_block.rsdp);
-		printf("RSDT address: 0x%08X\n", loader_block.rsdt);
-		printf("XSDT address: 0x%08X\n", (uint32_t) loader_block.xsdt);
+		printf("\nRSDP address: 0x%08X\n", loader_block->rsdp);
+		printf("RSDT address: 0x%08X\n", loader_block->rsdt);
+		printf("XSDT address: 0x%08X\n", (uint32_t) loader_block->xsdt);
 
 		/* Signal memory manager initialization to the APs */
 		cpu_t *cpu = cpu_data_area(CPU_CURRENT);
 		cpu->flags |= CPU_MM_INIT;
-
-		/* Initialize the kernel firmware interface */
-
-		/* Detect the interrupt controller and initialize it */
-	
-		/* Detect the system timer and initialize it */
-
-		/* Initialize the RTC */
 
 		/* Detect the relevant CPU topology information for itself */
 
@@ -162,15 +139,10 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 		/* Use the paging structures set up by the BSP */
 		paging_init(NULL, bsp);
 
-		/* Initialize the Local APIC timer */
-
 		/* Detect the relevant CPU topology information for itself */
 
 		/* Wait for the BSP to initialize the scheduler */
 
 		/* Go to the scheduler ready function and wait for threads */
 	}
-
-	/* Should never reach here */
-	while(1);
 }
