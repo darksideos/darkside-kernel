61f0f99381e972223368f33143497d4aedbeed27
George Klees <george.t.klees@gmail.com>
2014-07-04 20:15:16 -0400

    Cleaning up list.c and queue.h

--- libadt/include/queue.h
+++ libadt/include/queue.h
@@ -10,7 +10,7 @@
 #define queue_create() list_create()
 #define queue_destroy(queue) list_destroy(queue)
 #define queue_enqueue(queue, value) list_insert_tail(queue, value)
-#define queue_dequeue(queue) list_remove_tail(queue)
+#define queue_dequeue(queue) list_remove_head(queue)
 #define queue_length(queue) list_length(queue)
 
 #endif
