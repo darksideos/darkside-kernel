31a5dcdb624bf0cc5d6d0200d3cb4f9841d7185c
George Klees <george.t.klees@gmail.com>
2014-07-07 14:35:22 -0400

    Added interrupt ASM stub handling

--- kernel/src/microkernel/i686/interrupt.c
+++ kernel/src/microkernel/i686/interrupt.c
@@ -2,20 +2,26 @@
 #include <microkernel/lock.h>
 #include <microkernel/interrupt.h>
 #include <microkernel/i686/idt.h>
+#include <microkernel/i686/isr.h>
 #include <mm/slab.h>
 
 /* Registered interrupt object list */
 static interrupt_t *interrupts[224];
 static spinlock_t interrupts_lock;
 
-/* ASM interrupt code template */
-void asm_interrupt_code_template();
+/* ASM interrupt stub template */
+void interrupt_common_stub();
 
 /* Interrupt object slab cache */
 static slab_cache_t *interrupt_cache;
 
 /* ASM interrupt stub slab cache */
 static slab_cache_t *asm_interrupt_stub_cache;
+
+/* Common handler for all interrupts */
+void interrupt_handler(struct regs *regs)
+{
+}
 
 /* Create an interrupt object */
 interrupt_t *interrupt_create()
@@ -62,10 +68,14 @@ void interrupt_register_handler(interrupt_t *interrupt, interrupt_handler_t hand
 	if (!chained)
 	{
 		/* Allocate an ASM interrupt stub and copy in the template */
+		uint8_t *asm_interrupt_stub = slab_cache_alloc(asm_interrupt_stub_cache);
+		memcpy(asm_interrupt_stub, interrupt_common_stub, 0x30);
 
 		/* Modify the stub to contain the proper IDT vector number */
+		asm_interrupt_stub[3] = interrupt->vector;
 
 		/* Install an IDT entry for the interrupt */
+		idt_set_gate(interrupt->vector, (uint32_t) asm_interrupt_stub, IDT_GATE_INT, true);
 	}
 
 	/* Register the handler with the interrupt controller */
@@ -82,5 +92,5 @@ void interrupts_init()
 
 	/* Create the interrupt object and ASM interrupt code slab caches */
 	interrupt_cache = slab_cache_create(sizeof(interrupt_t), PAGE_READ | PAGE_WRITE);
-	asm_interrupt_stub_cache = slab_cache_create(32, PAGE_READ | PAGE_WRITE | PAGE_EXECUTE);
+	asm_interrupt_stub_cache = slab_cache_create(0x30, PAGE_READ | PAGE_WRITE | PAGE_EXECUTE);
 }
