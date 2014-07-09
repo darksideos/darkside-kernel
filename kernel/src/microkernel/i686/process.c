758152ed62dadc1a0450354f921c0c079ef117ab
Marionumber1 <gksharkboy@gmail.com>
2014-07-09 15:05:13 -0400

    Implemented process_current() and pid_current()

--- kernel/src/microkernel/i686/process.c
+++ kernel/src/microkernel/i686/process.c
@@ -6,21 +6,58 @@
 /* Current process ID to assign */
 static atomic_t current_pid = 1;
 
+/* Initialize a process */
 void process_init(process_t *process, int policy, int priority, int ideal_numa_domain, uint8_t *cpu_affinity)
 {
-	/* Atomically read and increment the current PID */
+	/* Choose a process ID for the process */
 	process->pid = (pid_t) atomic_xadd(&current_pid, 1);
 	
-	/* Initialize our process's addrspace */
+	/* Initialize the process's address space */
 	addrspace_init(&process->addrspace, vmm_create_address_space(), USER_ADDRSPACE_START, KERNEL_ADDRSPACE_START);
 }
 
+/* Get the current process */
 process_t *process_current()
 {
-	return thread_current()->process;
+	/* Get the current thread */
+	thread_t *thread = thread_current();
+
+	/* No thread running */
+	if (!thread)
+	{
+		return NULL;
+	}
+	/* Kernel thread */
+	else if (!thread->process)
+	{
+		return NULL;
+	}
+	/* User thread */
+	else
+	{
+		return thread->process;
+	}
 }
 
+/* Get the current process ID */
 pid_t pid_current()
 {
-	return process_current()->pid;
+	/* Get the current thread */
+	thread_t *thread = thread_current();
+
+	/* No thread running */
+	if (!thread)
+	{
+		return 0;
+	}
+	/* Kernel thread */
+	else if (!thread->process)
+	{
+		return 0;
+	}
+	/* User thread */
+	else
+	{
+		return thread->process->pid;
+	}
 }
