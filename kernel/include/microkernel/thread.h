06bfdcf26a3af22e0b4b48d580ea816f58575b39
Noah Singer <singerng@gmail.com>
2014-07-09 13:11:36 -0400

    Wrote thread_Init() and started filling in thread.c

--- kernel/include/microkernel/thread.h
+++ kernel/include/microkernel/thread.h
@@ -6,6 +6,14 @@
 #include <microkernel/i686/thread.h>
 #endif
 
+#include <init/loader.h>
+
+/* Initialize threading */
+void threading_init(loader_block_t *loader_block);
+
+/* Initialize a thread */
+void thread_init(thread_t *thread, struct process *parent_process, void (*main_fn)(void *args), void *args, uint32_t stack_size);
+
 /* Get the current thread and thread ID */
 thread_t *thread_current();
 tid_t tid_current();
