e99f274ec8c5fa033f807cc778b2c5f116a7b1df
George Klees <george.t.klees@gmail.com>
2014-07-09 18:29:02 -0400

    scheduler_init() done

--- kernel/src/microkernel/i686/scheduler.c
+++ kernel/src/microkernel/i686/scheduler.c
@@ -1,4 +1,5 @@
 #include <types.h>
+#include <queue.h>
 #include <init/loader.h>
 #include <microkernel/cpu.h>
 #include <microkernel/thread.h>
@@ -13,4 +14,17 @@ void scheduler_run(struct regs *context)
 /* Initialize the scheduler */
 void scheduler_init(loader_block_t *loader_block)
 {
+	/* Create each CPU's scheduling queues */
+	for (int i = 0; i < loader_block->num_cpus; i++)
+	{
+		/* Get the CPU */
+		cpu_t *cpu = cpu_data_area(i);
+
+		/* If the CPU was started */
+		if (cpu->flags)
+		{
+			/* TODO: Create queues for each policy and priority */
+			cpu->runqueue = queue_create();
+		}
+	}
 }
