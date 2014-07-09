2cf09b0cb06d56de12e3b1e6b256f27f4abc0e49
Marionumber1 <gksharkboy@gmail.com>
2014-07-09 17:36:46 -0400

    Reverting all affinity stuff

--- kernel/include/microkernel/thread.h
+++ kernel/include/microkernel/thread.h
@@ -9,7 +9,7 @@
 #endif
 
 /* Initialize a thread */
-void thread_init(thread_t *thread, struct process *parent_process, void (*fn)(void *args), void *args, uint32_t stack_size, int ideal_numa_domain, uint8_t *cpu_affinity);
+void thread_init(thread_t *thread, struct process *parent_process, void (*fn)(void *args), void *args, uint32_t stack_size);
 
 /* Run a thread */
 void thread_run(thread_t *thread);
