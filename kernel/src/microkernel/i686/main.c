7c645c0973cb1d8591fc2e558ed73a0507fbe54f
George Klees <george.t.klees@gmail.com>
2014-07-11 17:57:39 -0400

    One thread doesn't work (again)

--- kernel/src/microkernel/i686/main.c
+++ kernel/src/microkernel/i686/main.c
@@ -148,7 +148,7 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 		/* Thread test */
 		thread_t thread1, thread2;
 		thread_init(&thread1, NULL, &test, (void*) 1, 0);
-		thread_init(&thread2, NULL, &test, (void*) 2, 0);
+		//thread_init(&thread2, NULL, &test, (void*) 2, 0);
 		scheduler_run();
 
 		/* Start the executive services */
