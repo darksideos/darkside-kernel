1ee4875030cac420d7cfa618ae3f4e43f17b9637
George Klees <george.t.klees@gmail.com>
2014-07-14 10:32:07 -0400

    Scheduler builds again

--- kernel/include/microkernel/i686/scheduler.h
+++ kernel/include/microkernel/i686/scheduler.h
@@ -9,6 +9,10 @@
 #define POLICY_HIGH			1
 #define POLICY_LOW			2
 #define POLICY_BACKGROUND	3
+#define NUM_POLICIES		4
+
+/* Thread priorities */
+#define NUM_PRIORITIES		32
 
 /* Enqueue a thread onto a scheduling queue */
 void scheduler_enqueue(thread_t *thread);
