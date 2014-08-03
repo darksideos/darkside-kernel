9218d59ca0be7dfc83da1c22dc503feefaa6b955
George Klees <george.t.klees@gmail.com>
2014-08-03 12:36:11 -0400

    Adding license info and file info to the top of all kernel source

--- kernel/src/microkernel/i686/lock.c
+++ kernel/src/microkernel/i686/lock.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014 George Klees <gksharkboy@gmail.com>
+ * lock.c - Ticket spinlocks for the x86 architecture
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public Licens
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #include <types.h>
 #include <microkernel/atomic.h>
 #include <microkernel/lock.h>
