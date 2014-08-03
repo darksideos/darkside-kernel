d87427e20343668cb9172ccb779bddf624e31d7b
George Klees <george.t.klees@gmail.com>
2014-08-03 13:08:53 -0400

    Added it to the top of library sources

--- libadt/src/tree.c
+++ libadt/src/tree.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014 Noah Singer <singerng@gmail.com>
+ * tree.c - Tree abstract data type
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public Licens
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #include <types.h>
 #include <string.h>
 #include <stdlib.h>
