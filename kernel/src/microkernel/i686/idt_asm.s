037815c7a89b51ae24cd3f3e041a7f415f71e6be
George Klees <george.t.klees@gmail.com>
2014-08-16 17:26:22 -0400

    Finished adding licensing info to ASM files

--- kernel/src/microkernel/i686/idt_asm.s
+++ kernel/src/microkernel/i686/idt_asm.s
@@ -1,3 +1,20 @@
+; Copyright (C) 2014 DarkSide Project
+; Authored by George Klees <gksharkboy@gmail.com>
+; idt_asm.s - IDT loading for the i686 architecture
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License version 3 as
+; published by the Free Software Foundation.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public Licens
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 [BITS 32]
 
 ; Load our new IDT
