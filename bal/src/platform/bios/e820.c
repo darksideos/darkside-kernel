d4494da40ee2d46ecd52ffbe246d2b1de6d76fa3
George Klees <george.t.klees@gmail.com>
2014-08-16 16:28:26 -0400

    Converted copyrights for all BAL source files

--- bal/src/platform/bios/e820.c
+++ bal/src/platform/bios/e820.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2014 George Klees <gksharkboy@gmail.com>
+ * Copyright (C) 2014 DarkSide Project
+ * Authored by George Klees <gksharkboy@gmail.com>
  * e820.c - E820 memory map sanitization for BIOS-based PCs
  *
  * This program is free software; you can redistribute it and/or modify
--- bal/src/platform/bios/storage.c
+++ bal/src/platform/bios/storage.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2014 George Klees <gksharkboy@gmail.com>
+ * Copyright (C) 2014 DarkSide Project
+ * Authored by George Klees <gksharkboy@gmail.com>
  * storage.c - Generic storage interface for BIOS-based PCs
  *
  * This program is free software; you can redistribute it and/or modify
