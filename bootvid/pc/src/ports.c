15b01c1facc27141b7769cf9d4ce8dc9f708fa48
George Klees <george.t.klees@gmail.com>
2014-08-18 16:06:31 -0400

    All libraries build under AMD64 now

--- bootvid/pc/src/ports.c
+++ bootvid/pc/src/ports.c
@@ -18,38 +18,44 @@
  */
 #include <types.h>
 
-uint8_t inportb(uint16_t port)
+/* Read a byte from an I/O port */
+uint8_t io_read_8(uint32_t port)
 {
 	uint8_t rv;
-	__asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (port));
+	__asm__ volatile ("inb %1, %0" : "=a" (rv) : "dN" ((uint16_t)port));
 	return rv;
 }
 
-void outportb(uint16_t port, uint8_t data)
+/* Write a byte to an I/O port */
+void io_write_8(uint32_t port, uint8_t data)
 {
-	__asm__ __volatile__ ("outb %1, %0" : : "dN" (port), "a" (data));
+	__asm__ volatile ("outb %1, %0" : : "dN" ((uint16_t)port), "a" (data));
 }
 
-uint16_t inportw(uint16_t port)
+/* Read a word from an I/O port */
+uint16_t io_read_16(uint32_t port)
 {
 	uint16_t rv;
-	__asm__ __volatile__ ("inw %1, %0" : "=a" (rv) : "dN" (port));
+	__asm__ volatile ("inw %1, %0" : "=a" (rv) : "dN" ((uint16_t)port));
 	return rv;
 }
 
-void outportw(uint16_t port, uint16_t data)
+/* Write a word to an I/O port */
+void io_write_16(uint32_t port, uint16_t data)
 {
-	__asm__ __volatile__ ("outw %1, %0" : : "dN" (port), "a" (data));
+	__asm__ volatile ("outw %1, %0" : : "dN" ((uint16_t)port), "a" (data));
 }
 
-uint32_t inportl(uint16_t port)
+/* Read a dword from an I/O port */
+uint32_t io_read_32(uint32_t port)
 {
-	unsigned long rv;
-	__asm__ __volatile__ ("inl %1, %0" : "=a" (rv) : "dN" (port));
+	uint32_t rv;
+	__asm__ volatile ("inl %1, %0" : "=a" (rv) : "dN" ((uint16_t)port));
 	return rv;
 }
 
-void outportl(uint16_t port, uint32_t data)
+/* Write a dword to an I/O port */
+void io_write_32(uint32_t port, uint32_t data)
 {
-	__asm__ __volatile__ ("outl %1, %0" : : "dN" (port), "a" (data));
+	__asm__ volatile ("outl %1, %0" : : "dN" ((uint16_t)port), "a" (data));
 }
