2641aef5540492d4b7848ba1768183c96097cd7f
George Klees <gksharkboy@gmail.com>
2014-08-21 18:46:49 -0400

    Fixed text printing

--- kernel/src/microkernel/i686/main.c
+++ kernel/src/microkernel/i686/main.c
@@ -127,8 +127,9 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 		printf("Initializing free lists\n");
 		freelist_init(&loader_block, bsp);
 
-		/* Get the PS/2 keyboard driver */
+		/* Get the PS/2 keyboard driver's entry point */
 		executable_t *ps2kbd_driver = (executable_t*) list_remove_tail(loader_block.modules);
+		vaddr_t ps2kbd_module_init = ps2kbd_driver->entry_point;
 
 		/* Copy the framebuffer structure */
 		framebuffer_t fb;
@@ -171,7 +172,7 @@ void microkernel_init(loader_block_t *_loader_block, bool bsp)
 		while(num_scheduler_inits_left);
 		cpu->flags |= CPU_SCHEDULER_INIT;
 
-		demo(&fb, ps2kbd_driver->entry_point);
+		demo(&fb, ps2kbd_module_init);
 		while(1);
 
 		/* Start the executive services */
