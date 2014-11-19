8e43cec358e764136cc81146d42ffc7c944cdcb2
George Klees <george.t.klees@gmail.com>
2014-11-19 18:39:17 -0500

    Starting to define some privileges

---
+++ kernel/include/security/privilege.h
@@ -0,0 +1,15 @@
+#ifndef __PRIVILEGE_H
+#define __PRIVILEGE_H
+
+/* OS-defined privileges */
+#define PRIV_INSTALL_MODULES	0
+#define PRIV_SET_PRIORITY		1
+#define PRIV_SET_QUOTA			2
+#define PRIV_DEBUG				3
+#define PRIV_LOCK_PAGES			4
+#define PRIV_SHUTDOWN			5
+#define PRIV_SET_TIME			6
+#define PRIV_BACKUP_FILES		7
+#define PRIV_TAKE_OWNERSHIP		8
+
+#endif
