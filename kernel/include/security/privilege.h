f05f112b4675b462b9571150747e202eac905c84
George Klees <george.t.klees@gmail.com>
2014-11-19 18:43:37 -0500

    Add a privilege for marking executable memory

--- kernel/include/security/privilege.h
+++ kernel/include/security/privilege.h
@@ -6,10 +6,11 @@
 #define PRIV_SET_PRIORITY		1
 #define PRIV_SET_QUOTA			2
 #define PRIV_DEBUG				3
-#define PRIV_LOCK_PAGES			4
-#define PRIV_SHUTDOWN			5
-#define PRIV_SET_TIME			6
-#define PRIV_BACKUP_FILES		7
-#define PRIV_TAKE_OWNERSHIP		8
+#define PRIV_EXEC_MEMORY		4
+#define PRIV_LOCK_PAGES			5
+#define PRIV_SHUTDOWN			6
+#define PRIV_SET_TIME			7
+#define PRIV_BACKUP_FILES		8
+#define PRIV_TAKE_OWNERSHIP		9
 
 #endif
