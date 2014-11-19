7a5ba7f2d3c7c301f66d8163dd3ac262cda40cd9
Marionumber1 <gksharkboy@gmail.com>
2014-11-19 18:48:57 -0500

    Also adding interactive and network logon privileges

--- kernel/include/security/privilege.h
+++ kernel/include/security/privilege.h
@@ -2,15 +2,17 @@
 #define __PRIVILEGE_H
 
 /* OS-defined privileges */
-#define PRIV_INSTALL_MODULES	0
-#define PRIV_SET_PRIORITY		1
-#define PRIV_SET_QUOTA			2
-#define PRIV_DEBUG				3
-#define PRIV_EXEC_MEMORY		4
-#define PRIV_LOCK_PAGES			5
-#define PRIV_SHUTDOWN			6
-#define PRIV_SET_TIME			7
-#define PRIV_BACKUP_FILES		8
-#define PRIV_TAKE_OWNERSHIP		9
+#define PRIV_INTER_LOGON		0
+#define PRIV_NET_LOGON			1
+#define PRIV_INSTALL_MODULES	2
+#define PRIV_SET_PRIORITY		3
+#define PRIV_SET_QUOTA			4
+#define PRIV_DEBUG				5
+#define PRIV_EXEC_MEMORY		6
+#define PRIV_LOCK_PAGES			7
+#define PRIV_SHUTDOWN			8
+#define PRIV_SET_TIME			9
+#define PRIV_BACKUP_FILES		10
+#define PRIV_TAKE_OWNERSHIP		11
 
 #endif
