fbf3c9e0ce3e3b1611d90cf16af75aca74868322
Marionumber1 <gksharkboy@gmail.com>
2014-11-19 23:00:13 -0500

    Bit of refactoring (different ACLs for allowed and denied)

---
+++ kernel/src/security/access.c
@@ -0,0 +1,14 @@
+#include <types.h>
+#include <security/uid.h>
+#include <security/token.h>
+#include <security/acl.h>
+#include <security/sd.h>
+
+/* Check an access token against a security descriptor */
+bool sd_check_access(security_descriptor_t *descriptor, token_t *token, access_mask_t desired_access)
+{
+	/* No access granted initially */
+	bool access_granted = false;
+
+	/* 
+}
