6e50148bb06c7aca8815d91e9d31c501632c27ef
George Klees <george.t.klees@gmail.com>
2014-11-19 23:12:43 -0500

    Expanding sd_check_access() somewhat

--- kernel/src/security/access.c
+++ kernel/src/security/access.c
@@ -8,11 +8,20 @@
 /* Check an access token against a security descriptor */
 bool sd_check_access(security_descriptor_t *descriptor, token_t *token, access_mask_t desired_access)
 {
-	/* No access granted initially */
-	bool access_granted = false;
-
 	/* Current level of access granted */
 	uint64_t granted_access = 0;
 
-	/* First check the denied ACL */
+	/* First make sure none of our desired permissions are denied */
+
+	/* Go through all the allowed permissions and record the ones we get */
+
+	/* If we got all needed permissions, return true */
+	if ((granted_access & desired_access) == desired_access)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
