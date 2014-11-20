807165ac2268a6fced108d5c98c8b9b22b2287fa
George Klees <george.t.klees@gmail.com>
2014-11-19 20:46:08 -0500

    lsa_sandbox_violation() will take desired access too

--- kernel/include/security/lsa.h
+++ kernel/include/security/lsa.h
@@ -3,9 +3,10 @@
 
 #include <security/uid.h>
 #include <security/token.h>
+#include <security/access.h>
 
 /* LSA requests */
 token_t *lsa_request_app_token(uint8_t *executable_hash, uid_t user);
-bool lsa_sandbox_violation(void *object);
+bool lsa_sandbox_violation(void *object, access_mask_t desired_access);
 
 #endif
