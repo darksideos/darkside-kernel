5a0a5459c4dfd153b9dac2e633cf81f502ead59f
Artemis Tosini <me@artem.ist>
2014-11-22 18:36:40 -0500

    Basic raspi led

--- bal/src/platform/raspi/loader.s
+++ bal/src/platform/raspi/loader.s
@@ -12,14 +12,26 @@
 @ GNU General Public License for more details.
 @
 @ You should have received a copy of the GNU General Public Licens
-@ along with this program@ if not, write to the Free Software
+@ along with this program; if not, write to the Free Software
 @ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 @ Global start function
-.global start
+.global _start
 _start:
 	@ The stack pointer has to be moved to function on the raspi
 	mov sp, #0x8000
+
+	@ Turn on an LED as a test
+	@ Set the register address
+	ldr r0, =0x20200000
+	@ Set pin 16 as output
+	mov r1,#1
+	lsl r1,#18
+	str r1,[r0,#4]
+	@ Turn on the LED at pin 16 by turning it off
+	mov r1,#1
+	lsl r1,#16
+	str r1,[r0,#40]
 	@ Call our C main function
 	bl main
 
