c75994adb13c5a257f21bb71c197ff1afbddf145
George Klees <george.t.klees@gmail.com>
2014-11-23 15:11:17 -0500

    No such thing as inode_ops_t

--- kernel/include/vfs/inode.h
+++ kernel/include/vfs/inode.h
@@ -1,17 +1,6 @@
 #ifndef __INODE_H
 #define __INODE_H
 
-/* Inode operations structure */
-struct inode;
-typedef struct inode_ops
-{
-	/* Read data from the inode into a buffer */
-	uint64_t (*read)(struct inode *node, void *buffer, uint64_t offset, uint64_t length);
-
-	/* Write data from a buffer into the inode */
-	uint64_t (*write)(struct inode *node, void *buffer, uint64_t offset, uint64_t length);
-} inode_ops_t;
-
 #define INODE_FILE			0
 #define INODE_DIRECTORY		1
 #define INODE_SYMLINK		2
@@ -19,9 +8,6 @@ typedef struct inode_ops
 /* Inode structure */
 typedef struct inode
 {
-	/* Inode operations */
-	inode_ops_t *ops;
-
 	/* Filesystem the inode resides on */
 	struct filesystem *filesystem;
 
