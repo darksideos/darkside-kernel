254fa14f0c1640a017b0dc44c31510db00d98373
Marionumber1 <gksharkboy@gmail.com>
2014-11-22 19:08:53 -0500

    BAL now has generic code

--- bal/src/platform/bios/main.c
+++ bal/src/platform/bios/main.c
@@ -51,14 +51,10 @@ void bal_main(data_t *_data)
 	data_t *data = (data_t*) malloc(sizeof(data_t));
 	memcpy(data, _data, sizeof(data_t));
 
-	printf("About to do stuff\n");
-
 	/* Initialize the physical and virtual memory managers */
-	list_t *phys_mem_map = pmm_init(data->e820_entries, data->num_e820_entries);
-	printf("PMM initialized\n");
+	list_t *phys_mem_map = e820_map_sanitize(data->e820_entries, data->num_e820_entries);
+	pmm_init(phys_mem_map);
 	vmm_init();
-
-	printf("VMM initialized\n");
 
 	/* Initialize the storage tree */
 	storage_init(data->drive_number, data->partition_start);
