9913e21f90264de70f22ea35d842c384e563b3c7
George Klees <george.t.klees@gmail.com>
2014-11-25 21:55:01 -0500

    Declared object namespace functions

---
+++ kernel/include/object/namespace.h
@@ -0,0 +1,19 @@
+#ifndef __NAMESPACE_H
+#define __NAMESPACE_H
+
+/* Look up an object in the object namespace */
+void *namespace_finddir(char *path);
+
+/* Name an object in the object namespace */
+int namespace_hardlink(char *path, void *object);
+
+/* Rename an object in the object namespace */
+int namespace_rename(char *oldpath, char *newpath);
+
+/* Remove an object from the object namespace */
+int namespace_delete(char *path);
+
+/* Initialize the object namespace */
+void namespace_init();
+
+#endif
