cd0559cbb78c854fdac705069b6c75ac3bb40d1a
Marionumber1 <gksharkboy@gmail.com>
2014-12-23 20:12:21 -0500

    Starting object namespace code

---
+++ kernel/src/object/namespace.c
@@ -0,0 +1,8 @@
+#include <types.h>
+#include <string.h>
+#include <object/object.h>
+#include <object/directory.h>
+#include <object/namespace.h>
+
+/* Root of the object namespace */
+static directory_t *root;
