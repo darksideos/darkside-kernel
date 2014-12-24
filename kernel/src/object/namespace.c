fc2b1c3b1810e8b2349850c88830ea8dd3d1ef92
George Klees <george.t.klees@gmail.com>
2014-12-23 23:06:31 -0500

    Written namespace_hardlink()

--- kernel/src/object/namespace.c
+++ kernel/src/object/namespace.c
@@ -8,6 +8,34 @@
 /* Root of the object namespace */
 static directory_t *root;
 
+/* Split paths into parent and child */
+static char *parent_child_split(char *path)
+{
+	/* For root, always return NULL */
+	if (path[0] == '/')
+	{
+		return NULL;
+	}
+
+	/* Get the length of the path */
+	size_t path_len = strlen(path);
+
+	/* Count backwards and try to find a / delimeter */
+	size_t i = path_len - 1;
+	while (i > 0)
+	{
+		if (path[i] == '/')
+		{
+			path[i] = 0;
+			return &path[i];
+		}
+
+		i--;
+	}
+
+	return NULL;
+}
+
 /* Look up an object in the object namespace */
 void *namespace_finddir(char *path, int iid)
 {
@@ -21,7 +49,7 @@ void *namespace_finddir(char *path, int iid)
 	while (element)
 	{
 		/* Find the next spot down */
-		object_t *child = directory_finddir(current, element);
+		void *child = directory_finddir(current, element);
 		if (!child)
 		{
 			return NULL;
@@ -30,11 +58,11 @@ void *namespace_finddir(char *path, int iid)
 		/* If the entire path has been parsed, return the object */
 		if (!next)
 		{
-			return map_get(&child->interfaces, (uint64_t) iid);
+			return object_query_interface(child, iid);
 		}
 
 		/* If we have to traverse more and we didn't get a directory, fail */
-		current = (directory_t*) map_get(&child->interfaces, IID_DIRECTORY);
+		current = (directory_t*) object_query_interface(child, IID_DIRECTORY);
 		if (!current)
 		{
 			return NULL;
@@ -46,4 +74,15 @@ void *namespace_finddir(char *path, int iid)
 	}
 
 	return NULL;
+}
+
+/* Name an object in the object namespace */
+int namespace_hardlink(char *path, void *object)
+{
+	/* Split the path into parent and child components */
+	char *child_name = parent_child_split(path);
+
+	/* Get the parent directory and add the child */
+	directory_t *parent = (directory_t*) namespace_finddir(path, IID_DIRECTORY);
+	return directory_hardlink(parent, child_name, object);
 }
