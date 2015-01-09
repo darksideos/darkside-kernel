b1a47fb816ca244a0a7c5a1654495bfbd8e15222
George Klees <george.t.klees@gmail.com>
2015-01-09 17:57:50 -0500

    Started mutex code

--- kernel/src/microkernel/i686/mutex.c
+++ kernel/src/microkernel/i686/mutex.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2014 DarkSide Project
  * Authored by George Klees <gksharkboy@gmail.com>
- * mutex.h - Mutex public API
+ * mutex.c - Mutexes for the x86 architecture
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -16,22 +16,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#ifndef __MUTEX_H
-#define __MUTEX_H
-
+#include <types.h>
 #include <list.h>
+#include <microkernel/atomic.h>
+#include <microkernel/mutex.h>
 #include <microkernel/thread.h>
-
-/* Mutex structure */
-typedef struct mutex
-{
-	thread_t *owner;
-	list_t waitqueue;
-} mutex_t;
-
-/* Mutex methods */
-void mutex_init(mutex_t *mutex);
-void mutex_acquire(mutex_t *mutex);
-void mutex_release(mutex_t *mutex);
-
-#endif
