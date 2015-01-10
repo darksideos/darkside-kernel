7508fc29a268bd58a0bee8615f562a280e198b3e
Marionumber1 <gksharkboy@gmail.com>
2015-01-09 21:15:43 -0500

    Started rwlocks

--- kernel/src/microkernel/i686/rwlock.c
+++ kernel/src/microkernel/i686/rwlock.c
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2014, 2015 DarkSide Project
+ * Copyright (C) 2015 DarkSide Project
  * Authored by George Klees <gksharkboy@gmail.com>
- * rwlock.h - Readers/writer lock public API
+ * rwlock.c - Readers/writer locks for the x86 architecture
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -16,26 +16,16 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#ifndef __RWLOCK_H
-#define __RWLOCK_H
 
-#include <types.h>
-#include <list.h>
-#include <microkernel/lock.h>
-
-/* Readers/writer lock structure */
-typedef struct rwlock
+/* Initialize a readers/writer lock's values */
+void rwlock_init(rwlock_t *rwlock)
 {
-	unsigned read_count, write_count;
-	list_t waitqueue;
-	spinlock_t waitqueue_lock;
-} rwlock_t;
+	rwlock->read_count = rwlock->write_count = 0;
+	rwlock->waitqueue = list_create();
+	spinlock_init(&rwlock->waitqueue_lock);
+}
 
-/* Readers/writer lock methods */
-void rwlock_init(rwlock_t *rwlock);
-void rwlock_read_acquire(rwlock_t *rwlock);
-void rwlock_read_release(rwlock_t *rwlock);
-void rwlock_write_acquire(rwlock_t *rwlock);
-void rwlock_write_release(rwlock_t *rwlock);
-
-#endif
+/* Acquire a readers/writer lock for reading */
+void rwlock_read_acquire(rwlock_t *rwlock)
+{
+}
