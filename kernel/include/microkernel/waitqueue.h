7b3f25dbf1f75cefe4a617d1eeff5d87fe39ce1d
George Klees <george.t.klees@gmail.com>
2015-01-15 15:22:00 -0500

    Declared wait queue functions

---
+++ kernel/include/microkernel/waitqueue.h
@@ -0,0 +1,17 @@
+#ifndef __WAITQUEUE_H
+#define __WAITQUEUE_H
+
+#include <list.h>
+#include <microkernel/thread.h>
+
+/* Wait queue structure */
+typedef list_t waitqueue_t;
+
+/* Create a wait queue */
+waitqueue_t waitqueue_create();
+
+/* Block and unblock threads on a wait queue */
+void waitqueue_block(waitqueue_t *queue, mkthread_t *thread, int timeout);
+mkthread_t *waitqueue_unblock(waitqueue_t *queue);
+
+#endif
