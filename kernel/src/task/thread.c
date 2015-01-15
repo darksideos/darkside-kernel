b620a64393b255c22222fff812d360d793dc01a7
Marionumber1 <gksharkboy@gmail.com>
2015-01-15 17:41:48 -0500

    More executive thread stuff

--- kernel/src/task/thread.c
+++ kernel/src/task/thread.c
@@ -24,3 +24,43 @@
 #include <object/interface.h>
 #include <security/token.h>
 #include <task/thread.h>
+
+/* Thread object slab cache */
+slab_cache_t *thread_cache;
+
+/* List of threads to be reaped */
+static list_t reaper_list;
+
+/* Create a thread object */
+thread_t *thread_create(process_t *parent_process, void (*fn)(void *args), void *args, size_t stack_size, int numa_domain, int policy, int priority)
+{
+	/* Allocate an object from the slab cache */
+	object_t *object = (object_t*) slab_cache_alloc(thread_cache);
+	if (object == NULL)
+	{
+		return NULL;
+	}
+
+	/* Initialize the generic object header */
+	
+	/* Add the thread object as an interface */
+	object_t **obj_ptr = (object_t**) (((unsigned char*)object) + sizeof(object_t));
+	*obj_ptr = object;
+	thread_t *thread = (thread_t*) (((unsigned char*)object) + sizeof(object_t) + sizeof(object_t*));
+	map_append(&object->interfaces, IID_THREAD, thread);
+
+	/* Initialize the microkernel thread structure */
+	mkprocess_init(&thread->mkthread, parent_process, fn, args, numa_domain, policy, priority, stack_size);
+
+	/* Establish a link between the parent process and ourself */
+	if (parent_process)
+	{
+		list_insert_tail(&parent_process->threads, thread);
+		object_ref(parent_process);
+	}
+
+	/* Set the impersonation token to NULL */
+	thread->impersonate_token = NULL;
+
+	return thread;
+}
