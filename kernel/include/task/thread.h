3f30c8a8b1b4d0f425847d32b64c0fae748ac020
George Klees <george.t.klees@gmail.com>
2015-01-15 17:22:51 -0500

    Starting executive thread stuff

--- kernel/include/task/thread.h
+++ kernel/include/task/thread.h
@@ -18,4 +18,15 @@ typedef struct thread
 	/* Outstanding I/O requests for the thread */
 } thread_t;
 
+/* Create and destroy thread objects */
+thread_t *thread_create(process_t *parent_process, void (*fn)(void *args), void *args, size_t stack_size, int numa_domain, int policy, int priority);
+void thread_destroy(thread_t *thread);
+
+/* Kill a thread */
+void thread_kill(thread_t *thread);
+
+/* Suspend and resume a thread */
+void thread_suspend(thread_t *thread);
+void thread_resume(thread_t *thread);
+
 #endif
---
+++ kernel/src/task/thread.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015 DarkSide Project
+ * Authored by George Klees <gksharkboy@gmail.com>
+ * thread.c - Thread object implementation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public Licens
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include <types.h>
+#include <list.h>
+#include <map.h>
+#include <mm/slab.h>
+#include <object/object.h>
+#include <object/interface.h>
+#include <security/token.h>
+#include <task/thread.h>
