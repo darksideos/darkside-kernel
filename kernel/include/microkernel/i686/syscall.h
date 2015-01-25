89c56e53a091ef49025b20506055a151ee006e29
George Klees <george.t.klees@gmail.com>
2015-01-25 00:13:24 -0500

    syscall_register() takes number of bytes to copy

--- kernel/include/microkernel/i686/syscall.h
+++ kernel/include/microkernel/i686/syscall.h
@@ -2,7 +2,7 @@
 #define __SYSCALL_H
 
 /* Register and unregister syscalls */
-void syscall_register(int num, void *fn);
+void syscall_register(int num, void *fn, size_t params_size);
 void syscall_unregister(int num);
 
 /* Initialize the syscall manager */
