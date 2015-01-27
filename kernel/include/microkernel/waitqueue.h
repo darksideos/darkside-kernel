48e222760425e6b33c3bc6ac2423950ab1449f88
Marionumber1 <gksharkboy@gmail.com>
2015-01-27 17:01:38 -0500

    Finished rwlocks

--- kernel/include/microkernel/waitqueue.h
+++ kernel/include/microkernel/waitqueue.h
@@ -1,6 +1,7 @@
 #ifndef __WAITQUEUE_H
 #define __WAITQUEUE_H
 
+#include <iterator.h>
 #include <list.h>
 #include <microkernel/thread.h>
 
@@ -13,5 +14,8 @@ typedef list_t waitqueue_t;
 /* Block and unblock threads on a wait queue */
 void waitqueue_block(waitqueue_t *queue, mkthread_t *thread, int timeout);
 mkthread_t *waitqueue_unblock(waitqueue_t *queue);
+
+/* Get an iterator for all the threads on the waitqueue */
+iterator_t waitqueue_threads(waitqueue_t *queue);
 
 #endif
