dfbf3e75b237057558d305d0fa0b6943123e977c
George Klees <george.t.klees@gmail.com>
2015-05-02 14:08:18 -0400

    Add core device driver interface headers

---
+++ kernel/include/device/iface_device.h
@@ -0,0 +1,11 @@
+#ifndef __IFACE_DEVICE
+#define __IFACE_DEVICE
+
+/* Enumerate request */
+typedef struct enumerate_request
+{
+	/* Request header */
+	req_t header;
+} enumerate_request_t;
+
+#endif
