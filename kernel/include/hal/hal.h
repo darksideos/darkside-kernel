62571fa49c60f9c87372464ff0db10d25dad7310
George Klees <george.t.klees@gmail.com>
2015-05-09 22:48:25 -0400

    Simple HAL API written

--- kernel/include/hal/hal.h
+++ kernel/include/hal/hal.h
@@ -21,8 +21,22 @@
 
 #include <types.h>
 #include <init/loader.h>
+#include <microkernel/interrupt.h>
 
 /* Initialize the HAL */
 void hal_init(loader_block_t *loader_block, bool bsp);
+
+/* Translate a GSI number to an interrupt controller */
+interrupt_controller_t **get_int_controller(int gsi);
+
+/* Sleep for a specified amount of time (low and high precision) */
+void usleep(uint64_t us);
+void nanosleep(uint64_t ns);
+
+/* Stall execution for a specified amount of time */
+void ustall(uint64_t us);
+
+/* Get the system time */
+uint64_t get_systime();
 
 #endif
