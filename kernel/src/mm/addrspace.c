2495996495958aa8c051cd9bebc83d7a808b127e
George Klees <george.t.klees@gmail.com>
2015-07-26 01:29:45 -0400

    Begin rewrite of addrspace code

--- kernel/src/mm/addrspace.c
+++ kernel/src/mm/addrspace.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 DarkSide Project
+ * Copyright (C) 2015 DarkSide Project
  * Authored by George Klees <gksharkboy@gmail.com>
  * addrspace.c - Address space management
  *
@@ -18,6 +18,7 @@
  */
 #include <types.h>
 #include <string.h>
+#include <init/loader.h>
 #include <microkernel/cpu.h>
 #include <microkernel/lock.h>
 #include <microkernel/paging.h>
@@ -26,8 +27,6 @@
 #include <mm/vad.h>
 #include <mm/addrspace.h>
 #include <mm/slab.h>
-
-#include <stdio.h>
 
 /* System address space */
 static addrspace_t system_addrspace;
@@ -38,45 +37,16 @@ static slab_cache_t vad_cache;
 /* Resolve an address space constant to a pointer */
 static addrspace_t *resolve_addrspace(addrspace_t *addrspace)
 {
-	/* Current address space */
-	if (addrspace == ADDRSPACE_CURRENT)
-	{
-		return &(process_current()->addrspace);
-	}
-	/* System address space */
-	else if (addrspace == ADDRSPACE_SYSTEM)
-	{
-		return &system_addrspace;
-	}
-	/* Address space pointer */
-	else
-	{
-		return addrspace;
-	}
+	if (addrspace == ADDRSPACE_CURRENT) return &(process_current()->addrspace);
+	else if (addrspace == ADDRSPACE_SYSTEM) return &system_addrspace;
+	else return addrspace;
 }
 
 /* Initialize an address space */
-void addrspace_init(addrspace_t *addrspace, paddr_t address_space, vaddr_t free_start, vaddr_t free_length)
+void addrspace_init(addrspace_t *addrspace, paddr_t address_space, vaddr_t range_start, size_t range_length)
 {
-	/* System address space */
-	if (addrspace == ADDRSPACE_SYSTEM)
-	{
-		/* Allocate a slab */
-		for (size_t i = free_start; i < free_start + SLAB_SIZE; i += PAGE_SIZE)
-		{
-			int color = vaddr_cache_color(i, NUMA_DOMAIN_BEST, 0);
-			vmm_map_page(address_space, i, pmm_alloc_page(0, NUMA_DOMAIN_BEST, color), PAGE_READ | PAGE_WRITE | PAGE_GLOBAL);
-		}
-
-		/* Create the initial slab cache for VADs */
-		void *slab = (void*) free_start;
-		slab_cache_init(&vad_cache, slab, sizeof(vad_t), PAGE_READ | PAGE_WRITE);
-		free_start += SLAB_SIZE;
-		free_length -= SLAB_SIZE;
-
-		/* Set up the pointer to the system address space */
-		addrspace = &system_addrspace;
-	}
+	/* Get the address space pointer */
+	addrspace = resolve_addrspace(addrspace);
 
 	/* Fill in the information */
 	addrspace->address_space = address_space;
@@ -84,159 +54,33 @@ void addrspace_init(addrspace_t *addrspace, paddr_t address_space, vaddr_t free_
 	spinlock_recursive_init(&addrspace->lock);
 
 	/* Initialize the free VAD */
-	addrspace->free.start = free_start;
-	addrspace->free.length = free_length;
+	addrspace->free.start = range_start;
+	addrspace->free.length = range_length;
 	addrspace->free.flags = 0;
 	addrspace->free.prev = addrspace->free.next = NULL;
-
-	/* Initialize the used VAD */
-	addrspace->used_root = &addrspace->used;
-	addrspace->used.height = 0;
-	if (addrspace == &system_addrspace)
-	{
-		addrspace->used.start = KERNEL_ADDRSPACE_START;
-		addrspace->used.length = free_start - KERNEL_ADDRSPACE_START;
-		addrspace->used.flags = PAGE_READ | PAGE_WRITE | PAGE_EXECUTE | PAGE_PRIVATE;
-		addrspace->used.left = addrspace->used.right = NULL;
-	}
-	else
-	{
-		addrspace->used.start = USER_ADDRSPACE_START;
-		addrspace->used.length = free_start - USER_ADDRSPACE_START;
-		addrspace->used.flags = PAGE_INVALID | PAGE_PRIVATE;
-		addrspace->used.left = addrspace->used.right = NULL;
-	}
-}
-
-/* Destroy an address space */
-void addrspace_destroy(addrspace_t *addrspace)
-{
 }
 
 /* Allocate regions of a virtual address space */
 void *addrspace_alloc(addrspace_t *addrspace, size_t size_reserved, size_t size_committed, int flags)
 {
-	/* Get the address space pointer */
-	addrspace = resolve_addrspace(addrspace);
 
-	/* Round up both the reserved and committed sizes to a page boundary */
-	size_reserved = PAGE_ALIGN_UP(size_reserved);
-	size_committed = PAGE_ALIGN_UP(size_committed);
-
-	/* Make sure we don't commit more than we reserve */
-	if (size_committed > size_reserved)
-	{
-		size_committed = size_reserved;
-	}
-
-	/* Search the address space for a free region of suitable size */
-	spinlock_recursive_acquire(&addrspace->lock);
-	vad_t *vad = &addrspace->free;
-	while (vad)
-	{
-		/* Move on if it doesn't fit our allocation */
-		if (vad->length < size_reserved)
-		{
-			vad = vad->next;
-			continue;
-		}
-
-		/* Store the starting address of the allocation */
-		vaddr_t address = vad->start;
-
-		/* Create the guard page if requested */
-		vaddr_t i = address;
-		if (flags & GUARD_BOTTOM)
-		{
-			vmm_map_page(addrspace->address_space, i, 0, PAGE_INVALID);
-			i += PAGE_SIZE;
-		}
-
-		/* Commit all the needed pages */
-		for (; i < address + size_committed; i += PAGE_SIZE)
-		{
-			int color = vaddr_cache_color(i, addrspace->numa_domain, 0);
-			vmm_map_page(addrspace->address_space, i, pmm_alloc_page(0, addrspace->numa_domain, color), flags);
-		}
-
-		/* Modify the free VAD or remove it entirely */
-		if (size_reserved < vad->length)
-		{
-			vad->start += size_reserved;
-			vad->length -= size_reserved;
-		}
-		else
-		{
-			/* Later VAD */
-			if (vad != &addrspace->free)
-			{
-				/* Readjust the linked list */
-				vad->prev->next = vad->next;
-				vad->next->prev = vad->prev;
-
-				/* Free the VAD */
-				slab_cache_free(&vad_cache, vad);
-			}
-			/* Root VAD */
-			else
-			{
-				/* Copy the next VAD into the root one */
-				vad_t *vad_next = vad->next;
-				memcpy(vad, vad_next, sizeof(vad_t));
-
-				/* Free the dynamically-allocated VAD */
-				slab_cache_free(&vad_cache, vad_next);
-			}
-		}
-
-		/* Record metadata, unless told not to */
-		if (!(flags & PAGE_PRIVATE))
-		{
-			/* Create a new VAD to represent the now-used region */
-			vad = slab_cache_alloc(&vad_cache);
-			vad->start = address;
-			vad->length = size_reserved;
-			vad->flags = flags;
-			vad->left = vad->right = NULL;
-			vad->height = 0;
-
-			/* Insert it into the tree */
-			addrspace->used_root = vad_tree_insert(addrspace->used_root, vad);
-		}
-
-		/* Return the address of the allocated region */
-		spinlock_recursive_release(&addrspace->lock);
-		return (void*) address;
-	}
-
-	/* No free region of the address space available */
-	spinlock_recursive_release(&addrspace->lock);
-	return NULL;
 }
 
-/* Free regions of a virtual address space */
-void addrspace_free(addrspace_t *addrspace, void *ptr, size_t size)
+/* Initialize the system address space */
+void system_addrspace_init(loader_block_t *loader_block, paddr_t address_space)
 {
-}
-
-/* Query the protection of a virtual address */
-int addrspace_query(addrspace_t *addrspace, void *ptr)
-{
-	/* Get the address space pointer */
-	addrspace = resolve_addrspace(addrspace);
-
-	/* Look up the VAD for the address */
-	vad_t *vad = vad_tree_lookup(addrspace->used_root, (vaddr_t) ptr);
-	if (!vad)
+	/* Allocate a slab */
+	vaddr_t free_start = loader_block->system_free_start;
+	for (vaddr_t i = free_start; i < free_start + SLAB_SIZE; i += PAGE_SIZE)
 	{
-		return PAGE_INVALID;
+		int color = vaddr_cache_color(i, NUMA_DOMAIN_BEST, 0);
+		vmm_map_page(address_space, i, pmm_alloc_page(0, NUMA_DOMAIN_BEST, color), PAGE_READ | PAGE_WRITE | PAGE_GLOBAL);
 	}
 
-	/* Return the protection flags */
-	return vad->flags;
-}
+	/* Create the initial slab cache for VADs */
+	slab_cache_init(&vad_cache, (void*)free_start, sizeof(vad_t), PAGE_READ | PAGE_WRITE);
+	free_start += SLAB_SIZE;
 
-/* Set the protection of a virtual address range */
-void addrspace_protect(addrspace_t *addrspace, void *ptr, size_t size, int flags)
-{
+	/* Initialize the system address space */
+	addrspace_init(&system_addrspace, address_space, KERNEL_ADDRSPACE_START, KERNEL_ADDRSPACE_SIZE);
 }
