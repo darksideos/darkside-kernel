bf353f348843e26e5547ff424b4b812cd254be33
George Klees <george.t.klees@gmail.com>
2015-08-25 16:10:11 -0400

    Fix build with new dict code

--- libadt/include/dict.h
+++ libadt/include/dict.h
@@ -3,23 +3,26 @@
 
 #include <map.h>
 
-typedef struct dict_entry {
+typedef struct dict_entry
+{
     const char *key;
     void *value;
 } dict_entry_t;
 
-typedef struct dict {
+typedef struct dict
+{
     uint32_t occupied;
     uint32_t total_size;
 
     dict_entry_t **entries;
-};
+} dict_t;
 
 /* Dictionary methods */
-dict_t dict_create(uint32_t capacity);
+dict_t dict_create();
 void dict_destroy(dict_t *dict);
-void dict_append(dict_t *dict, const char *key, void *item);
 void *dict_get(dict_t *dict, const char *key);
 void dict_put(dict_t *dict, const char *key, void *item);
+#define dict_append(dict,key,item) dict_put(dict, key, item);
+void *dict_remove(dict_t *dict, const char *key);
 
 #endif
