91efc9c8545b454aec177e4eb98b331eeb79c95e
Noah Singer <singerng@gmail.com>
2015-08-25 15:46:51 -0400

    Replaced dict s with linear probing implementation

--- libadt/src/dict.c
+++ libadt/src/dict.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2014 DarkSide Project
- * Authored by George Klees <gksharkboy@gmail.com>
+ * Copyright (C) 2015 DarkSide Project
+ * Authored by Noah Singer <singerng@gmail.com>
  * dict.c - Dictionary abstract data type
  *
  * This program is free software; you can redistribute it and/or modify
@@ -17,7 +17,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <types.h>
-#include <map.h>
 #include <dict.h>
 
 /* Hash a dictionary key */
@@ -30,48 +29,82 @@ static uint64_t hash(const char *key)
 		hash_key *= 0x1F;
 		hash_key += *key;
 		key++;
-
 	}
 
 	return hash_key;
 }
 
 /* Create a dictionary */
-dict_t dict_create()
+dict_t dict_create(uint32_t capacity)
 {
-	return map_create();
+	dict_t dict;
+
+	dict.occupied = 0;
+	dict.total_size = capacity;
+	dict.entries = (dict_entry_t**) malloc(sizeof(dict_entry_t) * capacity);
+
+	return dict;
 }
 
 /* Destroy a dictionary */
 void dict_destroy(dict_t *dict)
 {
-	map_destroy(dict);
+	free(dict.entries);
 }
 
-/* Append an item to a dictionary */
-void dict_append(dict_t *dict, const char *key, void *item)
+/* Increase a dictionary's capacity when its load factor grows too large */
+static void dict_resize(dict_t *dict, uint32_t capacity)
 {
-	uint64_t hash_key = hash(key);
-	map_append(dict, hash_key, item);
+	dict_entry_t old_entries[] = *(dict->entries);
+	dict->entries = (dict_entry_t**) malloc(sizeof(dict_entry_t) * capacity);
+
+	for (int i = 0; i < dict->occupied; i++)
+	{
+		if (old_entries[i] != NULL) dict_set(dict, old_entries[i].key, old_entries[i].value);
+	}
 }
 
-/* Remove an item from a dictionary */
-void *dict_remove(dict_t *dict, const char *key)
+/* Set an item in a dictionary */
+static void dict_set(dict_t *dict, const char *key, void *item)
 {
+	dict_entry_t entries[] = *(dict->entries);
 	uint64_t hash_key = hash(key);
-	return map_remove(dict, hash_key);
+
+	/* If load factor >= .5, double the size */
+	if (dict->occupied >= dict->total_size / 2) dict_resize(dict, 2 * dict->total_size);
+
+	for (int i = hash_key % dict->total_size; entries[i].key != NULL; i = (i+1) % dict->total_size)
+	{
+		if (strcmp(entries[i].key, key))
+		{
+			entries[i].value = item;
+			return;
+		}
+	}
+
+	entries[i].key = key;
+	entries[i].value = value;
+}
+
+/* Put an item in a dictionary */
+void dict_put(dict_t *dict, const char *key, void *item)
+{
+	size_t len = strlen(key);
+	const char *buf = malloc(len+1);
+	memcpy(buf, key, len+1);
+
+	dict_set(dict, buf, item);
 }
 
 /* Get an item in a dictionary */
 void *dict_get(dict_t *dict, const char *key)
 {
-	uint64_t hash_key = hash(key);
-	return map_get(dict, hash_key);
-}
+	dict_entry_t entries[] = *(dict->entries);
 
-/* Set an item in a dictionary */
-void dict_set(dict_t *dict, const char *key, void *item)
-{
-	uint64_t hash_key = hash(key);
-	map_set(dict, hash_key, item);
+	for (int i = hash_key % dict->total_size; entries[i].key != NULL; i = (i+1) % dict->total_size)
+	{
+		if (strcmp(entries[i].key, key)) return entries[i].value;
+	}
+
+	return NULL;
 }
