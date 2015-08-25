73fd497ad7a95cc4a056acdf45a66c48f5d184f4
Noah Singer <singerng@gmail.com>
2015-08-25 15:46:51 -0400

    Replaced dict s with linear probing implementation

--- libadt/include/dict.h
+++ libadt/include/dict.h
@@ -3,15 +3,23 @@
 
 #include <map.h>
 
-/* Dictionary structure */
-typedef map_t dict_t;
+typedef struct dict_entry {
+    const char *key;
+    void *value;
+} dict_entry_t;
+
+typedef struct dict {
+    uint32_t occupied;
+    uint32_t total_size;
+
+    dict_entry_t **entries;
+};
 
 /* Dictionary methods */
-dict_t dict_create();
+dict_t dict_create(uint32_t capacity);
 void dict_destroy(dict_t *dict);
 void dict_append(dict_t *dict, const char *key, void *item);
-void *dict_remove(dict_t *dict, const char *key);
 void *dict_get(dict_t *dict, const char *key);
-void dict_set(dict_t *dict, const char *key, void *item);
+void dict_put(dict_t *dict, const char *key, void *item);
 
 #endif
