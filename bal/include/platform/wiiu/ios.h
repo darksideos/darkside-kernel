8a64f424324645525dca9917259694df01a181b7
George Klees <george.t.klees@gmail.com>
2015-12-27 01:22:09 -0500

    Finish IOSU implementation (except seek)

--- bal/include/platform/wiiu/ios.h
+++ bal/include/platform/wiiu/ios.h
@@ -1,7 +1,30 @@
 #ifndef __IOS_H
 #define __IOS_H
 
+/* ioctlv vector */
+struct iovec
+{
+	uint32_t addr_phys;
+	size_t len;
+	void *addr_virt;
+};
+
+/* Open an IOSU node */
+int IOS_Open(char *path, int mode);
+
 /* Close an open IOSU handle */
 int IOS_Close(int fd);
+
+/* Read data from an open IOSU node */
+int IOS_Read(int fd, void *buffer, size_t length);
+
+/* Write data to an open IOSU node */
+int IOS_Write(int fd, void *buffer, size_t length);
+
+/* Send an ioctl to an open IOSU node */
+int IOS_Ioctl(int fd, int cmd, void *inbuf, size_t inlen, void *outbuf, size_t outlen);
+
+/* Send an ioctlv to an open IOSU node */
+int IOS_Ioctlv(int fd, int cmd, int cnt_io, int cnt_out, struct iovec *vecs);
 
 #endif
--- bal/src/platform/wiiu/ios.c
+++ bal/src/platform/wiiu/ios.c
@@ -1,6 +1,7 @@
 #include <types.h>
 #include <string.h>
 #include <arch/ppc/cache.h>
+#include <platform/wiiu/ios.h>
 
 /* IPC engine registers */
 #define LT_IPC_PPC1_PPCMSG	((volatile uint32_t*)0x0D000410)
@@ -80,6 +81,7 @@ static void ipc_send_req()
 	ipc_bell(PPCCTRL_ACK);
 
 	/* Acknowledge the Latte IRQ */
+	*((uint32_t*)0x0d000454) = 0x40000000;
 }
 
 /* Wait for an IPC reply back */
@@ -97,7 +99,7 @@ static void ipc_recv_reply(void)
 		ipc_bell(PPCCTRL_DONE);
 
 		/* Acknowledge the Latte IRQ and tell IOSU to relaunch */
-		//Ack here
+		*((uint32_t*)0x0d000454) = 0x40000000;
 		ipc_bell(PPCCTRL_RELAUNCH);
 	}
 
@@ -121,6 +123,30 @@ static void ipc_set_common_fields()
 	ipc.title_id = 0x000500301001210A;
 }
 
+/* Open an IOSU node */
+int IOS_Open(char *path, int mode)
+{
+	/* Clear the request buffer */
+	memset(&ipc, 0, sizeof(ipc));
+
+	/* Get the length of the path and flush the path */
+	size_t pathlen = strlen(path);
+	DCFlushRange(path, pathlen+1);
+
+	/* Set the command and its arguments, and fill in the common fields */
+	ipc.cmd = ipc.prev_cmd = 1;
+	ipc.fd = ipc.prev_fd = 0;
+	ipc.args[0] = (uint32_t)path;
+	ipc.args[1] = pathlen;
+	ipc.args[2] = mode;
+	ipc_set_common_fields();
+
+	/* Send the request, wait for the reply, and return it */
+	ipc_send_req();
+	ipc_recv_reply();
+	return ipc.reply;
+}
+
 /* Close an open IOSU handle */
 int IOS_Close(int fd)
 {
@@ -131,6 +157,118 @@ int IOS_Close(int fd)
 	ipc.cmd = ipc.prev_cmd = 2;
 	ipc.fd = ipc.prev_fd = fd;
 	ipc_set_common_fields();
+
+	/* Send the request, wait for the reply, and return it */
+	ipc_send_req();
+	ipc_recv_reply();
+	return ipc.reply;
+}
+
+/* Read data from an open IOSU node */
+int IOS_Read(int fd, void *buffer, size_t length)
+{
+	/* Clear the request buffer */
+	memset(&ipc, 0, sizeof(ipc));
+
+	/* Flush the data buffer */
+	DCFlushRange(buffer, length);
+
+	/* Set the command and its arguments, and fill in the common fields */
+	ipc.cmd = ipc.prev_cmd = 3;
+	ipc.fd = ipc.prev_fd = fd;
+	ipc.args[0] = (uint32_t)buffer;
+	ipc.args[1] = length;
+	ipc_set_common_fields();
+
+	/* Send the request, wait for the reply, and return it */
+	ipc_send_req();
+	ipc_recv_reply();
+	return ipc.reply;
+}
+
+/* Write data to an open IOSU node */
+int IOS_Write(int fd, void *buffer, size_t length)
+{
+	/* Clear the request buffer */
+	memset(&ipc, 0, sizeof(ipc));
+
+	/* Set the command and its arguments, and fill in the common fields */
+	ipc.cmd = ipc.prev_cmd = 4;
+	ipc.fd = ipc.prev_fd = fd;
+	ipc.args[0] = (uint32_t)buffer;
+	ipc.args[1] = length;
+	ipc_set_common_fields();
+
+	/* Send the request and wait for the reply */
+	ipc_send_req();
+	ipc_recv_reply();
+
+	/* Invalidate the data buffer and return the reply */
+	DCInvalidateRange(buffer, length);
+	return ipc.reply;
+}
+
+/* Send an ioctl to an open IOSU node */
+int IOS_Ioctl(int fd, int cmd, void *inbuf, size_t inlen, void *outbuf, size_t outlen)
+{
+	/* Clear the request buffer */
+	memset(&ipc, 0, sizeof(ipc));
+
+	/* Flush the input data buffer (if valid) */
+	if (inbuf) DCFlushRange(inbuf, inlen);
+
+	/* Set the command and its arguments, and fill in the common fields */
+	ipc.cmd = ipc.prev_cmd = 6;
+	ipc.fd = ipc.prev_fd = fd;
+	ipc.args[0] = (uint32_t)cmd;
+	ipc.args[1] = (uint32_t)inbuf;
+	ipc.args[2] = inlen;
+	ipc.args[3] = (uint32_t)outbuf;
+	ipc.args[4] = outlen;
+	ipc_set_common_fields();
+
+	/* Invalidate the output data buffer (if valid) */
+	if (outbuf) DCInvalidateRange(outbuf, outlen);
+
+	/* Send the request, wait for the reply, and return it */
+	ipc_send_req();
+	ipc_recv_reply();
+	return ipc.reply;
+}
+
+/* Send an ioctlv to an open IOSU node */
+int IOS_Ioctlv(int fd, int cmd, int cnt_io, int cnt_out, struct iovec *vecs)
+{
+	/* Clear the request buffer */
+	memset(&ipc, 0, sizeof(ipc));
+
+	/* For every vector, store its physical address (and flush valid inputs) */
+	for (int i = 0; i < cnt_io + cnt_out; i++)
+	{
+		/* Store physical address */
+		vecs[i].addr_phys = (uint32_t)vecs[i].addr_virt;
+
+		/* Flush input vectors (if valid) */
+		if (i < cnt_io && vecs[i].addr_virt) DCFlushRange(vecs[i].addr_virt, vecs[i].len);
+	}
+
+	/* Flush the array of I/O vectors */
+	DCFlushRange(vecs, sizeof(struct iovec) * (cnt_io + cnt_out));
+
+	/* Set the command and its arguments, and fill in the common fields */
+	ipc.cmd = ipc.prev_cmd = 7;
+	ipc.fd = ipc.prev_fd = fd;
+	ipc.args[0] = (uint32_t)cmd;
+	ipc.args[1] = (uint32_t)cnt_io;
+	ipc.args[2] = (uint32_t)cnt_out;
+	ipc.args[3] = (uint32_t)vecs;
+	ipc_set_common_fields();
+
+	/* Invalidate the output data buffers (if valid) */
+	for (int i = 0; i < cnt_io + cnt_out; i++)
+	{
+		if (vecs[i].addr_virt) DCInvalidateRange(vecs[i].addr_virt, vecs[i].len);
+	}
 
 	/* Send the request, wait for the reply, and return it */
 	ipc_send_req();
