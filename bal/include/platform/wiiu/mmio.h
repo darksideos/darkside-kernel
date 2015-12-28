38519690a98e81873ed96528113db75faf510752
George Klees <george.t.klees@gmail.com>
2015-12-27 20:07:32 -0500

    Make write32 read back, still no reply

--- bal/include/platform/wiiu/mmio.h
+++ bal/include/platform/wiiu/mmio.h
@@ -12,7 +12,7 @@ static inline uint32_t read32(uint32_t addr)
 /* Write a 32-bit MMIO value */
 static inline void write32(uint32_t addr, uint32_t val)
 {
-	__asm__ volatile("stw %0, 0(%1); eieio" :: "r"(val), "b"(addr));
+	__asm__ volatile("stw %0, 0(%1); eieio; lwz %0, 0(%1)" :: "r"(val), "b"(addr));
 }
 
 #endif
